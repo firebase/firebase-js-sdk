f133a860363e9d77461e4e15285374b648d9f5e8
Josh Crowther <jshcrowthe@gmail.com>
2017-07-27 11:34:41 -0700

    fix(ie11): add Array.prototype.find/findIndex polyfills
    These two functions are part of ES2015 and are not present in IE11. In the migration we removed
    closure compiler which was injecting these by default

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,9 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
+// Import the needed shims
+import './utils/shims';
+
 // Import the createFirebaseNamespace function
 import { createFirebaseNamespace, FirebaseNamespace } from './app/firebase_app';
 
