2c42877a7924affe4fc339a14b736faabba562b1
Josh Crowther <jshcrowthe@gmail.com>
2017-09-20 16:30:40 -0700

    Fix issue with indv-module builds

--- packages/core/webpack.config.js
+++ packages/core/webpack.config.js
@@ -1,8 +1,9 @@
-const { resolve } = require('path');
+const { resolve, parse } = require('path');
 const CompressionPlugin = require('compression-webpack-plugin');
 const gitRev = require('git-rev-sync');
 const pkg = require('./package');
 const webpack = require('webpack');
+const WrapperPlugin = require('wrapper-webpack-plugin');
 
 const licenseHeader = `@license Firebase v${pkg.version}
 Build: rev-${gitRev.short()}
@@ -52,6 +53,11 @@ const singleExport = Object.assign({}, baseConfig, {
   })
 });
 
+function isFirebaseApp(fileName) {
+  const pathObj = parse(fileName);
+  return pathObj.name === 'firebase-app';
+}
+
 const multiExport = Object.assign({}, baseConfig, {
   entry: {
     'firebase-app': resolve(__dirname, 'app/index.js'),
@@ -62,6 +68,29 @@ const multiExport = Object.assign({}, baseConfig, {
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
       name: 'firebase-app'
+    }),
+    new WrapperPlugin({
+      header: fileName => {
+        return isFirebaseApp(fileName)
+          ? `var firebase = (function() {
+          var window = typeof window === 'undefined' ? self : window;
+        return `
+          : `try {
+        `;
+      },
+      footer: fileName => {
+        // Note: '.default' needed because of https://github.com/babel/babel/issues/2212
+        return isFirebaseApp(fileName)
+          ? `
+        })().default;`
+          : `
+        } catch(error) {
+          throw new Error(
+            'Cannot instantiate ${fileName} - ' +
+            'be sure to load firebase-app.js first.'
+          )
+        }`;
+      }
     }),
     ...baseConfig.plugins
   ]
