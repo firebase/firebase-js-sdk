c03cb7fb745dd22bd3de598cf6b8bdbc40e800ce
Josh Crowther <jshcrowthe@gmail.com>
2017-09-21 19:17:28 -0700

    Fixing broken int tests

--- integration/typescript/karma.conf.js
+++ integration/typescript/karma.conf.js
@@ -21,7 +21,7 @@ const karmaBase = require('../../config/karma.base');
 module.exports = function(config) {
   const karmaConfig = Object.assign({}, karmaBase, {
     // files to load into karma
-    files: [{ pattern: `dist/**/*` }],
+    files: ['test/**/*.test.*'],
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha']
