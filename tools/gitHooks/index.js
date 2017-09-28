129f269cdc068a4ff91ca031779afd22fe0a0d88
Josh Crowther <jshcrowthe@gmail.com>
2017-09-27 19:34:01 -0700

    Refactor precommit hook -> prepush hook

---
+++ tools/gitHooks/index.js
@@ -0,0 +1,30 @@
+const { doPrettierCommit } = require('./prettier');
+const { resolve } = require('path');
+const simpleGit = require('simple-git/promise');
+const chalk = require('chalk');
+
+// Computed Deps
+const root = resolve(__dirname, '../..');
+const git = simpleGit(root);
+
+const notCleanTreeString = chalk`
+{red Can only push a clean git tree. Please stash your changes and try again}
+
+{yellow You can stash your changes by running:}
+$ git stash -u
+
+{yellow You can then unstash your changes by running:}
+$ git stash pop
+`;
+
+(async () => {
+  const hasDiff = !!(await git.diff());
+
+  if (hasDiff) {
+    console.error(notCleanTreeString);
+    return process.exit(1);
+  }
+
+  // Style the code
+  await doPrettierCommit();
+})();
---
+++ tools/gitHooks/prettier.js
@@ -0,0 +1,36 @@
+const { resolve } = require('path');
+const { spawn } = require('child-process-promise');
+const fs = require('mz/fs');
+const glob = require('glob');
+const simpleGit = require('simple-git/promise');
+
+// Computed Deps
+const root = resolve(__dirname, '../..');
+const git = simpleGit(root);
+
+async function doPrettierCommit() {
+  await spawn('prettier', [
+    '--config',
+    `${resolve(root, '.prettierrc')}`,
+    '--write',
+    '**/*.{ts,js}'
+  ], {
+    stdio: 'inherit',
+    cwd: root,
+    env: {
+      PATH: `${resolve(root, 'node_modules/.bin')}:${process.env.PATH}`
+    }
+  });
+
+  const hasDiff = await git.diff();
+
+  if (!hasDiff) return;
+
+  await git.add('.');
+
+  await git.commit('[AUTOMATED]: Format Styling');
+}
+
+module.exports = {
+  doPrettierCommit
+};
