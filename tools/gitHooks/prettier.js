1047067b24ce63a3bc4de266d345a67b7df9d82b
Josh Crowther <jshcrowthe@gmail.com>
2017-09-28 00:30:24 -0700

    Add a spinner instead of the prettier stdio

--- tools/gitHooks/prettier.js
+++ tools/gitHooks/prettier.js
@@ -3,23 +3,26 @@ const { spawn } = require('child-process-promise');
 const fs = require('mz/fs');
 const glob = require('glob');
 const simpleGit = require('simple-git/promise');
+const ora = require('ora');
 
 // Computed Deps
 const root = resolve(__dirname, '../..');
 const git = simpleGit(root);
 
 async function doPrettierCommit() {
+  const spinner = ora('Formatting code with prettier').start();
   await spawn(
     'prettier',
     ['--config', `${resolve(root, '.prettierrc')}`, '--write', '**/*.{ts,js}'],
     {
-      stdio: 'inherit',
+      stderr: 'inherit',
       cwd: root,
       env: {
         PATH: `${resolve(root, 'node_modules/.bin')}:${process.env.PATH}`
       }
     }
   );
+  spinner.stop();
 
   const hasDiff = await git.diff();
 
