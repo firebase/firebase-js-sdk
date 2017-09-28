f7920c7b2ab47a54155d90b183f3a2e8be44784d
Josh Crowther <jshcrowthe@gmail.com>
2017-09-27 17:09:07 -0700

    Refactor test setup script and .travis.yml

--- tools/config.js
+++ tools/config.js
@@ -1,52 +1,63 @@
+const { argv } = require('yargs');
 const firebaseTools = require('firebase-tools');
 const inquirer = require('inquirer');
 const fs = require('mz/fs');
 const path = require('path');
 
-firebaseTools.login
-  .ci()
+// Command Line Arguments
+const userToken = argv.token;
+const projectId = argv.projectId;
+
+Promise.resolve(userToken)
   // Log in to firebase-tools
-  .then(authData => {
-    const { tokens: { access_token: token } } = authData;
+  .then(async userToken => {
+    if (userToken) return userToken;
+    const {
+      tokens: { refresh_token: freshToken }
+    } = await firebaseTools.login.ci();
+    return freshToken;
+  })
+  // Capture the firebase test project
+  .then(async token => {
+    const project = await (async () => {
+      if (projectId) return projectId;
 
-    return firebaseTools
-      .list({ token })
-      .then(projects =>
-        inquirer.prompt([
-          {
-            type: 'list',
-            name: 'projectId',
-            message: 'Which project would you like to use to test?',
-            choices: projects.map(project => ({
-              name: `${project.name} (${project.id})`,
-              value: project
-            }))
-          }
-        ])
-      )
-      .then(response => {
-        // Capture project id
-        const { projectId: { id: project } } = response;
+      const projects = await firebaseTools.list({ token });
+      const response = await inquirer.prompt([
+        {
+          type: 'list',
+          name: 'projectId',
+          message: 'Which project would you like to use to test?',
+          choices: projects.map(project => ({
+            name: `${project.name} (${project.id})`,
+            value: project
+          }))
+        }
+      ]);
 
-        // Write config to top-level config directory
-        const writeConfig = firebaseTools.setup
-          .web({ project, token })
-          .then(config =>
-            fs.writeFile(
-              path.resolve(__dirname, '../config/project.json'),
-              JSON.stringify(config, null, 2)
-            )
-          );
+      const { projectId: { id } } = response;
 
-        // Deploy database rules
-        const deployRules = firebaseTools.deploy({
-          project,
-          token,
-          cwd: path.resolve(__dirname, '../config')
-        });
+      return id;
+    })();
 
-        return Promise.all([writeConfig, deployRules]);
-      });
+    // Write config to top-level config directory
+    const writeConfig = firebaseTools.setup
+      .web({ project, token })
+      .then(config =>
+        fs.writeFile(
+          path.resolve(__dirname, '../config/project.json'),
+          JSON.stringify(config, null, 2)
+        )
+      );
+
+    // Deploy database rules
+    const deployRules = firebaseTools.deploy({
+      project,
+      token,
+      cwd: path.resolve(__dirname, '../config')
+    });
+
+    return Promise.all([writeConfig, deployRules]);
   })
   .then(() => {
     console.log('Success! Exiting...');
