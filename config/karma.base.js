9e4c93bab737b0cc929f6703f47231c499dd940e
Josh Crowther <jshcrowthe@gmail.com>
2017-10-02 15:08:21 -0700

    [AUTOMATED]: Prettier Code Styling

--- config/karma.base.js
+++ config/karma.base.js
@@ -82,15 +82,15 @@ const config = {
       colors: true
     }
   },
-  
+
   sauceLabs: {
     tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
     username: process.env.SAUCE_USERNAME,
     accessKey: process.env.SAUCE_ACCESS_KEY,
     startConnect: false,
     connectOptions: {
-        port: 5757,
-        logfile: 'sauce_connect.log'
+      port: 5757,
+      logfile: 'sauce_connect.log'
     }
   },
 
