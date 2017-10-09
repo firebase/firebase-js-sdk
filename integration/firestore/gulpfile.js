d600a7aa314ed23259bbaae7b7d9a8089b5eb0f0
Josh Crowther <jshcrowthe@gmail.com>
2017-10-09 14:43:43 -0700

    [AUTOMATED]: Prettier Code Styling

--- integration/firestore/gulpfile.js
+++ integration/firestore/gulpfile.js
@@ -6,13 +6,26 @@ const webpack = require('webpack');
 
 function compileIntTests() {
   const config = require('../../config/webpack.test');
-  return gulp.src(resolve(__dirname, '../../packages/firestore/test/integration/**/*.ts'))
-    .pipe(replace(/import firebase from ('|")[^\1]+firebase_export\1;?/, 'declare var firebase;'))
-    .pipe(webpackStream(Object.assign({}, config, {
-      output: {
-        filename: 'test-harness.js'
-      }
-    }), webpack))
+  return gulp
+    .src(
+      resolve(__dirname, '../../packages/firestore/test/integration/**/*.ts')
+    )
+    .pipe(
+      replace(
+        /import firebase from ('|")[^\1]+firebase_export\1;?/,
+        'declare var firebase;'
+      )
+    )
+    .pipe(
+      webpackStream(
+        Object.assign({}, config, {
+          output: {
+            filename: 'test-harness.js'
+          }
+        }),
+        webpack
+      )
+    )
     .pipe(gulp.dest('dist'));
 }
 
