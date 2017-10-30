f021072dc2757517b3abe23a037c1ba58599cf7c
Josh Crowther <jshcrowthe@gmail.com>
2017-10-30 16:49:58 -0700

    Open Source Firebase Authentication (#273)
    * Open source Firebase Auth
    
    * Cleans up some comments in the Auth open sourced code.
    
    * Recommiting yarn.lock

---
+++ packages/auth/test/storage/indexeddb_test.js
@@ -0,0 +1,328 @@
+/**
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+goog.provide('fireauth.storage.IndexedDBTest');
+
+goog.require('fireauth.AuthError');
+goog.require('fireauth.authenum.Error');
+goog.require('fireauth.storage.IndexedDB');
+goog.require('goog.Promise');
+goog.require('goog.testing.MockClock');
+goog.require('goog.testing.PropertyReplacer');
+goog.require('goog.testing.jsunit');
+goog.require('goog.testing.recordFunction');
+
+goog.setTestOnly('fireauth.storage.IndexedDBTest');
+
+
+var stubs = new goog.testing.PropertyReplacer();
+var db = null;
+var manager;
+var clock;
+var indexedDBMock;
+
+
+function setUp() {
+  // IndexedDB not supported in IE9.
+  stubs.replace(
+      fireauth.storage.IndexedDB,
+      'isAvailable',
+      function() {
+        return true;
+      });
+  clock = new goog.testing.MockClock(true);
+  indexedDBMock = {
+    open: function(dbName, version) {
+      assertEquals('firebaseLocalStorageDb', dbName);
+      assertEquals(1, version);
+      var dbRequest = {};
+      goog.Promise.resolve().then(function() {
+        db = {
+          key: null,
+          store: {},
+          createObjectStore: function(objectStoreName, keyPath) {
+            var request = {
+              'transaction': {
+              }
+            };
+            assertEquals(
+                'firebaseLocalStorage',
+                objectStoreName);
+            assertObjectEquals(
+                {
+                  'keyPath': 'fbase_key'
+                },
+                keyPath);
+            db.key = keyPath['keyPath'];
+            db.store[objectStoreName] = {};
+            goog.Promise.resolve().then(function() {
+              var event = {
+                'target': {
+                  'result': db
+                }
+              };
+              dbRequest.onsuccess(event);
+            });
+            return request;
+          },
+          transaction: function(objectStores, type) {
+            for (var i = 0; i < objectStores.length; i++) {
+              if (!db.store[objectStores[i]]) {
+                fail('Object store does not exist!');
+              }
+            }
+            return {
+              objectStore: function(objectStoreName) {
+                if (!db.store[objectStoreName]) {
+                  fail('Object store does not exist!');
+                }
+                return {
+                  add: function(data) {
+                    var request = {};
+                    if (type != 'readwrite') {
+                      fail('Invalid write operation!');
+                    }
+                    if (db.store[objectStoreName][data[db.key]]) {
+                      fail('Unable to add. Key already exists!');
+                    }
+                    goog.Promise.resolve().then(function() {
+                      db.store[objectStoreName][data[db.key]] = data;
+                      request.onsuccess();
+                    });
+                    return request;
+                  },
+                  put: function(data) {
+                    var request = {};
+                    if (type != 'readwrite') {
+                      fail('Invalid write operation!');
+                    }
+                    if (!db.store[objectStoreName][data[db.key]]) {
+                      fail('Unable to put. Key does not exist!');
+                    }
+                    for (var subKey in data) {
+                      db.store[objectStoreName][data[db.key]][subKey] =
+                          data[subKey];
+                    }
+                    goog.Promise.resolve().then(function() {
+                      request.onsuccess();
+                    });
+                    return request;
+                  },
+                  delete: function(keyToRemove) {
+                    var request = {};
+                    if (type != 'readwrite') {
+                      fail('Invalid write operation!');
+                    }
+                    if (db.store[objectStoreName][keyToRemove]) {
+                      delete db.store[objectStoreName][keyToRemove];
+                    }
+                    goog.Promise.resolve().then(function() {
+                      request.onsuccess();
+                    });
+                    return request;
+                  },
+                  get: function(keyToGet, callback) {
+                    var request = {};
+                    var data = db.store[objectStoreName][keyToGet] || null;
+                    goog.Promise.resolve().then(function() {
+                      var event = {
+                        'target': {
+                          'result': data
+                        }
+                      };
+                      request.onsuccess(event);
+                    });
+                    return request;
+                  },
+                  getAll: function() {
+                    var request = {};
+                    var results = [];
+                    for (var key in db.store[objectStoreName]) {
+                      results.push(db.store[objectStoreName][key]);
+                    }
+                    goog.Promise.resolve().then(function() {
+                      var event = {
+                        'target': {
+                          'result': results
+                        }
+                      };
+                      request.onsuccess(event);
+                    });
+                    return request;
+                  }
+                };
+              }
+            };
+          }
+        };
+        var event = {
+          'target': {
+            'result': db
+          }
+        };
+        dbRequest.onupgradeneeded(event);
+      });
+      return dbRequest;
+    }
+  };
+}
+
+
+function tearDown() {
+  if (manager) {
+    manager.removeAllStorageListeners();
+  }
+  manager = null;
+  db = null;
+  indexedDBMock = null;
+  stubs.reset();
+  goog.dispose(clock);
+}
+
+
+/**
+ * Asserts that two errors are equivalent. Plain assertObjectEquals cannot be
+ * used as Internet Explorer adds the stack trace as a property of the object.
+ * @param {!fireauth.AuthError} expected
+ * @param {!fireauth.AuthError} actual
+ */
+function assertErrorEquals(expected, actual) {
+  assertObjectEquals(expected.toPlainObject(), actual.toPlainObject());
+}
+
+
+/**
+ * @return {!fireauth.storage.IndexedDB} The default indexedDB
+ *     local storage manager to be used for testing.
+ */
+function getDefaultFireauthManager() {
+  return new fireauth.storage.IndexedDB(
+      'firebaseLocalStorageDb',
+      'firebaseLocalStorage',
+      'fbase_key',
+      'value',
+      1,
+      indexedDBMock);
+}
+
+
+function testIndexedDb_notSupported() {
+  // Test when indexedDB is not supported.
+  stubs.replace(
+      fireauth.storage.IndexedDB,
+      'isAvailable',
+      function() {
+        return false;
+      });
+  var expectedError = new fireauth.AuthError(
+      fireauth.authenum.Error.WEB_STORAGE_UNSUPPORTED);
+  try {
+    getDefaultFireauthManager();
+    fail('Should fail when indexedDB is not supported.');
+  } catch (error) {
+    assertErrorEquals(expectedError, error);
+  }
+}
+
+
+function testIndexedDb_null() {
+  manager = getDefaultFireauthManager();
+  return manager.get('key1')
+      .then(function(data) {
+        assertNull(data);
+      });
+}
+
+
+function testIndexedDb_setGetRemove() {
+  manager = getDefaultFireauthManager();
+  manager.addStorageListener(function() {
+    fail('Storage should not be triggered for local changes!');
+  });
+  return goog.Promise.resolve()
+      .then(function() {
+        return manager.set('key1', 'value1');
+      })
+      .then(function() {
+        return manager.get('key1');
+      })
+      .then(function(data) {
+        assertEquals('value1', data);
+      })
+      .then(function() {
+        return manager.remove('key1');
+      })
+      .then(function() {
+        return manager.get('key1');
+      })
+      .then(function(data) {
+        assertNull(data);
+      });
+}
+
+
+function testStartListeners() {
+  manager = getDefaultFireauthManager();
+  var listener1 = goog.testing.recordFunction();
+  var listener2 = goog.testing.recordFunction();
+
+  // Add listeners.
+  manager.addStorageListener(listener1);
+  manager.addStorageListener(listener2);
+  clock.tick(800);
+  clock.tick(800);
+  clock.tick(800);
+  db.store['firebaseLocalStorage'] = {
+    'key1': {'fbase_key': 'key1', 'value': 1},
+    'key2': {'fbase_key': 'key2', 'value': 2}
+  };
+  clock.tick(800);
+  assertEquals(1, listener1.getCallCount());
+  assertEquals(1, listener2.getCallCount());
+  assertArrayEquals(
+      ['key1', 'key2'], listener1.getLastCall().getArgument(0));
+  assertArrayEquals(
+      ['key1', 'key2'], listener2.getLastCall().getArgument(0));
+}
+
+
+function testStopListeners() {
+  manager = getDefaultFireauthManager();
+  var listener1 = goog.testing.recordFunction();
+  var listener2 = goog.testing.recordFunction();
+  var listener3 = goog.testing.recordFunction();
+  // Add listeners.
+  manager.addStorageListener(listener1);
+  manager.addStorageListener(listener2);
+  manager.addStorageListener(listener3);
+  clock.tick(800);
+  clock.tick(800);
+  clock.tick(800);
+  // Remove all but listener3.
+  manager.removeStorageListener(listener1);
+  manager.removeStorageListener(listener2);
+  db.store['firebaseLocalStorage'] = {
+    'key1': {'fbase_key': 'key1', 'value': 1},
+    'key2': {'fbase_key': 'key2', 'value': 2}
+  };
+  clock.tick(800);
+  // Only listener3 should be called.
+  assertEquals(0, listener1.getCallCount());
+  assertEquals(0, listener2.getCallCount());
+  assertEquals(1, listener3.getCallCount());
+  assertArrayEquals(
+      ['key1', 'key2'], listener3.getLastCall().getArgument(0));
+}
---
+++ packages/auth/test/token_test.js
@@ -0,0 +1,409 @@
+/**
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Tests for token.js
+ */
+
+goog.provide('fireauth.StsTokenManagerTest');
+
+goog.require('fireauth.AuthError');
+goog.require('fireauth.RpcHandler');
+goog.require('fireauth.StsTokenManager');
+goog.require('fireauth.authenum.Error');
+goog.require('goog.Promise');
+goog.require('goog.testing.AsyncTestCase');
+goog.require('goog.testing.PropertyReplacer');
+goog.require('goog.testing.jsunit');
+
+goog.setTestOnly('fireauth.StsTokenManagerTest');
+
+
+var token = null;
+var rpcHandler = null;
+var stubs = new goog.testing.PropertyReplacer();
+var asyncTestCase = goog.testing.AsyncTestCase.createAndInstall();
+var now = 1449534145526;
+
+
+function setUp() {
+  // Override goog.now().
+  stubs.replace(
+      goog,
+      'now',
+      function() {
+        return now;
+      });
+  // Initialize RPC handler and token.
+  rpcHandler = new fireauth.RpcHandler(
+      'apiKey',
+      {
+        'tokenEndpoint': 'https://securetoken.googleapis.com/v1/token',
+        'tokenTimeout': 10000,
+        'tokenHeaders': {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      });
+  token = new fireauth.StsTokenManager(rpcHandler);
+}
+
+
+function tearDown() {
+  // Reset property replacer, token and RPC handler.
+  token = null;
+  rpcHandler = null;
+  stubs.reset();
+}
+
+
+/**
+ * Helper function to check RPC requestStsToken parameters and simulate a
+ * returned response.
+ * @param {?Object|string} expectedData The expected body data.
+ * @param {?Object} xhrResponse The returned response when no error is returned.
+ * @param {?fireauth.AuthError=} opt_error The specific error returned.
+ */
+function assertRpcHandler(
+    expectedData,
+    xhrResponse,
+    opt_error) {
+  stubs.replace(
+      fireauth.RpcHandler.prototype,
+      'requestStsToken',
+      function(data) {
+        return new goog.Promise(function(resolve, reject) {
+          // Confirm expected data sent.
+          assertObjectEquals(expectedData, data);
+          if (xhrResponse) {
+            // Return expected response.
+            resolve(xhrResponse);
+          } else if (opt_error) {
+            reject(opt_error);
+          } else {
+            reject(
+                new fireauth.AuthError(fireauth.authenum.Error.INTERNAL_ERROR));
+          }
+        });
+      });
+}
+
+
+/**
+ * Asserts that two errors are equivalent. Plain assertObjectEquals cannot be
+ * used as Internet Explorer adds the stack trace as a property of the object.
+ * @param {!Error} expected
+ * @param {!Error} actual
+ */
+function assertErrorEquals(expected, actual) {
+  assertEquals(expected.code, actual.code);
+  assertEquals(expected.message, actual.message);
+}
+
+
+function testStsTokenManager_gettersSetters() {
+  var expirationTime = goog.now() + 3600 * 1000;
+  token.setRefreshToken('refreshToken');
+  token.setAccessToken('accessToken', expirationTime);
+  assertEquals('refreshToken', token.getRefreshToken());
+  assertEquals(expirationTime, token.getExpirationTime());
+}
+
+
+function testParseServerResponse() {
+  var serverResponse = {
+    'idToken': 'myStsAccessToken',
+    'refreshToken': 'myStsRefreshToken',
+    'expiresIn': '3600'
+  };
+  var accessToken = token.parseServerResponse(serverResponse);
+  assertEquals('myStsAccessToken', accessToken);
+  assertEquals('myStsRefreshToken', token.getRefreshToken());
+  assertEquals(now + 3600 * 1000, token.getExpirationTime());
+}
+
+
+function testStsTokenManager_getToken_noToken() {
+  // No token.
+  asyncTestCase.waitForSignals(1);
+  token.getToken().then(function(token) {
+    assertNull(token);
+    asyncTestCase.signal();
+  });
+}
+
+
+function testStsTokenManager_getToken_invalidResponse() {
+  // Test when an network error is returned and then called again successfully
+  // that the network error is not cached.
+  var expectedError =
+      new fireauth.AuthError(fireauth.authenum.Error.NETWORK_REQUEST_FAILED);
+  token.setRefreshToken('myRefreshToken');
+  // Simulate invalid response from server.
+  assertRpcHandler(
+      {
+        'grant_type': 'refresh_token',
+        'refresh_token': 'myRefreshToken'
+      },
+      null,
+      expectedError);
+  asyncTestCase.waitForSignals(1);
+  token.getToken().then(fail, function(error) {
+    // Invalid response error should be triggered.
+    assertErrorEquals(expectedError, error);
+    // Since this is not an expired token error, another call should still
+    // ping the backend.
+    assertRpcHandler(
+      {
+        'grant_type': 'refresh_token',
+        'refresh_token': 'myRefreshToken'
+      },
+      {
+        'access_token': 'accessToken2',
+        'refresh_token': 'refreshToken2',
+        'expires_in': '3600'
+      });
+      token.getToken().then(function(response) {
+        // Confirm all properties updated.
+        assertEquals('accessToken2', response['accessToken']);
+        assertEquals('refreshToken2', response['refreshToken']);
+        assertEquals(
+            goog.now() + 3600 * 1000, response['expirationTime']);
+        asyncTestCase.signal();
+      });
+  });
+}
+
+
+function testStsTokenManager_getToken_tokenExpiredError() {
+  // Test when expired refresh token error is returned.
+  // Simulate Id token is expired to force refresh.
+  var expirationTime = goog.now() - 3600 * 1000;
+  // Expected token expired error.
+  var expectedError =
+      new fireauth.AuthError(fireauth.authenum.Error.TOKEN_EXPIRED);
+  token.setAccessToken('accessToken', expirationTime);
+  token.setRefreshToken('myRefreshToken');
+  assertFalse(token.isRefreshTokenExpired());
+  // Simulate token expired error from server.
+  assertRpcHandler(
+      {
+        'grant_type': 'refresh_token',
+        'refresh_token': 'myRefreshToken'
+      },
+      null,
+      expectedError);
+  asyncTestCase.waitForSignals(4);
+  // This call will return token expired error.
+  token.getToken().then(fail, function(error) {
+    assertTrue(token.isRefreshTokenExpired());
+    // If another RPC is sent, it will resolve with valid STS token.
+    // This should not happen since the token expired error is cached.
+    assertRpcHandler(
+      {
+        'grant_type': 'refresh_token',
+        'refresh_token': 'refreshToken2'
+      },
+      {
+        'access_token': 'accessToken2',
+        'refresh_token': 'refreshToken2',
+        'expires_in': '3600'
+      });
+    // Token expired error should be thrown.
+    assertErrorEquals(expectedError, error);
+    // Try again, cached expired token error should be triggered.
+    token.getToken(false).thenCatch(function(error) {
+      assertErrorEquals(expectedError, error);
+      asyncTestCase.signal();
+    });
+    // Try again with forced refresh, cached expired token error should be
+    // triggered.
+    token.getToken(true).thenCatch(function(error) {
+      assertErrorEquals(expectedError, error);
+      asyncTestCase.signal();
+    });
+    // Plain object should have refresh token reset.
+    assertObjectEquals(
+        {
+          'apiKey': 'apiKey',
+          'refreshToken': null,
+          'accessToken': 'accessToken',
+          'expirationTime': expirationTime
+        },
+        token.toPlainObject());
+    // If the refresh token is manually updated, the cached error should be
+    // cleared.
+    token.setRefreshToken('refreshToken2');
+    // This should now resolve.
+    token.getToken().then(function(response) {
+      assertFalse(token.isRefreshTokenExpired());
+      // Confirm all properties updated.
+      assertEquals('accessToken2', response['accessToken']);
+      assertEquals('refreshToken2', response['refreshToken']);
+      assertEquals(
+          goog.now() + 3600 * 1000, response['expirationTime']);
+      // Plain object should have the new refresh token set.
+      assertObjectEquals(
+          {
+            'apiKey': 'apiKey',
+            'refreshToken': 'refreshToken2',
+            'accessToken': 'accessToken2',
+            'expirationTime': goog.now() + 3600 * 1000
+          },
+          token.toPlainObject());
+      asyncTestCase.signal();
+    });
+    asyncTestCase.signal();
+  });
+}
+
+
+function testStsTokenManager_getToken_exchangeRefreshToken() {
+  // Set a previously cached access token that is expired.
+  var expirationTime = goog.now() - 3600;
+  token.setRefreshToken('refreshToken');
+  // Expired access token.
+  token.setAccessToken('expiredAccessToken', expirationTime);
+  // It will attempt to exchange refresh token for STS token.
+  assertRpcHandler(
+      {
+        'grant_type': 'refresh_token',
+        'refresh_token': 'refreshToken'
+      },
+      {
+        'access_token': 'accessToken2',
+        'refresh_token': 'refreshToken2',
+        'expires_in': '3600'
+      });
+  asyncTestCase.waitForSignals(1);
+  token.getToken().then(function(response) {
+    // Confirm all properties updated.
+    assertEquals('accessToken2', token.accessToken_);
+    assertEquals('refreshToken2', token.getRefreshToken());
+    assertEquals(
+        goog.now() + 3600 * 1000, token.getExpirationTime());
+    // Confirm correct STS response.
+    assertObjectEquals(
+        {
+          'accessToken': 'accessToken2',
+          'expirationTime': goog.now() + 3600 * 1000,
+          'refreshToken': 'refreshToken2'
+        },
+        response);
+    asyncTestCase.signal();
+  });
+}
+
+
+function testStsTokenManager_getToken_cached() {
+  // Set a previously cached access token that hasn't expired yet.
+  var expirationTime = goog.now() + 60 * 1000;
+  // Set refresh token and unexpired access token.
+  // No XHR request needed.
+  token.setRefreshToken('refreshToken');
+  token.setAccessToken('accessToken', expirationTime);
+  asyncTestCase.waitForSignals(1);
+  token.getToken().then(function(response) {
+    // Confirm all properties updated.
+    assertEquals('accessToken', token.accessToken_);
+    assertEquals('refreshToken', token.getRefreshToken());
+    assertEquals(
+        expirationTime, token.getExpirationTime());
+    // Confirm correct STS response.
+    assertObjectEquals(
+        {
+          'accessToken': 'accessToken',
+          'expirationTime': expirationTime,
+          'refreshToken': 'refreshToken'
+        },
+        response);
+    asyncTestCase.signal();
+  });
+}
+
+
+function testStsTokenManager_getToken_forceRefresh() {
+  // Set a previously cached access token that hasn't expired yet.
+  var expirationTime = goog.now() + 1000;
+  // Set ID token, refresh token and unexpired access token.
+  token.setRefreshToken('refreshToken');
+  token.setAccessToken('accessToken', expirationTime);
+  // Even though unexpired access token, it will attempt to exchange for refresh
+  // token since force refresh is set to true.
+  assertRpcHandler(
+      {
+        'grant_type': 'refresh_token',
+        'refresh_token': 'refreshToken'
+      },
+      {
+        'access_token': 'accessToken2',
+        'refresh_token': 'refreshToken2',
+        'expires_in': '3600'
+      });
+  asyncTestCase.waitForSignals(1);
+  token.getToken(true).then(function(response) {
+    // Confirm all properties updated.
+    assertEquals('accessToken2', token.accessToken_);
+    assertEquals('refreshToken2', token.getRefreshToken());
+    assertEquals(
+        goog.now() + 3600 * 1000, token.getExpirationTime());
+    // Confirm correct STS response.
+    assertObjectEquals(
+        {
+          'accessToken': 'accessToken2',
+          'expirationTime': goog.now() + 3600 * 1000,
+          'refreshToken': 'refreshToken2'
+        },
+        response);
+    asyncTestCase.signal();
+  });
+}
+
+
+function testToPlainObject() {
+  var expirationTime = goog.now() + 3600 * 1000;
+  token.setRefreshToken('refreshToken');
+  token.setAccessToken('accessToken', expirationTime);
+  assertObjectEquals(
+      {
+        'apiKey': 'apiKey',
+        'refreshToken': 'refreshToken',
+        'accessToken': 'accessToken',
+        'expirationTime': expirationTime
+      },
+      token.toPlainObject());
+}
+
+
+function testFromPlainObject() {
+  var expirationTime = goog.now() + 3600 * 1000;
+  assertNull(
+      fireauth.StsTokenManager.fromPlainObject(
+          new fireauth.RpcHandler('apiKey'),
+          {}));
+
+  token.setRefreshToken('refreshToken');
+  token.setAccessToken('accessToken', expirationTime);
+  assertObjectEquals(
+      token,
+      fireauth.StsTokenManager.fromPlainObject(
+          rpcHandler,
+          {
+            'apiKey': 'apiKey',
+            'refreshToken': 'refreshToken',
+            'accessToken': 'accessToken',
+            'expirationTime': expirationTime
+          }));
+}
