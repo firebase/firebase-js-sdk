88e8733a9a40aa3aab4288dbaea5f1707e67226b
Ryan P. Brewster <RyanPBrewster@gmail.com>
2017-12-08 20:54:44 -0800

    [AUTOMATED]: Prettier Code Styling

--- packages/util/src/crypt.ts
+++ packages/util/src/crypt.ts
@@ -16,7 +16,8 @@
 
 const stringToByteArray = function(str) {
   // TODO(user): Use native implementations if/when available
-  var out = [], p = 0;
+  var out = [],
+    p = 0;
   for (var i = 0; i < str.length; i++) {
     var c = str.charCodeAt(i);
     if (c < 128) {
@@ -25,10 +26,12 @@ const stringToByteArray = function(str) {
       out[p++] = (c >> 6) | 192;
       out[p++] = (c & 63) | 128;
     } else if (
-        ((c & 0xFC00) == 0xD800) && (i + 1) < str.length &&
-        ((str.charCodeAt(i + 1) & 0xFC00) == 0xDC00)) {
+      (c & 0xfc00) == 0xd800 &&
+      i + 1 < str.length &&
+      (str.charCodeAt(i + 1) & 0xfc00) == 0xdc00
+    ) {
       // Surrogate Pair
-      c = 0x10000 + ((c & 0x03FF) << 10) + (str.charCodeAt(++i) & 0x03FF);
+      c = 0x10000 + ((c & 0x03ff) << 10) + (str.charCodeAt(++i) & 0x03ff);
       out[p++] = (c >> 18) | 240;
       out[p++] = ((c >> 12) & 63) | 128;
       out[p++] = ((c >> 6) & 63) | 128;
@@ -50,28 +53,32 @@ const stringToByteArray = function(str) {
  */
 const byteArrayToString = function(bytes) {
   // TODO(user): Use native implementations if/when available
-  var out = [], pos = 0, c = 0;
+  var out = [],
+    pos = 0,
+    c = 0;
   while (pos < bytes.length) {
     var c1 = bytes[pos++];
     if (c1 < 128) {
       out[c++] = String.fromCharCode(c1);
     } else if (c1 > 191 && c1 < 224) {
       var c2 = bytes[pos++];
-      out[c++] = String.fromCharCode((c1 & 31) << 6 | c2 & 63);
+      out[c++] = String.fromCharCode(((c1 & 31) << 6) | (c2 & 63));
     } else if (c1 > 239 && c1 < 365) {
       // Surrogate Pair
       var c2 = bytes[pos++];
       var c3 = bytes[pos++];
       var c4 = bytes[pos++];
-      var u = ((c1 & 7) << 18 | (c2 & 63) << 12 | (c3 & 63) << 6 | c4 & 63) -
-          0x10000;
-      out[c++] = String.fromCharCode(0xD800 + (u >> 10));
-      out[c++] = String.fromCharCode(0xDC00 + (u & 1023));
+      var u =
+        (((c1 & 7) << 18) | ((c2 & 63) << 12) | ((c3 & 63) << 6) | (c4 & 63)) -
+        0x10000;
+      out[c++] = String.fromCharCode(0xd800 + (u >> 10));
+      out[c++] = String.fromCharCode(0xdc00 + (u & 1023));
     } else {
       var c2 = bytes[pos++];
       var c3 = bytes[pos++];
-      out[c++] =
-          String.fromCharCode((c1 & 15) << 12 | (c2 & 63) << 6 | c3 & 63);
+      out[c++] = String.fromCharCode(
+        ((c1 & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63)
+      );
     }
   }
   return out.join('');
