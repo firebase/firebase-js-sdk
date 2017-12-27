de3ddd25e7916513c8ef0abea0b1e20004af2b4a
Josh Crowther <jshcrowthe@gmail.com>
2017-12-27 10:49:45 -0800

    [AUTOMATED]: Prettier Code Styling

--- packages/database/index.node.ts
+++ packages/database/index.node.ts
@@ -91,7 +91,7 @@ declare module '@firebase/app-types' {
       (app?: FirebaseApp): types.FirebaseDatabase;
       enableLogging: typeof types.enableLogging;
       ServerValue: types.ServerValue;
-      Database: typeof types.FirebaseDatabase
+      Database: typeof types.FirebaseDatabase;
     };
   }
   interface FirebaseApp {
