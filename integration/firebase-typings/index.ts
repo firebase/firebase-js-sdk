aa4b65c2cbe1886ccdf54dae9284d20f4cc994ba
Josh Crowther <jshcrowthe@gmail.com>
2017-12-26 16:41:38 -1000

    Add a simple test to validate that the typings are properly exported

---
+++ integration/firebase-typings/index.ts
@@ -0,0 +1,12 @@
+import * as firebase from "firebase";
+
+/**
+ * Verifying the namespace types are properly exposed from the `firebase`
+ * package
+ */
+let app: firebase.app.App;
+let database: firebase.database.Database;
+let firestore: firebase.firestore.Firestore;
+let messaging: firebase.messaging.Messaging;
+let storage: firebase.storage.Storage;
+
