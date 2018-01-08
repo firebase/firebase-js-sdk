fc2562a77784a09aeb54c4c1d75745d5f630d4f0
Josh Crowther <jshcrowthe@gmail.com>
2018-01-08 13:48:28 -0500

    Partial Typings Revert (#401)
    * Partial revert of a6b6689 to fix wrapper package typings
    
    * Add helper to setup links to all packages
    
    * [AUTOMATED]: Prettier Code Styling
    
    * Add a simple test to validate that the typings are properly exported
    
    * [AUTOMATED]: Prettier Code Styling
    
    * [AUTOMATED]: License Headers
    
    * Fix database typings issue
    
    * [AUTOMATED]: Prettier Code Styling
    
    * Fixing nit from @hiranya911

--- integration/firebase-typings/index.ts
+++ integration/firebase-typings/index.ts
@@ -14,4 +14,14 @@
  * limitations under the License.
  */
 
-import '@firebase/storage';
+import * as firebase from 'firebase';
+
+/**
+ * Verifying the namespace types are properly exposed from the `firebase`
+ * package
+ */
+let app: firebase.app.App;
+let database: firebase.database.Database;
+let firestore: firebase.firestore.Firestore;
+let messaging: firebase.messaging.Messaging;
+let storage: firebase.storage.Storage;
