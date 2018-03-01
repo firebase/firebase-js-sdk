9111376b21cbd6d67f750b021d4614e984af09f4
Sebastian Schmidt <mrschmidt@google.com>
2018-02-28 18:02:58 -0800

    Adding assert to AsyncQueue

--- packages/firestore/src/util/async_queue.ts
+++ packages/firestore/src/util/async_queue.ts
@@ -218,6 +218,9 @@ export class AsyncQueue {
   ): CancelablePromise<T> {
     this.verifyNotFailed();
 
+    assert(delayMs >= 0,
+        `Attempted to schedule an operation with a negative delay of ${delayMs}`);
+
     // While not necessarily harmful, we currently don't expect to have multiple
     // ops with the same timer id in the queue, so defensively reject them.
     assert(
