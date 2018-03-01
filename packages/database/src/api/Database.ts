dbed6899923aceedb87860a1381b478d8d9880c4
Sebastian Schmidt <mrschmidt@google.com>
2018-03-01 09:45:46 -0800

    Database API: Ref from Ref (#534)

--- packages/database/src/api/Database.ts
+++ packages/database/src/api/Database.ts
@@ -62,15 +62,26 @@ export class Database implements FirebaseService {
   }
 
   /**
-   * Returns a reference to the root or the path specified in opt_pathString.
-   * @param {string=} pathString
+   * Returns a reference to the root or to the path specified in the provided
+   * argument.
+
+   * @param {string|Reference=} path The relative string path or an existing
+   * Reference to a database location.
+   * @throws If a Reference is provided, throws if it does not belong to the
+   * same project.
    * @return {!Reference} Firebase reference.
-   */
-  ref(pathString?: string): Reference {
+   **/
+  ref(path?: string): Reference;
+  ref(path?: Reference): Reference;
+  ref(path?: string | Reference): Reference {
     this.checkDeleted_('ref');
     validateArgCount('database.ref', 0, 1, arguments.length);
 
-    return pathString !== undefined ? this.root_.child(pathString) : this.root_;
+    if (path instanceof Reference) {
+      return this.refFromURL(path.toString());
+    }
+
+    return path !== undefined ? this.root_.child(path) : this.root_;
   }
 
   /**
