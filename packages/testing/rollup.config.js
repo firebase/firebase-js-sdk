016b8707ae5566af37fac8ea1ce9d266b4ac250e
Josh Crowther <jshcrowthe@gmail.com>
2018-04-09 09:30:24 -0700

    @firebase/testing Build Refactor

---
+++ packages/testing/rollup.config.js
@@ -0,0 +1,24 @@
+import resolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
+import pkg from './package.json';
+
+const plugins = [
+  typescript({
+    typescript: require('typescript')
+  }),
+  resolve(),
+  commonjs(),
+];
+
+const external = Object.keys(
+  Object.assign({}, pkg.peerDependencies, pkg.dependencies)
+);
+export default {
+  input: 'index.ts',
+  output: [
+    { file: pkg.main, format: 'cjs' }
+  ],
+  plugins,
+  external,
+};
