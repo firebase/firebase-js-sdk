741f98af4c07e0ca333a5bd664e5b7614f9ae4b6
Josh Crowther <jshcrowthe@gmail.com>
2018-04-09 09:53:27 -0700

    @firebase/functions Build Refactor

---
+++ packages/functions/rollup.config.js
@@ -0,0 +1,41 @@
+import resolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
+import pkg from './package.json';
+
+const plugins = [
+  typescript({
+    typescript: require('typescript')
+  }),
+  resolve(),
+  commonjs(),
+];
+
+const external = Object.keys(
+  Object.assign({}, pkg.peerDependencies, pkg.dependencies)
+);
+export default [
+  /**
+   * Browser Builds
+   */
+  {
+    input: 'index.ts',
+    output: [
+      { file: pkg.browser, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins,
+    external,
+  },
+  /**
+   * Node.js Build
+   */
+  {
+    input: 'index.node.ts',
+    output: [
+      { file: pkg.main, format: 'cjs' }
+    ],
+    plugins,
+    external,
+  }
+];
