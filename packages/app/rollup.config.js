2a9bdf981390aad871d987cd9c52cd35b4bd57e2
Josh Crowther <jshcrowthe@gmail.com>
2018-04-09 10:13:52 -0700

    @firebase/app version replace

--- packages/app/rollup.config.js
+++ packages/app/rollup.config.js
@@ -1,11 +1,20 @@
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
 import pkg from './package.json';
+
+import firebasePkg from '../firebase/package.json'
 
 const plugins = [
   typescript({
     typescript: require('typescript')
+  }),
+  replace({
+    delimiters: ['${', '}'],
+    values: {
+      JSCORE_VERSION: firebasePkg.version
+    }
   }),
   resolve(),
   commonjs(),
