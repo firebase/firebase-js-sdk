14c2c4a33b98691e6eef7c511a08b239471569b9
Josh Crowther <jshcrowthe+github@gmail.com>
2018-04-10 16:05:28 -0700

    Build Refactoring (#653)
    * Removing gulp dependency from the default build path
    
    * Updating .gitignore
    
    * @firebase/util Build Refactor
    
    * @firebase/testing Build Refactor
    
    * @firebase/template Build Refactor
    
    * @firebase/storage Build Refactor
    
    * @firebase/polyfill Build Refactor
    
    * @firebase/messaging Build Refactor
    
    * @firebase/logger Build Refactor
    
    * @firebase/functions Build Refactor
    
    * @firebase/database Build Refactor
    
    * @firebase/app Build Refactor
    
    * @firebase/firestore Build Refactor
    
    * @firebase/app version replace
    
    * Fix script rename in @firebase/util
    
    * [AUTOMATED]: Prettier Code Styling
    
    * [AUTOMATED]: License Headers
    
    * Remove unnecessary build artifacts
    
    * Removing unused test files from types-* packages
    
    * [AUTOMATED]: Prettier Code Styling
    
    * firebase Package Build Refactor
    
    * [AUTOMATED]: Prettier Code Styling
    
    * Refactor peerDependencies to properly reference the entire major 0
    
    * Change NPM Publish Concurrency
    
    * Add sourcemaps
    
    * Refactor messaging named export
    
    * Add try-catch block for firebase-app error
    
    * Add package-lock.json to the .gitignore
    
    * Adding a package.json to remove duplicate .d.ts

--- packages/testing/rollup.config.js
+++ packages/testing/rollup.config.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017 Google Inc.
+ * Copyright 2018 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,5 +14,25 @@
  * limitations under the License.
  */
 
-import { firebase } from '@firebase/app';
-import '@firebase/functions';
+import resolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
+import pkg from './package.json';
+
+const plugins = [
+  typescript({
+    typescript: require('typescript')
+  }),
+  resolve(),
+  commonjs()
+];
+
+const external = Object.keys(
+  Object.assign({}, pkg.peerDependencies, pkg.dependencies)
+);
+export default {
+  input: 'index.ts',
+  output: [{ file: pkg.main, format: 'cjs' }],
+  plugins,
+  external
+};
