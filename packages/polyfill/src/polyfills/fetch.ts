0043fed3a3e30fc0f6e0af79b77d86dfa671efb2
Josh Crowther <jshcrowthe+github@gmail.com>
2018-04-10 17:05:58 -0700

    Porting Polyfills Fix (missing in master) (#668)
    * Fix polyfills for fetch
    
    * [AUTOMATED]: Prettier Code Styling
    
    * [AUTOMATED]: License Headers
    
    * Removing legacy file

--- packages/polyfill/src/polyfills/fetch.ts
+++ packages/polyfill/src/polyfills/fetch.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017 Google Inc.
+ * Copyright 2018 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,10 +14,9 @@
  * limitations under the License.
  */
 
+import './promise';
 import { global } from './global';
 
-// Polyfill Promise
-if (typeof Promise === 'undefined') {
-  // HACK: TS throws an error if I attempt to use 'dot-notation'
-  global['Promise'] = require('promise-polyfill') as PromiseConstructor;
-}
+(self => {
+  require('whatwg-fetch');
+})(global);
