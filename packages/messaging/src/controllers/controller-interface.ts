e91742db40332061e13cdb6245d38bebed346c71
Mertcan Mermerkaya <mertcan@mermerkaya.net>
2018-05-09 16:50:31 +0100

    Refresh token instead of breaking if VAPID key doesn't exist (#794)

--- packages/messaging/src/controllers/controller-interface.ts
+++ packages/messaging/src/controllers/controller-interface.ts
@@ -371,6 +371,7 @@ function isTokenStillValid(
   tokenDetails: TokenDetails
 ): boolean {
   if (
+    !tokenDetails.vapidKey ||
     !isArrayBufferEqual(publicVapidKey.buffer, tokenDetails.vapidKey.buffer)
   ) {
     return false;
