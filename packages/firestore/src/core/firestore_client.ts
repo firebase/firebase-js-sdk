259d343b6f410e50703d6e7fa559f17659777106
Sebastian Schmidt <mrschmidt@google.com>
2018-05-18 10:43:32 -0700

    [AUTOMATED]: Prettier Code Styling

--- packages/firestore/src/core/firestore_client.ts
+++ packages/firestore/src/core/firestore_client.ts
@@ -62,7 +62,7 @@ const DOM_EXCEPTION_ABORTED = 22;
 /** The DOMException code for quota exceeded. */
 const DOM_EXCEPTION_QUOTA_EXCEEDED = 22;
 
-    /**
+/**
  * FirestoreClient is a top-level class that constructs and owns all of the
  * pieces of the client SDK architecture. It is responsible for creating the
  * async queue that is shared by all of the other components in the system.
@@ -247,8 +247,8 @@ export class FirestoreClient {
       error instanceof DOMException
     ) {
       return (
-          error.code === DOM_EXCEPTION_QUOTA_EXCEEDED ||
-          error.code === DOM_EXCEPTION_ABORTED
+        error.code === DOM_EXCEPTION_QUOTA_EXCEEDED ||
+        error.code === DOM_EXCEPTION_ABORTED
       );
     }
 
