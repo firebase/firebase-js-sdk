62f1481fc3f3c1aca09955d954dfdd2b496e36ad
Josh Crowther <jshcrowthe@gmail.com>
2018-05-22 16:06:09 -0700

    Refactor @firebase/firestore registration

--- packages/firestore/src/platform/config.ts
+++ packages/firestore/src/platform/config.ts
@@ -14,10 +14,8 @@
  * limitations under the License.
  */
 
-import { FirebaseApp, FirebaseNamespace } from '@firebase/app-types';
-import { _FirebaseNamespace } from '@firebase/app-types/private';
-import { PublicBlob } from '../api/blob';
-import {
+export { PublicBlob } from '../api/blob';
+export {
   Firestore,
   PublicCollectionReference,
   PublicDocumentReference,
@@ -29,52 +27,7 @@ import {
   PublicTransaction,
   PublicWriteBatch
 } from '../api/database';
-import { FieldPath } from '../api/field_path';
-import { PublicFieldValue } from '../api/field_value';
-import { GeoPoint } from '../api/geo_point';
-import { Timestamp } from '../api/timestamp';
-import { shallowCopy } from '../util/obj';
-
-const firestoreNamespace = {
-  Firestore: PublicFirestore,
-  GeoPoint,
-  Timestamp,
-  Blob: PublicBlob,
-  Transaction: PublicTransaction,
-  WriteBatch: PublicWriteBatch,
-  DocumentReference: PublicDocumentReference,
-  DocumentSnapshot: PublicDocumentSnapshot,
-  Query: PublicQuery,
-  QueryDocumentSnapshot: PublicQueryDocumentSnapshot,
-  QuerySnapshot: PublicQuerySnapshot,
-  CollectionReference: PublicCollectionReference,
-  FieldPath,
-  FieldValue: PublicFieldValue,
-  setLogLevel: Firestore.setLogLevel
-};
-
-/**
- * Configures Firestore as part of the Firebase SDK by calling registerService.
- */
-export function configureForFirebase(firebase: FirebaseNamespace): void {
-  (firebase as _FirebaseNamespace).INTERNAL.registerService(
-    'firestore',
-    (app: FirebaseApp) => new Firestore(app),
-    shallowCopy(firestoreNamespace)
-  );
-}
-
-/**
- * Exports the Firestore namespace into the provided `exportObject` object under
- * the key 'firestore'. This is used for wrapped binary that exposes Firestore
- * as a goog module.
- */
-export function configureForStandalone(exportObject: {
-  [key: string]: {};
-}): void {
-  const copiedNamespace = shallowCopy(firestoreNamespace);
-  // Unlike the use with Firebase, the standalone allows the use of the
-  // constructor, so export it's internal class
-  copiedNamespace['Firestore'] = Firestore;
-  exportObject['firestore'] = copiedNamespace;
-}
+export { FieldPath } from '../api/field_path';
+export { PublicFieldValue } from '../api/field_value';
+export { GeoPoint } from '../api/geo_point';
+export { Timestamp } from '../api/timestamp';
