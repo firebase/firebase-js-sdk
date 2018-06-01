af4a60169423162081488ccbe074ab87a550bba9
David East <deast@google.com>
2018-06-01 13:08:19 -0600

    testing config. storage observables.

--- packages/rxfire/storage/index.ts
+++ packages/rxfire/storage/index.ts
@@ -1,5 +1,6 @@
 import { storage } from 'firebase/app';
-import { Observable } from 'rxjs';
+import { Observable, from } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 export function fromTask(task: storage.UploadTask) {
   return new Observable<storage.UploadTaskSnapshot>(subscriber => {
@@ -9,4 +10,31 @@ export function fromTask(task: storage.UploadTask) {
     task.on('state_changed', progress, error, complete);
     return () => task.cancel();
   });
+}
+
+export function getDownloadURL(ref: storage.Reference) {
+  return from(ref.getDownloadURL());
+}
+
+export function getMetadata(ref: storage.Reference) {
+  return from(ref.getMetadata());
+}
+
+export function put(
+  ref: storage.Reference, data: any, metadata?: storage.UploadMetadata
+) {
+  return fromTask(ref.put(data, metadata));
+}
+
+export function putString(
+  ref: storage.Reference, data: string, format?: storage.StringFormat, 
+  metadata?: storage.UploadMetadata
+) {
+  return fromTask(ref.put(data, metadata));
+}
+
+export function percentage(task: storage.UploadTask) {
+  return fromTask(task).pipe(
+    map(s => s.bytesTransferred / s.totalBytes * 100)
+  );
 }
---
+++ packages/rxfire/test/index.test.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+// import { spy } from 'sinon';
+
+describe('RxFire Auth', () => {
+
+  it('should be true', () => {
+    expect(true).to.equal(true);
+  });
+
+});
