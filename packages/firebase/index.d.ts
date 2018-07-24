f73452aec1264f027421e2f8bb1fdb1fdcf93cd1
Sebastian Schmidt <mrschmidt@google.com>
2018-07-23 18:00:44 -0700

    [AUTOMATED]: Prettier Code Styling

--- packages/firebase/index.d.ts
+++ packages/firebase/index.d.ts
@@ -769,26 +769,26 @@ declare namespace firebase.firestore {
     timestampsInSnapshots?: boolean;
   }
 
-// TODO(multitab): Uncomment when multi-tab is released publicly.
-// /**
-//  * Settings that can be passed to Firestore.enablePersistence() to configure
-//  * Firestore persistence.
-//  */
-// export interface PersistenceSettings {
-//   /**
-//    * Whether to synchronize the in-memory state of multiple tabs. Setting this
-//    * to 'true' in all open tabs enables shared access to local persistence,
-//    * shared execution of queries and latency-compensated local document updates
-//    * across all connected instances.
-//    *
-//    * To enable this mode, `experimentalTabSynchronization:true` needs to be set
-//    * globally in all active tabs. If omitted or set to 'false',
-//    * `enablePersistence()` will fail in all but the first tab.
-//    *
-//    * NOTE: This mode is not yet recommended for production use.
-//    */
-//   experimentalTabSynchronization?: boolean;
-// }
+  // TODO(multitab): Uncomment when multi-tab is released publicly.
+  // /**
+  //  * Settings that can be passed to Firestore.enablePersistence() to configure
+  //  * Firestore persistence.
+  //  */
+  // export interface PersistenceSettings {
+  //   /**
+  //    * Whether to synchronize the in-memory state of multiple tabs. Setting this
+  //    * to 'true' in all open tabs enables shared access to local persistence,
+  //    * shared execution of queries and latency-compensated local document updates
+  //    * across all connected instances.
+  //    *
+  //    * To enable this mode, `experimentalTabSynchronization:true` needs to be set
+  //    * globally in all active tabs. If omitted or set to 'false',
+  //    * `enablePersistence()` will fail in all but the first tab.
+  //    *
+  //    * NOTE: This mode is not yet recommended for production use.
+  //    */
+  //   experimentalTabSynchronization?: boolean;
+  // }
 
   export type LogLevel = 'debug' | 'error' | 'silent';
 
