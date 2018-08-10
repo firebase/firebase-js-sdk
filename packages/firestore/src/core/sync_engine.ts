d21071e913405533194504fceebad29cd5b05e61
Sebastian Schmidt <mrschmidt@google.com>
2018-08-09 19:55:41 -0700

    Garbage Collect Mutation Batch Keys (#1106)

--- packages/firestore/src/core/sync_engine.ts
+++ packages/firestore/src/core/sync_engine.ts
@@ -852,6 +852,7 @@ export class SyncEngine implements RemoteSyncer, SharedClientStateSyncer {
     return this.localStore
       .handleUserChange(user)
       .then(result => {
+        // TODO(multitab): Consider calling this only in the primary tab.
         this.sharedClientState.handleUserChange(
           user,
           result.removedBatchIds,
