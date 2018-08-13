89d46be765feb7c4c6576985dd0fa707a1bfbe97
Sebastian Schmidt <mrschmidt@google.com>
2018-08-13 11:13:29 -0700

    Using mutable spec runner (#1114)

--- packages/firestore/test/unit/specs/perf_spec.test.ts
+++ packages/firestore/test/unit/specs/perf_spec.test.ts
@@ -28,9 +28,9 @@ describeSpec(
   ['benchmark'],
   () => {
     specTest('Insert a new document', [], () => {
-      let steps = spec().withGCEnabled(false);
+      const steps = spec().withGCEnabled(false);
       for (let i = 0; i < STEP_COUNT; ++i) {
-        steps = steps
+        steps
           .userSets(`collection/{i}`, { doc: i })
           .writeAcks(`collection/{i}`, i);
       }
@@ -42,7 +42,7 @@ describeSpec(
       [],
       () => {
         let currentVersion = 1;
-        let steps = spec().withGCEnabled(false);
+        const steps = spec().withGCEnabled(false);
 
         for (let i = 0; i < STEP_COUNT; ++i) {
           const query = Query.atPath(path(`collection/${i}`));
@@ -56,7 +56,7 @@ describeSpec(
             doc: i
           });
 
-          steps = steps
+          steps
             .userListens(query)
             .userSets(`collection/${i}`, { doc: i })
             .expectEvents(query, {
@@ -78,9 +78,7 @@ describeSpec(
       const cachedDocumentCount = 100;
 
       const query = Query.atPath(path(`collection`)).addOrderBy(orderBy('v'));
-
-      let steps = spec().withGCEnabled(false);
-
+      const steps = spec().withGCEnabled(false);
       const docs = [];
 
       for (let i = 0; i < cachedDocumentCount; ++i) {
@@ -91,7 +89,7 @@ describeSpec(
       }
 
       for (let i = 1; i <= STEP_COUNT; ++i) {
-        steps = steps
+        steps
           .userListens(query)
           .expectEvents(query, {
             added: docs,
@@ -105,10 +103,12 @@ describeSpec(
     });
 
     specTest('Update a single document', [], () => {
-      let steps = spec().withGCEnabled(false);
-      steps = steps.userSets(`collection/doc`, { v: 0 });
+      const steps = spec()
+        .withGCEnabled(false)
+        .userSets(`collection/doc`, { v: 0 });
+
       for (let i = 1; i <= STEP_COUNT; ++i) {
-        steps = steps
+        steps
           .userPatches(`collection/doc`, { v: i })
           .writeAcks(`collection/doc`, i);
       }
@@ -122,7 +122,7 @@ describeSpec(
         const query = Query.atPath(path(`collection/doc`));
 
         let currentVersion = 1;
-        let steps = spec().withGCEnabled(false);
+        const steps = spec().withGCEnabled(false);
 
         let docLocal = doc(
           `collection/doc`,
@@ -133,7 +133,7 @@ describeSpec(
         let docRemote = doc(`collection/doc`, ++currentVersion, { v: 0 });
         let lastRemoteVersion = currentVersion;
 
-        steps = steps
+        steps
           .userListens(query)
           .userSets(`collection/doc`, { v: 0 })
           .expectEvents(query, {
@@ -155,7 +155,7 @@ describeSpec(
           docRemote = doc(`collection/doc`, ++currentVersion, { v: i });
           lastRemoteVersion = currentVersion;
 
-          steps = steps
+          steps
             .userPatches(`collection/doc`, { v: i })
             .expectEvents(query, {
               modified: [docLocal],
@@ -177,11 +177,11 @@ describeSpec(
         const documentsPerStep = 100;
 
         const query = Query.atPath(path(`collection`)).addOrderBy(orderBy('v'));
+        const steps = spec().withGCEnabled(false);
 
         let currentVersion = 1;
-        let steps = spec().withGCEnabled(false);
 
-        steps = steps
+        steps
           .userListens(query)
           .watchAcksFull(query, currentVersion)
           .expectEvents(query, {});
@@ -197,7 +197,7 @@ describeSpec(
 
           const changeType = i === 1 ? 'added' : 'modified';
 
-          steps = steps
+          steps
             .watchSends({ affects: [query] }, ...docs)
             .watchSnapshots(++currentVersion)
             .expectEvents(query, { [changeType]: docs });
@@ -214,7 +214,7 @@ describeSpec(
         const documentsPerStep = 100;
 
         let currentVersion = 1;
-        let steps = spec().withGCEnabled(false);
+        const steps = spec().withGCEnabled(false);
 
         for (let i = 1; i <= STEP_COUNT; ++i) {
           const collPath = `collection/${i}/coll`;
@@ -225,7 +225,7 @@ describeSpec(
             docs.push(doc(`${collPath}/${j}`, ++currentVersion, { v: j }));
           }
 
-          steps = steps
+          steps
             .userListens(query)
             .watchAcksFull(query, ++currentVersion, ...docs)
             .expectEvents(query, { added: docs })
@@ -247,7 +247,7 @@ describeSpec(
       const queriesPerStep = 25;
 
       let currentVersion = 1;
-      let steps = spec().withGCEnabled(false);
+      const steps = spec().withGCEnabled(false);
 
       for (let i = 1; i <= STEP_COUNT; ++i) {
         // We use a different subcollection for each iteration to ensure
@@ -266,16 +266,16 @@ describeSpec(
             filter('val', '<=', j)
           );
           queries.push(query);
-          steps = steps.userListens(query).watchAcks(query);
+          steps.userListens(query).watchAcks(query);
         }
 
-        steps = steps
+        steps
           .watchSends({ affects: queries }, matchingDoc)
           .watchSnapshots(++currentVersion);
 
         // Registers the snapshot expectations with the spec runner.
         for (const query of queries) {
-          steps = steps.expectEvents(query, {
+          steps.expectEvents(query, {
             added: [matchingDoc],
             fromCache: true
           });
@@ -283,7 +283,7 @@ describeSpec(
 
         // Unlisten and clean up the query.
         for (const query of queries) {
-          steps = steps.userUnlistens(query).watchRemoves(query);
+          steps.userUnlistens(query).watchRemoves(query);
         }
       }
 
