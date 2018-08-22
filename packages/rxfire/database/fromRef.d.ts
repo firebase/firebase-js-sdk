7f5c38385505e1baf63586326b48c5984c41404c
David East <deast@google.com>
2018-08-22 13:24:51 -0600

    changing peerDeps and rollup config

--- packages/rxfire/database/fromRef.d.ts
+++ packages/rxfire/database/fromRef.d.ts
@@ -21,7 +21,4 @@ import { ListenEvent, QueryChange } from './interfaces';
  * @param ref Database Reference
  * @param event Listen event type ('value', 'added', 'changed', 'removed', 'moved')
  */
-export declare function fromRef(
-  ref: database.Query,
-  event: ListenEvent
-): Observable<QueryChange>;
+export declare function fromRef(ref: database.Query, event: ListenEvent): Observable<QueryChange>;
