45e8ac0053bb8ad71ae0751bdb09795e1d4fc308
Feiyang1 <plane1113@gmail.com>
2018-10-17 15:16:56 -0700

    auth sourcemaps

--- packages/auth/src/exports_auth.js
+++ packages/auth/src/exports_auth.js
@@ -619,6 +619,7 @@ fireauth.exportlib.exportPrototypeMethods(
         ]);
 
     // Register Auth service with firebase.App.
+    debugger;
     firebase.INTERNAL.registerService(
         fireauth.exportlib.AUTH_TYPE,
         factory,
