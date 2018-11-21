24ac0b802c2db272cc5bf1066639b82b85f2b2f9
Greg Soltis <gsoltis@google.com>
2018-11-20 16:16:58 -0800

    Update types in firebase too

--- packages/firebase/index.d.ts
+++ packages/firebase/index.d.ts
@@ -742,6 +742,8 @@ declare namespace firebase.firestore {
    */
   export type UpdateData = { [fieldPath: string]: any };
 
+  export const CACHE_SIZE_UNLIMITED: number;
+
   /** Settings used to configure a `Firestore` instance. */
   export interface Settings {
     /** The hostname to connect to. */
@@ -768,6 +770,17 @@ declare namespace firebase.firestore {
      * use Timestamp now and opt-in to this new behavior as soon as you can.
      */
     timestampsInSnapshots?: boolean;
+
+    /**
+     * An approximate cache size threshold for the on-disk data. If the cache grows beyond this
+     * size, Firestore will start removing data that hasn't been recently used. The size is not a
+     * guarantee that the cache will stay below that size, only that if the cache exceeds the given
+     * size, cleanup will be attempted.
+     *
+     * The default value is 40 MB. The threshold must be set to at least 1 MB, and can be set to
+     * CACHE_SIZE_UNLIMITED to disable garbage collection.
+     */
+    cacheSizeBytes?: number;
   }
 
   /**
