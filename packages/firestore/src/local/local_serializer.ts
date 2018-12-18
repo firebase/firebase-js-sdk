8f293ebb8ee649491e2adf67360f76d65edab3f5
Konstantin Varlamov <varconst@google.com>
2018-12-17 21:27:21 -0500

    Serializer

--- packages/firestore/src/local/local_serializer.ts
+++ packages/firestore/src/local/local_serializer.ts
@@ -71,7 +71,12 @@ export class LocalSerializer {
   /** Encodes a document for storage locally. */
   toDbRemoteDocument(maybeDoc: MaybeDocument): DbRemoteDocument {
     if (maybeDoc instanceof Document) {
-      const doc = this.remoteSerializer.toDocument(maybeDoc);
+      let doc: api.Document;
+      if (maybeDoc.proto) {
+        doc = maybeDoc.proto;
+      } else {
+        doc = this.remoteSerializer.toDocument(maybeDoc);
+      }
       const hasCommittedMutations = maybeDoc.hasCommittedMutations;
       return new DbRemoteDocument(
         /* unknownDocument= */ null,
