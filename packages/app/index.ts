d7aedbf682d32fb012cf7b1ac551543ec67ef6dc
Christina Holland <hsubox@gmail.com>
2019-02-12 16:05:13 -0800

    Add warning if browser bundle is used in node

--- packages/app/index.ts
+++ packages/app/index.ts
@@ -18,6 +18,21 @@
 import { FirebaseNamespace } from '@firebase/app-types';
 import { createFirebaseNamespace } from './src/firebaseApp';
 
+// Node detection logic from: https://github.com/iliakan/detect-node/
+let isNode = false;
+try {
+    isNode = Object.prototype.toString.call(global.process) === '[object process]';
+} catch(e) {}
+
+isNode && console.warn(`
+Warning: This is a browser-targeted Firebase bundle but it appears it is being run in a Node
+environment.  This may be because you are using ES module resolution and the Node bundle is
+only provided as a CommonJS module.
+
+In order to get the CommonJS Node bundle try setting your bundler to use CommonJS module
+resolution if the option is available, or specify that it should look at "main" for modules.
+`);
+
 export const firebase = createFirebaseNamespace();
 
 export default firebase;
