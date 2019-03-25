c30863335f1009480686c4e79359ed791afd9319
Eric Gilmore <egilmore@google.com>
2019-03-25 11:00:45 -0700

    Fixing parameter error

--- packages/firebase/index.d.ts
+++ packages/firebase/index.d.ts
@@ -7209,21 +7209,21 @@ declare namespace firebase.firestore {
     /**
      * Returns a special value that can be used with `set()` or `update()` that tells
      * the server to increment the field's current value by the given value.
-     *
+     * 
      * If either the operand or the current field value uses floating point precision,
      * all arithmetic follows IEEE 754 semantics. If both values are integers,
      * values outside of JavaScript's safe number range (`Number.MIN_SAFE_INTEGER` to
      * `Number.MAX_SAFE_INTEGER`) are also subject to precision loss. Furthermore,
      * once processed by the Firestore backend, all integer operations are capped
      * between -2^63 and 2^63-1.
-     *
+     * 
      * If the current field value is not of type `number`, or if the field does not
      * yet exist, the transformation sets the field to the given value.
      *
      * @param n The value to increment by.
      * @return The FieldValue sentinel for use in a call to `set()` or `update()`.
      */
-    static increment(...elements: any[]): FieldValue;
+    static increment(n: number): FieldValue;
 
     /**
      * Returns true if this `FieldValue` is equal to the provided one.
