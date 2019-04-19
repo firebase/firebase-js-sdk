1f0cb0a15e5b81aa9c96e963a20eb0f38704f10d
Brian Chen <chenbrian@google.com>
2019-04-19 16:59:10 -0700

    [AUTOMATED]: Prettier Code Styling

--- packages/firestore-types/index.d.ts
+++ packages/firestore-types/index.d.ts
@@ -209,9 +209,9 @@ export class FirebaseFirestore {
    * Creates a write batch, used for performing multiple writes as a single
    * atomic operation. The maximum number of writes that can be passed to a commit operation or
    * performed in a transaction is 500.
-   * 
+   *
    * @return
-     *   A `WriteBatch` that can be used to atomically execute multiple writes.
+   *   A `WriteBatch` that can be used to atomically execute multiple writes.
    */
   batch(): WriteBatch;
 
