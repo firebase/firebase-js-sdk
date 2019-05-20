8356e8e702ed11693707b129afaa204df6a32e69
Ryan P. Brewster <RyanPBrewster@gmail.com>
2019-05-20 16:02:14 -0700

    Use @grpc/proto-loader in @firebase/testing

--- packages/testing/src/api/index.ts
+++ packages/testing/src/api/index.ts
@@ -20,19 +20,15 @@ import * as request from 'request';
 import { base64 } from '@firebase/util';
 import { setLogLevel, LogLevel } from '@firebase/logger';
 import * as grpc from 'grpc';
+import * as protoLoader from '@grpc/proto-loader';
 import { resolve } from 'path';
-import * as fs from 'fs';
 
 export { database, firestore } from 'firebase';
 
-const PROTO_ROOT = {
-  root: resolve(
-    __dirname,
-    process.env.FIRESTORE_EMULATOR_PROTO_ROOT || '../protos'
-  ),
-  file: 'google/firestore/emulator/v1/firestore_emulator.proto'
-};
-const PROTOS = grpc.load(PROTO_ROOT, /* format = */ 'proto');
+const ROOT = resolve(__dirname, process.env.FIRESTORE_EMULATOR_PROTO_ROOT || '../protos');
+const PROTO_FILE = resolve(ROOT, 'google/firestore/emulator/v1/firestore_emulator.proto');
+const PKG_DEF = protoLoader.loadSync(PROTO_FILE, { includeDirs: [ROOT] });
+const PROTOS = grpc.loadPackageDefinition(PKG_DEF);
 const EMULATOR = PROTOS['google']['firestore']['emulator']['v1'];
 
 /** If this environment variable is set, use it for the database emulator's address. */
@@ -203,12 +199,6 @@ export function loadFirestoreRules(
   let client = new EMULATOR.FirestoreEmulator(
     FIRESTORE_ADDRESS,
     grpc.credentials.createInsecure(),
-    {
-      // Cap how much backoff gRPC will perform. This is testing code, so
-      // efficiency is less important than responsiveness.
-      'grpc.initial_reconnect_backoff_ms': 100,
-      'grpc.max_reconnect_backoff_ms': 100
-    }
   );
   return new Promise((resolve, reject) => {
     client.setSecurityRules(
