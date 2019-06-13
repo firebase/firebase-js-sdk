5511cbd6c2659868bae524209a833b6894ddc8c9
Erik Macik <42585523+esmacik@users.noreply.github.com>
2019-06-13 09:49:09 -0700

    add code fonts (#1871)

--- packages/firebase/index.d.ts
+++ packages/firebase/index.d.ts
@@ -1503,10 +1503,10 @@ declare namespace firebase.auth {
     /**
      * The data associated with the action code.
      *
-     * For the PASSWORD_RESET, VERIFY_EMAIL, and RECOVER_EMAIL actions, this object
+     * For the `PASSWORD_RESET`, `VERIFY_EMAIL`, and `RECOVER_EMAIL` actions, this object
      * contains an `email` field with the address the email was sent to.
      *
-     * For the RECOVER_EMAIL action, which allows a user to undo an email address
+     * For the `RECOVER_EMAIL` action, which allows a user to undo an email address
      * change, this object also contains a `fromEmail` field with the user account's
      * new email address. After the action completes, the user's email address will
      * revert to the value in the `email` field from the value in `fromEmail` field.
@@ -1518,13 +1518,13 @@ declare namespace firebase.auth {
     /**
      * The type of operation that generated the action code. This could be:
      * <ul>
-     * <li>`PASSWORD_RESET`: password reset code generated via
+     * <li><code>PASSWORD_RESET</code>: password reset code generated via
      *     {@link firebase.auth.Auth.sendPasswordResetEmail}.</li>
-     * <li>`VERIFY_EMAIL`: email verification code generated via
+     * <li><code>VERIFY_EMAIL</code>: email verification code generated via
      *     {@link firebase.User.sendEmailVerification}.</li>
-     * <li>`RECOVER_EMAIL`: email change revocation code generated via
+     * <li><code>RECOVER_EMAIL</code>: email change revocation code generated via
      *     {@link firebase.User.updateEmail}.</li>
-     * <li>`EMAIL_SIGNIN`: email sign in code generated via
+     * <li><code>EMAIL_SIGNIN</code>: email sign in code generated via
      *     {@link firebase.auth.Auth.sendSignInLinkToEmail}.</li>
      * </ul>
      */
