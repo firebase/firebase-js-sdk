b0760a63292425cae0fd7262ac0c58d5b9956900
Jack Steam <teamsteamdev@gmail.com>
2019-06-13 19:20:44 -0500

    Add linting ignore and remove redundant condition

--- packages/app/index.ts
+++ packages/app/index.ts
@@ -23,12 +23,8 @@ import { Logger } from '@firebase/logger';
 const logger = new Logger('@firebase/app');
 
 // Firebase Lite detection
-if (
-  isBrowser() &&
-  'firebase' in self &&
-  // Check that firebase is not undefined
-  ((self as any).firebase as FirebaseNamespace) !== undefined
-) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+if (isBrowser() && (self as any).firebase !== undefined) {
   logger.warn(`
     Warning: Firebase is already defined in the global scope. Please make sure
     Firebase library is only loaded once.
