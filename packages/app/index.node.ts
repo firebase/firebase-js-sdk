ada5ec851ca939226ce53c2df0f0448f48c95b8a
Christina Holland <hsubox@gmail.com>
2019-06-24 12:00:40 -0700

    Remove extra character

--- packages/app/index.node.ts
+++ packages/app/index.node.ts
@@ -21,7 +21,7 @@ import { createFirebaseNamespace } from './src/firebaseNamespace';
 // Node specific packages.
 // @ts-ignore
 import Storage from 'dom-storage';
-// @ts-ignore`
+// @ts-ignore
 import { XMLHttpRequest } from 'xmlhttprequest';
 
 const _firebase = createFirebaseNamespace() as _FirebaseNamespace;
