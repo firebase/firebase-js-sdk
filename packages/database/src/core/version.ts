9088571fdc7d48f15064e459fefda50b7ef98ba4
Feiyang1 <plane1113@gmail.com>
2019-06-25 14:43:10 -0700

    some comment

--- packages/database/src/core/version.ts
+++ packages/database/src/core/version.ts
@@ -2,6 +2,7 @@
 /** The semver (www.semver.org) version of the SDK. */
 export let SDK_VERSION = '';
 
+// SDK_VERSION should be set before any database instance is created
 export function setSDKVersion(version: string): void {
     SDK_VERSION = version;
 }
