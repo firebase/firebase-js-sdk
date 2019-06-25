56301fdfe447dfd7b34432b5ba2fba5205b0707d
Michael Lehenbauer <mikelehen@google.com>
2019-06-24 17:35:11 -0700

    Fix prettier.

--- tools/gitHooks/prettier.js
+++ tools/gitHooks/prettier.js
@@ -91,7 +91,7 @@ async function doPrettierCommit() {
   if (!hasDiff) return;
 
   const gitSpinner = ora(' Creating automated style commit').start();
-  await git.add('.');
+  await git.add(targetFiles);
 
   await git.commit('[AUTOMATED]: Prettier Code Styling');
   gitSpinner.stopAndPersist({
