2348a259ca0e63e451f677909e71388fdb802e70
Mertcan Mermerkaya <mertcan@google.com>
2019-07-01 18:25:04 +0100

    Use the FID from the server if client FID generation fails (#1925)
    Starting from SDK v0.2.0, the server will start returning a valid FID instead of returning an error if the client tries to register an invalid FID, and the SDK will save that FID to the DB, replacing the invalid one. In case of a failure with client side FID generation, getId will wait for a valid FID from the server before resolving instead of throwing or returning an invalid FID.

--- packages/installations/src/helpers/generate-fid.ts
+++ packages/installations/src/helpers/generate-fid.ts
@@ -17,17 +17,32 @@
 
 import { bufferToBase64UrlSafe } from './buffer-to-base64-url-safe';
 
-/** Generates a new FID using random values from Web Crypto API. */
+export const VALID_FID_PATTERN = /^[cdef][\w-]{21}$/;
+export const INVALID_FID = '';
+
+/**
+ * Generates a new FID using random values from Web Crypto API.
+ * Returns an empty string if FID generation fails for any reason.
+ */
 export function generateFid(): string {
-  // A valid FID has exactly 22 base64 characters, which is 132 bits, or 16.5
-  // bytes. our implementation generates a 17 byte array instead.
-  const fidByteArray = new Uint8Array(17);
-  crypto.getRandomValues(fidByteArray);
+  try {
+    // A valid FID has exactly 22 base64 characters, which is 132 bits, or 16.5
+    // bytes. our implementation generates a 17 byte array instead.
+    const fidByteArray = new Uint8Array(17);
+    const crypto =
+      self.crypto || ((self as unknown) as { msCrypto: Crypto }).msCrypto;
+    crypto.getRandomValues(fidByteArray);
 
-  // Replace the first 4 random bits with the constant FID header of 0b0111.
-  fidByteArray[0] = 0b01110000 + (fidByteArray[0] % 0b00010000);
+    // Replace the first 4 random bits with the constant FID header of 0b0111.
+    fidByteArray[0] = 0b01110000 + (fidByteArray[0] % 0b00010000);
 
-  return encode(fidByteArray);
+    const fid = encode(fidByteArray);
+
+    return VALID_FID_PATTERN.test(fid) ? fid : INVALID_FID;
+  } catch {
+    // FID generation errored
+    return INVALID_FID;
+  }
 }
 
 /** Converts a FID Uint8Array to a base64 string representation. */
