e8900fee2899168656e067bc886c1f1cad78af5b
Mertcan Mermerkaya <mertcan@google.com>
2019-07-02 13:30:57 +0100

    Handle case where the server does not return an FID (#1936)

--- packages/installations/src/api/create-installation.test.ts
+++ packages/installations/src/api/create-installation.test.ts
@@ -105,6 +105,24 @@ describe('createInstallation', () => {
     });
   });
 
+  it('returns the FID from the request if the response does not contain one', async () => {
+    response = {
+      refreshToken: 'refreshToken',
+      authToken: {
+        token:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+        expiresIn: '604800s'
+      }
+    };
+    fetchSpy.resolves(new Response(JSON.stringify(response)));
+
+    const registeredInstallationEntry = await createInstallation(
+      appConfig,
+      inProgressInstallationEntry
+    );
+    expect(registeredInstallationEntry.fid).to.equal(FID);
+  });
+
   describe('failed request', () => {
     it('throws a FirebaseError with the error information from the server', async () => {
       const errorResponse: ErrorResponse = {
