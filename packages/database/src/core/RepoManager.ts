568f923d7ebe35e5426f4f78cdff2930b6b70332
Jan Wyszynski <wyszynski@google.com>
2019-07-25 14:48:05 -0700

    infer namespace from subdomain in database emulator environment variable (#2016)
    * set database emulator namespace to databaseURL subdomain
    
    * [AUTOMATED]: Prettier Code Styling
    
    * use let binding for `dbUrl`
    
    * [AUTOMATED]: Prettier Code Styling
    
    * fix typos
    
    * remove unnecessary parser diff
    
    * nit
    
    * use RepoInfo namespace member for ns query parameter
    
    * type spec for `dbUrl` variable

--- packages/database/src/core/RepoManager.ts
+++ packages/database/src/core/RepoManager.ts
@@ -19,7 +19,7 @@ import { FirebaseApp } from '@firebase/app-types';
 import { safeGet } from '@firebase/util';
 import { Repo } from './Repo';
 import { fatal } from './util/util';
-import { parseRepoInfo } from './util/libs/parser';
+import { parseRepoInfo, parseURL } from './util/libs/parser';
 import { validateUrl } from './util/validation';
 import './Repo_transaction';
 import { Database } from '../api/Database';
@@ -90,12 +90,7 @@ export class RepoManager {
    * @return {!Database}
    */
   databaseFromApp(app: FirebaseApp, url?: string): Database {
-    let dbEmulatorHost = process.env[FIREBASE_DATABASE_EMULATOR_HOST_VAR];
-    if (dbEmulatorHost) {
-      dbEmulatorHost = `http://${dbEmulatorHost}`;
-    }
-    const dbUrl: string =
-      url || dbEmulatorHost || app.options[DATABASE_URL_OPTION];
+    let dbUrl: string | undefined = url || app.options[DATABASE_URL_OPTION];
     if (dbUrl === undefined) {
       fatal(
         "Can't determine Firebase Database URL.  Be sure to include " +
@@ -106,6 +101,11 @@ export class RepoManager {
 
     const parsedUrl = parseRepoInfo(dbUrl);
     const repoInfo = parsedUrl.repoInfo;
+
+    let dbEmulatorHost = process.env[FIREBASE_DATABASE_EMULATOR_HOST_VAR];
+    if (dbEmulatorHost) {
+      dbUrl = `http://${dbEmulatorHost}?ns=${repoInfo.namespace}`;
+    }
 
     validateUrl('Invalid Firebase Database URL', 1, parsedUrl);
     if (!parsedUrl.path.isEmpty()) {
