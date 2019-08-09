3f9e8e8f15d387e57b4e61762c1a33a830f83133
Michael Lehenbauer <mikelehen@google.com>
2019-08-09 13:10:15 -0700

    Enable strictBindCallApply for Firestore. (#2074)

--- packages/firestore/src/util/async_queue.ts
+++ packages/firestore/src/util/async_queue.ts
@@ -326,14 +326,15 @@ export class AsyncQueue {
       `Attempted to schedule multiple operations with timer id ${timerId}.`
     );
 
-    const delayedOp = DelayedOperation.createAndSchedule<unknown>(
+    const delayedOp = DelayedOperation.createAndSchedule<T>(
       this,
       timerId,
       delayMs,
       op,
-      op => this.removeDelayedOperation(op)
+      removedOp =>
+        this.removeDelayedOperation(removedOp as DelayedOperation<unknown>)
     );
-    this.delayedOperations.push(delayedOp);
+    this.delayedOperations.push(delayedOp as DelayedOperation<unknown>);
 
     return delayedOp;
   }
