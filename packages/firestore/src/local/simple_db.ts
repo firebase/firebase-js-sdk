58274e60d718205ca6d8cb1196962c0fb8ce6cfa
Sebastian Schmidt <mrschmidt@google.com>
2019-10-10 21:43:16 -0700

    Comment in test hack

--- packages/firestore/src/local/simple_db.ts
+++ packages/firestore/src/local/simple_db.ts
@@ -257,7 +257,7 @@ export class SimpleDb {
 
   async runTransaction<T>(
     mode: SimpleDbTransactionMode,
-    objectStores: string[],
+    objectStores: string[],x
     transactionFn: (transaction: SimpleDbTransaction) => PersistencePromise<T>
   ): Promise<T> {
     const readonly = mode.startsWith('readonly');
@@ -276,15 +276,15 @@ export class SimpleDb {
         const transactionFnResult = transactionFn(transaction)
           // TODO(schmidt-sebastian): Remove this code/comment or find a way to
           // make this a test-only setting.
-          // // Horrible hack to verify that idempotent functions can be run more
-          // // than once.
-          //   .next(result => {
-          //     if (idempotent && attemptNumber == 1) {
-          //       class DOMException {}
-          //       throw new DOMException();
-          //     }
-          //     return result;
-          //   })
+          // Horrible hack to verify that idempotent functions can be run more
+          // than once.
+          .next(result => {
+            if (idempotent && attemptNumber == 1) {
+              class DOMException {}
+              throw new DOMException();
+            }
+            return result;
+          })
           .catch(error => {
             // Abort the transaction if there was an error.
             transaction.abort(error);
