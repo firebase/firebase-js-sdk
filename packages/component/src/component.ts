fb6ea298fd21ef0e1ff9a8218e73db3426adbcbf
Feiyang1 <plane1113@gmail.com>
2019-10-21 13:57:54 -0700

    add comments for ComponentType

--- packages/component/src/component.ts
+++ packages/component/src/component.ts
@@ -53,6 +53,14 @@ export class Component<T = unknown> {
   }
 }
 
+/**
+ * PUBLIC: A public component provides a set of public APIs to customers. A service namespace will be patched 
+ * onto `firebase` namespace. Assume the component name is `test`, customers will be able
+ * to get the service by calling `firebase.test()` or `app.test()` where `app` is a `FirebaseApp` instance.
+ * 
+ * PRIVATE: A private component provides a set of private APIs that are used internally by other
+ * Firebase SDKs. No serivce namespace is created in `firebase` namespace and customers have no way to get them.
+ */
 export enum ComponentType {
   PUBLIC,
   PRIVATE
