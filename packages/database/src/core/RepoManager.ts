34884f2f566ef722797c45f4b8080259ce4da74b
Jan Wyszynski <wyszynski@google.com>
2019-10-30 16:43:31 -0700

    Revert "use fake "owner" credential when rtdb emulator env var is set… (#2308)
    * Revert "use fake "owner" credential when rtdb emulator env var is set (#2029)"
    
    This reverts commit 4ebedd071ddd4f4f7ab427a87fa6a725622225c3.
    
    * bump @firebase/database package version
    
    * undo database package version bump

--- packages/database/src/core/RepoManager.ts
+++ packages/database/src/core/RepoManager.ts
@@ -18,8 +18,8 @@
 import { FirebaseApp } from '@firebase/app-types';
 import { safeGet } from '@firebase/util';
 import { Repo } from './Repo';
-import { fatal, FIREBASE_DATABASE_EMULATOR_HOST_VAR } from './util/util';
-import { parseRepoInfo, parseDatabaseURL } from './util/libs/parser';
+import { fatal } from './util/util';
+import { parseRepoInfo } from './util/libs/parser';
 import { validateUrl } from './util/validation';
 import './Repo_transaction';
 import { Database } from '../api/Database';
@@ -27,6 +27,16 @@ import { RepoInfo } from './RepoInfo';
 
 /** @const {string} */
 const DATABASE_URL_OPTION = 'databaseURL';
+
+/**
+ * This variable is also defined in the firebase node.js admin SDK. Before
+ * modifying this definition, consult the definition in:
+ *
+ * https://github.com/firebase/firebase-admin-node
+ *
+ * and make sure the two are consistent.
+ */
+const FIREBASE_DATABASE_EMULATOR_HOST_VAR = 'FIREBASE_DATABASE_EMULATOR_HOST';
 
 let _staticInstance: RepoManager;
 
