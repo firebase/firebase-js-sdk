86971ea3c727084fa1a99322afe473d19068bac5
Thomas Bouldin <inlined@users.noreply.github.com>
2019-11-18 08:55:38 -0800

    Add Database.Servervalue.increment(x) (#2348)
    * Added ServerValue._increment() for not-yet-working operator (needs server-side rollout and API approval)
    * Changed server value local resolution to include current offline caches
    * Add new tests for ServerValues in general + offline increments

--- packages/database/src/core/Repo.ts
+++ packages/database/src/core/Repo.ts
@@ -307,8 +307,10 @@ export class Repo {
     // (b) store unresolved paths on JSON parse
     const serverValues = this.generateServerValues();
     const newNodeUnresolved = nodeFromJSON(newVal, newPriority);
+    const existing = this.serverSyncTree_.calcCompleteEventCache(path);
     const newNode = resolveDeferredValueSnapshot(
       newNodeUnresolved,
+      existing,
       serverValues
     );
 
@@ -359,6 +361,7 @@ export class Repo {
       const newNodeUnresolved = nodeFromJSON(changedValue);
       changedChildren[changedKey] = resolveDeferredValueSnapshot(
         newNodeUnresolved,
+        this.serverSyncTree_.calcCompleteEventCache(path),
         serverValues
       );
     });
@@ -413,6 +416,7 @@ export class Repo {
     const serverValues = this.generateServerValues();
     const resolvedOnDisconnectTree = resolveDeferredValueTree(
       this.onDisconnect_,
+      this.serverSyncTree_,
       serverValues
     );
     let events: Event[] = [];
