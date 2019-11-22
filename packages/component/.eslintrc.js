bfb30b8a249fd39e3236b17630c8af3ee5004340
Feiyang1 <plane1113@gmail.com>
2019-11-22 15:18:26 -0800

    [AUTOMATED]: Prettier Code Styling

--- packages/component/.eslintrc.js
+++ packages/component/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-    extends: "../../config/.eslintrc.js",
-    parserOptions: {
-        project: "tsconfig.json"
-    }
+  extends: '../../config/.eslintrc.js',
+  parserOptions: {
+    project: 'tsconfig.json'
+  }
 };
