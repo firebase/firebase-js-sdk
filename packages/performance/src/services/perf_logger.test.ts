3c118bd8ba6ef7e44aa0a61f1ac6d5c09bfa072e
Viswanathan Munisamy <visum@google.com>
2020-01-09 13:45:48 -0800

    Add unit tests for proto packing of Page Load Trace.

--- packages/performance/src/services/perf_logger.test.ts
+++ packages/performance/src/services/perf_logger.test.ts
@@ -74,7 +74,6 @@ describe('Performance Monitoring > perf_logger', () => {
     stub(Api.prototype, 'getUrl').returns(PAGE_URL);
     stub(Api.prototype, 'getTimeOrigin').returns(TIME_ORIGIN);
     stub(initializationService, 'isPerfInitialized').returns(true);
-    stub(attributeUtils, 'getVisibilityState').returns(VISIBILITY_STATE);
     stub(attributeUtils, 'getEffectiveConnectionType').returns(
       EFFECTIVE_CONNECTION_TYPE
     );
@@ -96,6 +95,7 @@ describe('Performance Monitoring > perf_logger', () => {
 "client_start_time_us":${START_TIME * 1000},"duration_us":${DURATION * 1000},\
 "counters":{"counter1":3},"custom_attributes":{"attr":"val"}}}`;
       getIidStub.returns(IID);
+      stub(attributeUtils, 'getVisibilityState').returns(VISIBILITY_STATE);
       SettingsService.getInstance().loggingEnabled = true;
       SettingsService.getInstance().logTraceAfterSampling = true;
       const trace = new Trace(TRACE_NAME);
@@ -112,6 +112,7 @@ describe('Performance Monitoring > perf_logger', () => {
 
     it('does not log an event if cookies are disabled in the browser', () => {
       stub(Api.prototype, 'requiredApisAvailable').returns(false);
+      stub(attributeUtils, 'getVisibilityState').returns(VISIBILITY_STATE);
       const trace = new Trace(TRACE_NAME);
       trace.record(START_TIME, DURATION);
       clock.tick(1);
@@ -132,6 +133,7 @@ describe('Performance Monitoring > perf_logger', () => {
 "counter25":25,"counter26":26,"counter27":27,"counter28":28,"counter29":29,"counter30":30,\
 "counter31":31,"counter32":32}}}`;
       getIidStub.returns(IID);
+      stub(attributeUtils, 'getVisibilityState').returns(VISIBILITY_STATE);
       SettingsService.getInstance().loggingEnabled = true;
       SettingsService.getInstance().logTraceAfterSampling = true;
       const trace = new Trace(TRACE_NAME);
@@ -155,6 +157,7 @@ describe('Performance Monitoring > perf_logger', () => {
 "client_start_time_us":${START_TIME * 1000},"duration_us":${DURATION * 1000},\
 "custom_attributes":{"attr1":"val1","attr2":"val2","attr3":"val3","attr4":"val4","attr5":"val5"}}}`;
       getIidStub.returns(IID);
+      stub(attributeUtils, 'getVisibilityState').returns(VISIBILITY_STATE);
       SettingsService.getInstance().loggingEnabled = true;
       SettingsService.getInstance().logTraceAfterSampling = true;
       const trace = new Trace(TRACE_NAME);
@@ -162,6 +165,71 @@ describe('Performance Monitoring > perf_logger', () => {
         trace.putAttribute('attr' + i, 'val' + i);
       }
       trace.record(START_TIME, DURATION);
+      clock.tick(1);
+
+      expect(addToQueueStub).to.be.called;
+      expect(addToQueueStub.getCall(0).args[0].message).to.be.equal(
+        EXPECTED_TRACE_MESSAGE
+      );
+    });
+  });
+
+  describe('logPageLoadTrace', () => {
+    it('creates, serializes and sends a page load trace to cc service', () => {
+      const flooredStartTime = Math.floor(TIME_ORIGIN * 1000);
+      const EXPECTED_TRACE_MESSAGE = `{"application_info":{"google_app_id":"${APP_ID}",\
+"app_instance_id":"${IID}","web_app_info":{"sdk_version":"${SDK_VERSION}",\
+"page_url":"${PAGE_URL}","service_worker_status":${SERVICE_WORKER_STATUS},\
+"visibility_state":${attributeUtils.VisibilityState.VISIBLE},"effective_connection_type":${EFFECTIVE_CONNECTION_TYPE}},\
+"application_process_state":0},"trace_metric":{"name":"_wt_${PAGE_URL}","is_auto":true,\
+"client_start_time_us":${flooredStartTime},"duration_us":${DURATION * 1000},\
+"counters":{"domInteractive":10000,"domContentLoadedEventEnd":20000,"loadEventEnd":10000,\
+"_fp":40000,"_fcp":50000,"_fid":90000}}}`;
+      getIidStub.returns(IID);
+      SettingsService.getInstance().loggingEnabled = true;
+      SettingsService.getInstance().logTraceAfterSampling = true;
+
+      stub(attributeUtils, 'getVisibilityState').returns(attributeUtils.VisibilityState.VISIBLE);
+
+      const navigationTiming: PerformanceNavigationTiming = {
+        domComplete: 100,
+        domContentLoadedEventEnd: 20,
+        domContentLoadedEventStart: 10,
+        domInteractive: 10,
+        loadEventEnd: 10,
+        loadEventStart: 10,
+        redirectCount: 10,
+        type: 'navigate',
+        unloadEventEnd: 10,
+        unloadEventStart: 10,
+        duration: DURATION,
+      } as PerformanceNavigationTiming;
+
+      const navigationTimings: PerformanceNavigationTiming[] = [
+        navigationTiming
+      ];
+
+      const firstPaint: PerformanceEntry = {
+        name: 'first-paint',
+        startTime: 40,
+        duration: 100,
+        entryType: 'url',
+        toJSON() {},
+      };
+
+      const firstContentfulPaint:Readonly<PerformanceEntry> = {
+        name: 'first-contentful-paint',
+        startTime: 50,
+        duration: 100,
+        entryType: 'url',
+        toJSON() {} 
+      };
+
+      const paintTimings: PerformanceEntry[] = [
+        firstPaint, firstContentfulPaint
+      ];
+ 
+      Trace.createOobTrace(navigationTimings, paintTimings, 90);
       clock.tick(1);
 
       expect(addToQueueStub).to.be.called;
@@ -215,6 +283,7 @@ describe('Performance Monitoring > perf_logger', () => {
 "client_start_time_us":${START_TIME},\
 "time_to_response_completed_us":${TIME_TO_RESPONSE_COMPLETED}}}`;
       getIidStub.returns(IID);
+      stub(attributeUtils, 'getVisibilityState').returns(VISIBILITY_STATE);
       SettingsService.getInstance().loggingEnabled = true;
       SettingsService.getInstance().logNetworkAfterSampling = true;
       // Calls logNetworkRequest under the hood.
