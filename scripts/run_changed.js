7fc2507b6d16b642192f3760cf3587e626564983
Christina Holland <hsubox@gmail.com>
2020-01-22 13:23:41 -0800

    add commit to test

---
+++ scripts/run_changed.js
@@ -0,0 +1,26 @@
+const { resolve } = require('path');
+const { spawn } = require('child-process-promise');
+const simpleGit = require('simple-git/promise');
+const root = resolve(__dirname, '..');
+const git = simpleGit(root);
+
+async function runTestsOnChangedPackages() {
+  try {
+    const diff = await git.diff(['--name-only', 'origin/master...HEAD']);
+    const changedFiles = diff.split('\n');
+    console.log(diff);
+    const changedPackages = {};
+    for (const filename of changedFiles) {
+      console.log(filename);
+      if (filename.includes('packages/')) {
+        console.log(filename);
+      }
+    }
+  
+  } catch(e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+runTestsOnChangedPackages();
