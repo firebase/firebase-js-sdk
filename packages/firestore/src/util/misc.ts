97aada8c57d8f481433b8b3fb288c33dd91f931a
Wu-Hui <wu.hui.github@gmail.com>
2020-03-20 21:09:41 -0400

    Declare magic numbers.

--- packages/firestore/src/util/misc.ts
+++ packages/firestore/src/util/misc.ts
@@ -35,7 +35,8 @@ export class AutoId {
         // Length of `chars` is 62. We only take bytes between 0 and 62*4-1
         // (both inclusive). The value is then evenly mapped to indices of `char`
         // via a modulo operation.
-        if (autoId.length < 20 && b <= 62 * 4 - 1) {
+        const maxValue = 62 * 4 - 1;
+        if (autoId.length < 20 && b <= maxValue) {
           autoId += chars.charAt(b % 62);
         }
       });
