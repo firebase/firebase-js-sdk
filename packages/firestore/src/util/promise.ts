3928eaa3f2d9cc485f5f8623b6c04de5d4a94380
Sebastian Schmidt <mrschmidt@google.com>
2020-04-02 10:51:04 -0700

    Fix finally usage

--- packages/firestore/src/util/promise.ts
+++ packages/firestore/src/util/promise.ts
@@ -23,7 +23,25 @@ export interface Rejecter {
   (reason?: Error): void;
 }
 
-export interface CancelablePromise<T> extends Promise<T> {
+export interface CancelablePromise<T> {
+  // We are not extending Promise, since Node's Promise API require us to
+  // implement 'finally', which is not fully supported on Web.
+  then<TResult1 = T, TResult2 = never>(
+    onfulfilled?:
+      | ((value: T) => TResult1 | PromiseLike<TResult1>)
+      | undefined
+      | null,
+    onrejected?:
+      | ((reason: any) => TResult2 | PromiseLike<TResult2>)
+      | undefined
+      | null
+  ): Promise<TResult1 | TResult2>;
+  catch<TResult = never>(
+    onrejected?:
+      | ((reason: any) => TResult | PromiseLike<TResult>)
+      | undefined
+      | null
+  ): Promise<T | TResult>;
   cancel(): void;
 }
 
