7446b8aa0488be1753930906898453d8afa43c30
Feiyang1 <plane1113@gmail.com>
2020-04-07 17:34:44 -0700

    produce build artifacts without -exp in package names

---
+++ scripts/exp/ts-transform-import-path.js
@@ -0,0 +1,49 @@
+import * as ts from 'typescript';
+/**
+ * remove '-exp' in import paths.
+ * For example, `import {...} from '@firebase/app-exp'` becomes `import {...} from '@firebase/app'`.
+ */
+
+export const importPathTransformer = () => ({
+    before: [transformImportPath()],
+    after: [],
+    afterDeclarations: [transformImportPath()]
+});
+
+function transformImportPath() {
+    return (context) => (file) => {
+        return visitNodeAndChildren(file, context);
+    };
+}
+
+function visitNodeAndChildren(
+    node,
+    context
+) {
+    return ts.visitEachChild(
+        visitNode(node),
+        (childNode) => visitNodeAndChildren(childNode, context),
+        context
+    );
+}
+
+function visitNode(node) {
+    let importPath;
+    if (ts.isImportDeclaration(node)) {
+        const importPathWithQuotes = node.moduleSpecifier.getText();
+        importPath = importPathWithQuotes.substr(1, importPathWithQuotes.length - 2)
+
+        const pattern = /^(@firebase.*)-exp(.*)$/g;
+        const captures = pattern.exec(importPath);
+
+        if (captures) {
+            const newName = `${captures[1]}${captures[2]}`;
+            const newNode = ts.getMutableClone(node);
+            newNode.moduleSpecifier = ts.createLiteral(newName);
+            return newNode;
+        }
+
+    }
+
+    return node;
+}
