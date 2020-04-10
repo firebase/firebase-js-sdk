8278bfc2c17d91835c4a3f550ba5508273619a43
Alex Volkovitsky <avolkovi@google.com>
2020-04-10 11:16:59 -0700

    Add core types & error map to auth-exp (#2890)
    * Add core types & error map
    
    * [AUTOMATED]: Prettier Code Styling
    
    * [AUTOMATED]: License Headers
    
    * User should be an interface for now
    
    we can make an implementation class later

---
+++ packages-exp/auth-exp/test/core/errors.test.ts
@@ -0,0 +1,32 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { expect } from 'chai';
+import { AUTH_ERROR_FACTORY, AuthErrorCode } from '../../src/core/errors';
+
+describe('AUTH_ERROR_FACTORY', () => {
+  it('should create an Auth namespaced FirebaseError', () => {
+    const error = AUTH_ERROR_FACTORY.create(AuthErrorCode.INTERNAL_ERROR, {
+      appName: 'my-app'
+    });
+    expect(error.code).to.eq('auth/internal-error');
+    expect(error.message).to.eq(
+      'Firebase: An internal AuthError has occurred. (auth/internal-error).'
+    );
+    expect(error.name).to.eq('FirebaseError');
+  });
+});
