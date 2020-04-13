61a52c979e3b880d7768fc9a00954b36bac0963e
Alex Volkovitsky <avolkovi@google.com>
2020-04-13 15:13:20 -0700

    Add signUp API method for auth-next (#2892)
    * User should be an interface for now
    
    we can make an implementation class later
    
    * Add API call to signUp
    
    * [AUTOMATED]: Prettier Code Styling
    
    * [AUTOMATED]: License Headers
    
    * Update tests to test a little more
    
    * [AUTOMATED]: Prettier Code Styling

---
+++ packages-exp/auth-exp/src/api/authentication/sign_up.ts
@@ -0,0 +1,43 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Auth } from '../../model/auth';
+import { IdTokenResponse } from '../../model/id_token';
+import { performSignInRequest, HttpMethod, Endpoint } from '..';
+
+export interface SignUpRequest {
+  returnSecureToken?: boolean;
+  email?: string;
+  password?: string;
+}
+
+export interface SignUpResponse extends IdTokenResponse {
+  displayName?: string;
+  email?: string;
+}
+
+export async function signUp(
+  auth: Auth,
+  request: SignUpRequest
+): Promise<SignUpResponse> {
+  return performSignInRequest<SignUpRequest, SignUpResponse>(
+    auth,
+    HttpMethod.POST,
+    Endpoint.SIGN_UP,
+    request
+  );
+}
