099c76a1c9552417ec44cf7e48d4d94a330ae07d
Kai Wu <kaiwu@google.com>
2020-04-14 11:00:41 -0700

    [AUTOMATED]: License Headers

--- integration/messaging/test/static/constants.js
+++ integration/messaging/test/static/constants.js
@@ -1,3 +1,20 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const EIGHT_DAYS_IN_MS = 8 * 86400000;
 const PUBLIC_VAPID_KEY =
   'BAN_2QvaSnxnjz-2y1sARN1ErEpZhcb8kaSI08pLxiAtQPB4ja3wg_1T0cGBMpt-vRd_6G1LZeVcVYlQeSPlbt8';
