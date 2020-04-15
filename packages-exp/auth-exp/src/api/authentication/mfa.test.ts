3d433f0f75267cffee7a2c3ce8852d9124c4da39
Alex Volkovitsky <avolkovi@google.com>
2020-04-15 13:52:33 -0700

    Add remaining API methods to auth-next (#2901)
    * User should be an interface for now
    
    we can make an implementation class later
    
    * Add API call to signUp
    
    * [AUTOMATED]: Prettier Code Styling
    
    * Update tests to test a little more
    
    * Add remaining API methods to auth-next
    
    * [AUTOMATED]: Prettier Code Styling
    
    * [AUTOMATED]: License Headers
    
    * Add more tests to account API methods
    
    * Pass in SDK version & correctly send GET request params
    
    * [AUTOMATED]: Prettier Code Styling
    
    * [AUTOMATED]: Prettier Code Styling
    
    * Fix import ordering
    
    * Minor formatting change
    
    * [AUTOMATED]: Prettier Code Styling

---
+++ packages-exp/auth-exp/src/api/authentication/mfa.test.ts
@@ -0,0 +1,138 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { FirebaseError } from '@firebase/util';
+import { expect, use } from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+import { Endpoint } from '..';
+import { mockEndpoint } from '../../../test/api/helper';
+import { mockAuth } from '../../../test/mock_auth';
+import * as mockFetch from '../../../test/mock_fetch';
+import { ServerError } from '../errors';
+import { finalizeSignInPhoneMfa, startSignInPhoneMfa } from './mfa';
+
+use(chaiAsPromised);
+
+describe('startSignInPhoneMfa', () => {
+  const request = {
+    mfaPendingCredential: 'my-creds',
+    mfaEnrollmentId: 'my-enrollment-id',
+    phoneSignInInfo: {
+      recaptchaToken: 'catpcha-token'
+    }
+  };
+
+  beforeEach(mockFetch.setUp);
+  afterEach(mockFetch.tearDown);
+
+  it('should POST to the correct endpoint', async () => {
+    const mock = mockEndpoint(Endpoint.START_PHONE_MFA_SIGN_IN, {
+      phoneResponseInfo: {
+        sessionInfo: 'session-info'
+      }
+    });
+
+    const response = await startSignInPhoneMfa(mockAuth, request);
+    expect(response.phoneResponseInfo.sessionInfo).to.eq('session-info');
+    expect(mock.calls[0].request).to.eql(request);
+    expect(mock.calls[0].method).to.eq('POST');
+    expect(mock.calls[0].headers).to.eql({
+      'Content-Type': 'application/json',
+      'X-Client-Version': 'testSDK/0.0.0'
+    });
+  });
+
+  it('should handle errors', async () => {
+    const mock = mockEndpoint(
+      Endpoint.START_PHONE_MFA_SIGN_IN,
+      {
+        error: {
+          code: 400,
+          message: ServerError.INVALID_PENDING_TOKEN,
+          errors: [
+            {
+              message: ServerError.INVALID_PENDING_TOKEN
+            }
+          ]
+        }
+      },
+      400
+    );
+
+    await expect(startSignInPhoneMfa(mockAuth, request)).to.be.rejectedWith(
+      FirebaseError,
+      'Firebase: The supplied auth credential is malformed or has expired. (auth/invalid-credential).'
+    );
+    expect(mock.calls[0].request).to.eql(request);
+  });
+});
+
+describe('finalizeSignInPhoneMfa', () => {
+  const request = {
+    mfaPendingCredential: 'pending-cred',
+    phoneVerificationInfo: {
+      temporaryProof: 'proof',
+      phoneNumber: '123456789',
+      sessionInfo: 'session-info',
+      code: 'my-code'
+    }
+  };
+
+  beforeEach(mockFetch.setUp);
+  afterEach(mockFetch.tearDown);
+
+  it('should POST to the correct endpoint', async () => {
+    const mock = mockEndpoint(Endpoint.FINALIZE_PHONE_MFA_SIGN_IN, {
+      displayName: 'my-name',
+      idToken: 'id-token'
+    });
+
+    const response = await finalizeSignInPhoneMfa(mockAuth, request);
+    expect(response.displayName).to.eq('my-name');
+    expect(response.idToken).to.eq('id-token');
+    expect(mock.calls[0].request).to.eql(request);
+    expect(mock.calls[0].method).to.eq('POST');
+    expect(mock.calls[0].headers).to.eql({
+      'Content-Type': 'application/json',
+      'X-Client-Version': 'testSDK/0.0.0'
+    });
+  });
+
+  it('should handle errors', async () => {
+    const mock = mockEndpoint(
+      Endpoint.FINALIZE_PHONE_MFA_SIGN_IN,
+      {
+        error: {
+          code: 400,
+          message: ServerError.INVALID_CODE,
+          errors: [
+            {
+              message: ServerError.INVALID_CODE
+            }
+          ]
+        }
+      },
+      400
+    );
+
+    await expect(finalizeSignInPhoneMfa(mockAuth, request)).to.be.rejectedWith(
+      FirebaseError,
+      'Firebase: The SMS verification code used to create the phone auth credential is invalid. Please resend the verification code sms and be sure use the verification code provided by the user. (auth/invalid-verification-code).'
+    );
+    expect(mock.calls[0].request).to.eql(request);
+  });
+});
