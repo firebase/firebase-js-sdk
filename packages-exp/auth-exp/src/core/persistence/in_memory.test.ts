77d0809b663763f77a0b67ebb350ef798610d1c7
samhorlbeck <sam.horlsen@gmail.com>
2020-04-16 13:44:31 -0700

    Add persistence layer: index db, in memory, and browser{local, session} (#2908)

---
+++ packages-exp/auth-exp/src/core/persistence/in_memory.test.ts
@@ -0,0 +1,51 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { inMemoryPersistence as persistence } from './in_memory';
+import { PersistenceType } from '.';
+import { expect } from 'chai';
+import { User } from '../../model/user';
+import { testUser } from '../../../test/mock_auth';
+
+describe('core/persistence/in_memory', () => {
+  it('should work with persistence type', async () => {
+    const key = 'my-super-special-persistence-type';
+    const value = PersistenceType.LOCAL;
+    expect(await persistence.get(key)).to.be.null;
+    await persistence.set(key, value);
+    expect(await persistence.get(key)).to.be.eq(value);
+    expect(await persistence.get('other-key')).to.be.null;
+    await persistence.remove(key);
+    expect(await persistence.get(key)).to.be.null;
+  });
+
+  it('should work with user', async () => {
+    const key = 'my-super-special-user';
+    const value = testUser('uid');
+
+    expect(await persistence.get(key)).to.be.null;
+    await persistence.set(key, value);
+    expect(await persistence.get<User>(key)).to.eql(value);
+    expect(await persistence.get('other-key')).to.be.null;
+    await persistence.remove(key);
+    expect(await persistence.get(key)).to.be.null;
+  });
+
+  it('isAvailable returns true', async () => {
+    expect(await persistence.isAvailable()).to.be.true;
+  });
+});
