60cf557562a34838ede6b1f35ad08104b02cf063
Sebastian Schmidt <mrschmidt@google.com>
2020-04-18 20:06:51 -0700

    [AUTOMATED]: Prettier Code Styling

--- packages/firestore-types/sample.ts
+++ packages/firestore-types/sample.ts
@@ -3,19 +3,20 @@ import {
   memoryPersistence,
   onSnapshot,
   DocumentSnapshot,
-  QuerySnapshot, onSnapshotsInSync,
-} from "./";
+  QuerySnapshot,
+  onSnapshotsInSync
+} from './';
 
 const firestore = getFirestore(app);
-initializeFirestore(firestore, { persistence: memoryPersistence});
+initializeFirestore(firestore, { persistence: memoryPersistence });
 
 const docRef = firestore.doc('collection/doc');
-const unsubscribe1 = onSnapshot(docRef, (snapshot:DocumentSnapshot) => {
+const unsubscribe1 = onSnapshot(docRef, (snapshot: DocumentSnapshot) => {
   //
 });
 
 const collRef = firestore.collection('collection');
-const unsubscribe2 = onSnapshot(collRef, (snapshot:QuerySnapshot)=> {
+const unsubscribe2 = onSnapshot(collRef, (snapshot: QuerySnapshot) => {
   //
 });
 
