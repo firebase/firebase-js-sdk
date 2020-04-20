78bed5add8e83e889ee82d05186b29363b336d09
Sebastian Schmidt <mrschmidt@google.com>
2020-04-20 11:25:44 -0700

    WIP

---
+++ packages/firestore/rest/src/api/crud.ts
@@ -0,0 +1,5 @@
+import {DocumentReference, FirebaseFirestore} from "./database";
+
+export function getDocument(reference: DocumentReference) {
+  
+}
---
+++ packages/firestore/rest/src/api/database.ts
@@ -0,0 +1,7 @@
+export class FirebaseFirestore {
+  
+}
+
+export class DocumentReference {
+  
+}
