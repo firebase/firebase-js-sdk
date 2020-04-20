6347a9369265feea45a7a45ec3fe45f2c846a38d
Sebastian Schmidt <mrschmidt@google.com>
2020-04-20 14:57:29 -0700

    WIP

--- packages/firestore/rest/src/api/crud.ts
+++ packages/firestore/rest/src/api/crud.ts
@@ -1,5 +1,11 @@
 import {DocumentReference, FirebaseFirestore} from "./database";
+import {DocumentSnapshot} from "../../../src/api/database";
 
-export function getDocument(reference: DocumentReference) {
+export function initializeFirestore(
+  firestore: FirebaseFirestore,
+  settings: Settings
+): Promise<void>;
+
+export function getDocument(reference: DocumentReference) : Promise<DocumentSnapshot> {
   
 }
