18fb16b74ed0e6b4e3a2b379a11fb07d6ab654e0
Nicolas Delperdange <nicolas_delperdange@hotmail.com>
2020-04-21 19:34:17 +0200

    feat(sw-controller): compare host instead of href for getWindowClient (#2772)

--- packages/messaging/src/controllers/sw-controller.ts
+++ packages/messaging/src/controllers/sw-controller.ts
@@ -273,13 +273,13 @@ function getNotificationData(
 async function getWindowClient(url: string): Promise<WindowClient | null> {
   // Use URL to normalize the URL when comparing to windowClients.
   // This at least handles whether to include trailing slashes or not
-  const parsedURL = new URL(url, self.location.href).href;
+  const parsedURL = new URL(url, self.location.href);
 
   const clientList = await getClientList();
 
   for (const client of clientList) {
-    const parsedClientUrl = new URL(client.url, self.location.href).href;
-    if (parsedClientUrl === parsedURL) {
+    const parsedClientUrl = new URL(client.url, self.location.href);
+    if (parsedClientUrl.host === parsedURL.host) {
       return client;
     }
   }
