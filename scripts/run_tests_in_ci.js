5d25a623037576888ad9d2586ece4440e9bba65a
Feiyang1 <plane1113@gmail.com>
2020-04-23 16:33:44 -0700

    [AUTOMATED]: API Reports

---
+++ scripts/run_tests_in_ci.js
@@ -0,0 +1,30 @@
+const { argv } = require('yargs');
+const path = require('path');
+const { spawn } = require('child-process-promise');
+
+(async () => {
+    const myPath = argv._[0] || '.'; // default to the current directory
+    const dir = path.resolve(myPath);
+    const { name } = require(`${dir}/package.json`);
+
+    let stdio;
+    let stderr;
+    try {
+        const testProcess = spawn('yarn', ['--cwd', dir, 'test']);
+
+        testProcess.childProcess.stdout.on('data', data => {
+            stdio += data.toString();
+        });
+        testProcess.childProcess.stderr.on('data', data => {
+            stderr += data.toString();
+        });
+
+        await testProcess;
+        console.log('Success: ' + name);
+    } catch (e) {
+        console.error('Failure: ' + name);
+        console.log(stdio);
+        console.error(stderr);
+        process.exit(1);
+    }
+})();
