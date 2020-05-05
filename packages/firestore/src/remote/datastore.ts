65a25252ce9113df20bd5ce92761fe7c2996672c
Sebastian Schmidt <mrschmidt@google.com>
2020-05-05 14:31:58 -0700

    Add invokeRunQueryRpc()

--- packages/firestore/src/remote/datastore.ts
+++ packages/firestore/src/remote/datastore.ts
@@ -16,7 +16,7 @@
  */
 
 import { CredentialsProvider } from '../api/credentials';
-import { MaybeDocument } from '../model/document';
+import { MaybeDocument, Document } from '../model/document';
 import { DocumentKey } from '../model/document_key';
 import { Mutation, MutationResult } from '../model/mutation';
 import * as api from '../protos/firestore_proto_api';
@@ -31,6 +31,7 @@ import {
   WriteStreamListener
 } from './persistent_stream';
 import { AsyncQueue } from '../util/async_queue';
+import { Query } from '../core/query';
 
 /**
  * Datastore and its related methods are a wrapper around the external Google
@@ -147,6 +148,33 @@ export async function invokeBatchGetDocumentsRpc(
     result.push(doc);
   });
   return result;
+}
+
+export async function invokeRunQueryRpc(
+  datastore: Datastore,
+  query: Query
+): Promise<Document[]> {
+  const datastoreImpl = debugCast(datastore, DatastoreImpl);
+  const { structuredQuery, parent } = datastoreImpl.serializer.toQueryTarget(
+    query.toTarget()
+  );
+  const params = {
+    database: datastoreImpl.serializer.encodedDatabaseId,
+    parent,
+    structuredQuery
+  };
+
+  const response = await datastoreImpl.invokeStreamingRPC<
+    api.RunQueryRequest,
+    api.RunQueryResponse
+  >('RunQuery', params);
+
+  return (
+    response
+      // Filter RunQueryResponses that only contain readTimes.
+      .filter(proto => !!proto.document)
+      .map(proto => datastoreImpl.serializer.fromDocument(proto.document!))
+  );
 }
 
 export function newPersistentWriteStream(
