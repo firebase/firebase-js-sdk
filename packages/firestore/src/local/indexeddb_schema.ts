83c770a0edfe995e0286eddd3d0b29e0ff97a0d8
Wu-Hui <wu.hui.github@gmail.com>
2020-05-15 15:28:00 -0400

    Reordering imports

--- packages/firestore/src/local/indexeddb_schema.ts
+++ packages/firestore/src/local/indexeddb_schema.ts
@@ -17,8 +17,8 @@
 
 import { BatchId, ListenSequenceNumber, TargetId } from '../core/types';
 import { ResourcePath } from '../model/path';
-import * as api from '../protos/firestore_proto_api';
 import { BundledQuery } from '../protos/firestore_bundle_proto';
+import * as api from '../protos/firestore_proto_api';
 import { hardAssert, debugAssert } from '../util/assert';
 
 import { SnapshotVersion } from '../core/snapshot_version';
