fdec92fc70f6bee7c484f3b62fd490821b94b4aa
Alex Volkovitsky <avolkovi@google.com>
2020-05-29 14:02:17 -0700

    Add more tests

--- packages-exp/auth-exp/src/core/strategies/anonymous.test.ts
+++ packages-exp/auth-exp/src/core/strategies/anonymous.test.ts
@@ -15,10 +15,14 @@
  * limitations under the License.
  */
 
-import { OperationType, ProviderId, SignInMethod } from '@firebase/auth-types-exp';
+import {
+  OperationType,
+  ProviderId,
+  SignInMethod
+} from '@firebase/auth-types-exp';
 import { expect } from 'chai';
 import { mockEndpoint } from '../../../test/api/helper';
-import { mockAuth } from '../../../test/mock_auth';
+import { mockAuth, testUser } from '../../../test/mock_auth';
 import * as mockFetch from '../../../test/mock_fetch';
 import { Endpoint } from '../../api';
 import { APIUserInfo } from '../../api/account_management/account';
@@ -55,8 +59,37 @@ describe('core/strategies/anonymous', () => {
       expect(user.isAnonymous).to.be.true;
     });
 
-    context('already signed in anonymousl', () => {});
+    context('already signed in anonymously', () => {
+      it('should return the current user', async () => {
+        const userCredential = await signInAnonymously(mockAuth);
+        expect(userCredential.user.isAnonymous).to.be.true;
 
-    context('already signed in with a non-anonymous account', () => {});
+        const { credential, user, operationType } = await signInAnonymously(
+          mockAuth
+        );
+        expect(credential?.providerId).to.eq(ProviderId.ANONYMOUS);
+        expect(credential?.signInMethod).to.eq(SignInMethod.ANONYMOUS);
+        expect(operationType).to.eq(OperationType.SIGN_IN);
+        expect(user.uid).to.eq(userCredential.user.uid);
+        expect(user.isAnonymous).to.be.true;
+      });
+    });
+
+    context('already signed in with a non-anonymous account', () => {
+      it('should sign in as a new user user', async () => {
+        const fakeUser = testUser('other-uid');
+        await mockAuth.updateCurrentUser(fakeUser);
+        expect(fakeUser.isAnonymous).to.be.false;
+
+        const { credential, user, operationType } = await signInAnonymously(
+          mockAuth
+        );
+        expect(credential?.providerId).to.eq(ProviderId.ANONYMOUS);
+        expect(credential?.signInMethod).to.eq(SignInMethod.ANONYMOUS);
+        expect(operationType).to.eq(OperationType.SIGN_IN);
+        expect(user.uid).to.not.eq(fakeUser.uid);
+        expect(user.isAnonymous).to.be.true;
+      });
+    });
   });
 });
