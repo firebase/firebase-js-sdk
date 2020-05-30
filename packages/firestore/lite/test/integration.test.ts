7649b6789b64015e6ecb9d6f63a7e702fcc246fa
Sebastian Schmidt <mrschmidt@google.com>
2020-05-29 23:54:30 -0700

    Throw when user data parsing fails

--- packages/firestore/lite/test/integration.test.ts
+++ packages/firestore/lite/test/integration.test.ts
@@ -23,7 +23,12 @@ import {
   getFirestore,
   initializeFirestore
 } from '../src/api/database';
-import { withTestDb, withTestDoc, withTestDocAndInitialData } from './helpers';
+import {
+  withTestDb,
+  withTestDbSettings,
+  withTestDoc,
+  withTestDocAndInitialData
+} from './helpers';
 import {
   parent,
   collection,
@@ -35,6 +40,10 @@ import {
   setDoc
 } from '../src/api/reference';
 import { FieldPath } from '../src/api/field_path';
+import {
+  DEFAULT_PROJECT_ID,
+  DEFAULT_SETTINGS
+} from '../../test/integration/util/settings';
 
 describe('Firestore', () => {
   it('can provide setting', () => {
@@ -221,6 +230,29 @@ describe('setDoc()', () => {
         baz: { foobar: 3 }
       });
     });
+  });
+
+  it("rejects 'undefined' by default", () => {
+    return withTestDoc(async docRef => {
+      expect(() => {
+        setDoc(docRef, { val: undefined });
+      }).to.throw(
+        'Function setDoc() called with invalid data. Unsupported field value: undefined (found in field val)'
+      );
+    });
+  });
+
+  it("ignores 'undefined' when enabled", () => {
+    return withTestDbSettings(
+      DEFAULT_PROJECT_ID,
+      { ...DEFAULT_SETTINGS, ignoreUndefinedProperties: true },
+      async db => {
+        const docRef = doc(collection(db, 'test-collection'));
+        await setDoc(docRef, { val: undefined });
+        const docSnap = await getDoc(docRef);
+        expect(docSnap.data()).to.deep.equal({});
+      }
+    );
   });
 });
 
