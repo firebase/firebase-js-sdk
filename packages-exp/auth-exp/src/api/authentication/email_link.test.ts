d14d5081d9d667bbfbae4ce47d378a0a2e310958
Sam Horlbeck Olsen <sam.horlsen@gmail.com>
2020-06-17 09:57:18 -0700

    Add link and reauth implementation to email credential (#3226)
    * Add linking and reauth to email credential
    
    * Formatting
    
    * Add tests for new api method
    
    * Formatting

--- packages-exp/auth-exp/src/api/authentication/email_link.test.ts
+++ packages-exp/auth-exp/src/api/authentication/email_link.test.ts
@@ -26,16 +26,14 @@ import { testAuth } from '../../../test/mock_auth';
 import * as mockFetch from '../../../test/mock_fetch';
 import { Auth } from '../../model/auth';
 import { ServerError } from '../errors';
-import { signInWithEmailLink } from './email_link';
+import {
+  signInWithEmailLink,
+  signInWithEmailLinkForLinking
+} from './email_link';
 
 use(chaiAsPromised);
 
-describe('api/authentication/signInWithEmailLink', () => {
-  const request = {
-    email: 'foo@bar.com',
-    oobCode: 'my-code'
-  };
-
+describe('api/authentication/email_link', () => {
   let auth: Auth;
 
   beforeEach(async () => {
@@ -45,44 +43,102 @@ describe('api/authentication/signInWithEmailLink', () => {
 
   afterEach(mockFetch.tearDown);
 
-  it('should POST to the correct endpoint', async () => {
-    const mock = mockEndpoint(Endpoint.SIGN_IN_WITH_EMAIL_LINK, {
-      displayName: 'my-name',
-      email: 'test@foo.com'
+  context('signInWithEmailLink', () => {
+    const request = {
+      email: 'foo@bar.com',
+      oobCode: 'my-code'
+    };
+
+    it('should POST to the correct endpoint', async () => {
+      const mock = mockEndpoint(Endpoint.SIGN_IN_WITH_EMAIL_LINK, {
+        displayName: 'my-name',
+        email: 'test@foo.com'
+      });
+
+      const response = await signInWithEmailLink(auth, request);
+      expect(response.displayName).to.eq('my-name');
+      expect(response.email).to.eq('test@foo.com');
+      expect(mock.calls[0].request).to.eql(request);
+      expect(mock.calls[0].method).to.eq('POST');
+      expect(mock.calls[0].headers).to.eql({
+        'Content-Type': 'application/json',
+        'X-Client-Version': 'testSDK/0.0.0'
+      });
     });
 
-    const response = await signInWithEmailLink(auth, request);
-    expect(response.displayName).to.eq('my-name');
-    expect(response.email).to.eq('test@foo.com');
-    expect(mock.calls[0].request).to.eql(request);
-    expect(mock.calls[0].method).to.eq('POST');
-    expect(mock.calls[0].headers).to.eql({
-      'Content-Type': 'application/json',
-      'X-Client-Version': 'testSDK/0.0.0'
+    it('should handle errors', async () => {
+      const mock = mockEndpoint(
+        Endpoint.SIGN_IN_WITH_EMAIL_LINK,
+        {
+          error: {
+            code: 400,
+            message: ServerError.INVALID_EMAIL,
+            errors: [
+              {
+                message: ServerError.INVALID_EMAIL
+              }
+            ]
+          }
+        },
+        400
+      );
+
+      await expect(signInWithEmailLink(auth, request)).to.be.rejectedWith(
+        FirebaseError,
+        'Firebase: The email address is badly formatted. (auth/invalid-email).'
+      );
+      expect(mock.calls[0].request).to.eql(request);
     });
   });
 
-  it('should handle errors', async () => {
-    const mock = mockEndpoint(
-      Endpoint.SIGN_IN_WITH_EMAIL_LINK,
-      {
-        error: {
-          code: 400,
-          message: ServerError.INVALID_EMAIL,
-          errors: [
-            {
-              message: ServerError.INVALID_EMAIL
-            }
-          ]
-        }
-      },
-      400
-    );
+  context('signInWithEmailLinkForLinking', () => {
+    const request = {
+      email: 'foo@bar.com',
+      oobCode: 'my-code',
+      idToken: 'id-token-2'
+    };
 
-    await expect(signInWithEmailLink(auth, request)).to.be.rejectedWith(
-      FirebaseError,
-      'Firebase: The email address is badly formatted. (auth/invalid-email).'
-    );
-    expect(mock.calls[0].request).to.eql(request);
+    it('should POST to the correct endpoint', async () => {
+      const mock = mockEndpoint(Endpoint.SIGN_IN_WITH_EMAIL_LINK, {
+        displayName: 'my-name',
+        email: 'test@foo.com'
+      });
+
+      const response = await signInWithEmailLinkForLinking(auth, request);
+      expect(response.displayName).to.eq('my-name');
+      expect(response.email).to.eq('test@foo.com');
+      expect(mock.calls[0].request).to.eql(request);
+      expect(mock.calls[0].method).to.eq('POST');
+      expect(mock.calls[0].headers).to.eql({
+        'Content-Type': 'application/json',
+        'X-Client-Version': 'testSDK/0.0.0'
+      });
+    });
+
+    it('should handle errors', async () => {
+      const mock = mockEndpoint(
+        Endpoint.SIGN_IN_WITH_EMAIL_LINK,
+        {
+          error: {
+            code: 400,
+            message: ServerError.INVALID_EMAIL,
+            errors: [
+              {
+                message: ServerError.INVALID_EMAIL
+              }
+            ]
+          }
+        },
+        400
+      );
+
+      await expect(
+        signInWithEmailLinkForLinking(auth, request)
+      ).to.be.rejectedWith(
+        FirebaseError,
+        'Firebase: The email address is badly formatted. (auth/invalid-email).'
+      );
+      expect(mock.calls[0].request).to.eql(request);
+    });
   });
 });
