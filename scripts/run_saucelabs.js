c7d4518c6aa3093d308f3abe53950ee96f2724c4
kai <zwu52@uw.edu>
2020-06-25 13:47:58 -0700

    Update run_saucelabs.js

--- scripts/run_saucelabs.js
+++ scripts/run_saucelabs.js
@@ -36,12 +36,13 @@ const { configFiles } = yargs
 // runNextTest() pulls filenames one-by-one from this queue.
 const testFiles = configFiles.length
   ? configFiles
-  : glob.sync(`{packages,integration}/*/karma.conf.js`).filter(
-      name =>
-        // Automated tests in integration/firestore and are currently disabled.
-        !name.includes('integration/firestore') &&
-        !name.includes('integration/messaging')
-    );
+  : glob
+      .sync(`{packages,integration}/*/karma.conf.js`)
+      .filter(
+        name =>
+          !name.includes('integration/firestore') &&
+          !name.includes('integration/messaging')
+      );
 
 // Get CI build number or generate one if running locally.
 const buildNumber =
