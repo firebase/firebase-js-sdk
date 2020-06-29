d3ca86c98c9869440269e55a3ab90a174628020c
Feiyang <feiyangc@google.com>
2020-06-29 14:26:00 -0700

    Make exp release script work again (#3301)
    * change to ts
    
    * use the correct file name
    
    * remove -exp from types packages when releasing
    
    * werid hack to make exp release build
    
    * skip tests in exp release
    
    * do not run test
    
    * revert changes made for testing
    
    * remove extraneous build target
    
    * Create warm-suns-dream.md

--- scripts/exp/remove-exp.js
+++ scripts/exp/remove-exp.js
@@ -25,22 +25,30 @@ const { readdirSync, statSync, readFileSync, writeFileSync } = require('fs');
 
 // can be used in command line
 if (argv._[0]) {
-  const dir = path.resolve(argv._[0]);
-  removeExpSuffix(dir);
+  const dirOrFile = path.resolve(argv._[0]);
+  if (statSync(dirOrFile).isFile()) {
+    removeExpSuffixFromFile(dirOrFile);
+  } else {
+    removeExpSuffix(dir);
+  }
 }
 
 function removeExpSuffix(dir) {
   for (const file of readdirSync(dir)) {
     const filePath = `${dir}/${file}`;
     if (statSync(filePath).isFile()) {
-      const content = readFileSync(filePath, 'utf-8');
-
-      // replace -exp with empty string
-      const modified = content.replace(/-exp/g, '');
-
-      writeFileSync(filePath, modified, 'utf-8');
+      removeExpSuffixFromFile(filePath);
     }
   }
+}
+
+function removeExpSuffixFromFile(filePath) {
+  const content = readFileSync(filePath, 'utf-8');
+
+  // replace -exp with empty string
+  const modified = content.replace(/-exp/g, '');
+
+  writeFileSync(filePath, modified, 'utf-8');
 }
 
 exports.removeExpSuffix = removeExpSuffix;
