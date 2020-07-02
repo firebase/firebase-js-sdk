7461415506ea5d7e4493bef56c070d5baca1f2a9
Wu-Hui <wu.hui.github@gmail.com>
2020-07-02 11:50:26 -0400

    Test batch EF

--- packages/firestore/scripts/extract-api.ts
+++ packages/firestore/scripts/extract-api.ts
@@ -16,8 +16,9 @@
  */
 
 // eslint-disable-next-line import/no-extraneous-dependencies
-import * as ts from 'typescript';
 import * as fs from 'fs';
+
+import * as ts from 'typescript';
 
 function extractIdentifiersFromNodeAndChildren(
   node: ts.Node,
