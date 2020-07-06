bfa985b142fefd82a1725e4097be23a59aa5fb93
Sam Horlbeck Olsen <sam.horlsen@gmail.com>
2020-07-06 12:21:15 -0700

    Add popup util and bare-bones OAuthProvider (#3335)

---
+++ packages-exp/auth-exp/src/core/providers/oauth.ts
@@ -0,0 +1,80 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as externs from '@firebase/auth-types-exp';
+
+import { AuthErrorCode } from '../errors';
+import { debugFail } from '../util/assert';
+
+export type CustomParameters = Record<string, string>;
+
+interface CredentialParameters {
+  idToken?: string;
+  accessToken?: string;
+  rawNonce?: string;
+}
+
+export class OAuthProvider implements externs.AuthProvider {
+  defaultLanguageCode: string | null = null;
+  private scopes: string[] = [];
+  private customParameters: CustomParameters = {};
+  private constructor(readonly providerId: externs.ProviderId) {}
+  static credentialFromResult(
+    _userCredential: externs.UserCredential
+  ): externs.OAuthCredential | null {
+    debugFail('not implemented');
+  }
+  static credentialFromError(
+    _error: AuthErrorCode
+  ): externs.OAuthCredential | null {
+    debugFail('not implemented');
+  }
+  static credentialFromJSON(_json: object): externs.OAuthCredential {
+    debugFail('not implemented');
+  }
+
+  credential(_params: CredentialParameters): externs.OAuthCredential {
+    debugFail('not implemented');
+  }
+
+  setDefaultLanguage(languageCode: string | null): void {
+    this.defaultLanguageCode = languageCode;
+  }
+
+  setCustomParameters(
+    customOAuthParameters: CustomParameters
+  ): externs.AuthProvider {
+    this.customParameters = customOAuthParameters;
+    return this;
+  }
+
+  getCustomParameters(): CustomParameters {
+    return this.customParameters;
+  }
+
+  addScope(scope: string): externs.AuthProvider {
+    // If not already added, add scope to list.
+    if (!this.scopes.includes(scope)) {
+      this.scopes.push(scope);
+    }
+    return this;
+  }
+
+  getScopes(): string[] {
+    return [...this.scopes];
+  }
+}
