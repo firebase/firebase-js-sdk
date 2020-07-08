303f85ddc8ecae884113c6740328b917de38180f
Sam Horlbeck Olsen <sam.horlsen@gmail.com>
2020-07-08 09:57:32 -0700

    Tree shakeable persistence (#3288)
    * Make the persistence objects tree-shakeable
    
    * Formatting
    
    * Updated to stop using casts everywhere
    
    * Formatting
    
    * Make the browser impls tree shakeable
    
    * Formatting
    
    * Fix the react native implementation in auth-compat-exp
    
    * Formatting
    
    * Make the react native class wrapped in a closure, to avoid the prototype nonsense
    
    * Formatting
    
    * Fix index.rn.ts
    
    * Formatting
    
    * PR feedback
    
    * Fix ordering in index.ts
    
    * Formatting
    
    * Fix test
    
    * Formatting

--- packages-exp/auth-exp/src/core/persistence/react_native.ts
+++ packages-exp/auth-exp/src/core/persistence/react_native.ts
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-import { ReactNativeAsyncStorage } from '@firebase/auth-types-exp';
+import * as externs from '@firebase/auth-types-exp';
 
 import {
   Persistence,
@@ -25,36 +25,50 @@ import {
 } from './';
 
 /**
- * Persistence class that wraps AsyncStorage imported from `react-native` or `@react-native-community/async-storage`.
+ * Returns a persistence class that wraps AsyncStorage imported from
+ * `react-native` or `@react-native-community/async-storage`.
+ *
+ * Creates a "new"-able subclass on the fly that has an empty constructor.
+ *
+ * In the _getInstance() implementation (see src/core/persistence/index.ts),
+ * we expect each "externs.Persistence" object passed to us by the user to
+ * be able to be instantiated (as a class) using "new". That function also
+ * expects the constructor to be empty. Since ReactNativeStorage requires the
+ * underlying storage layer, we need to be able to create subclasses
+ * (closures, esentially) that have the storage layer but empty constructor.
  */
-export class ReactNativePersistence implements Persistence {
-  readonly type: PersistenceType = PersistenceType.LOCAL;
 
-  constructor(private readonly storage: ReactNativeAsyncStorage) {}
+export function getReactNativePersistence(
+  storage: externs.ReactNativeAsyncStorage
+): externs.Persistence {
+  return class implements Persistence {
+    static type: 'LOCAL' = 'LOCAL';
+    readonly type: PersistenceType = PersistenceType.LOCAL;
 
-  async isAvailable(): Promise<boolean> {
-    try {
-      if (!this.storage) {
+    async isAvailable(): Promise<boolean> {
+      try {
+        if (!storage) {
+          return false;
+        }
+        await storage.setItem(STORAGE_AVAILABLE_KEY, '1');
+        await storage.removeItem(STORAGE_AVAILABLE_KEY);
+        return true;
+      } catch {
         return false;
       }
-      await this.storage.setItem(STORAGE_AVAILABLE_KEY, '1');
-      await this.storage.removeItem(STORAGE_AVAILABLE_KEY);
-      return true;
-    } catch {
-      return false;
     }
-  }
 
-  async set(key: string, value: PersistenceValue): Promise<void> {
-    await this.storage.setItem(key, JSON.stringify(value));
-  }
+    set(key: string, value: PersistenceValue): Promise<void> {
+      return storage.setItem(key, JSON.stringify(value));
+    }
 
-  async get<T extends PersistenceValue>(key: string): Promise<T | null> {
-    const json = await this.storage.getItem(key);
-    return json ? JSON.parse(json) : null;
-  }
+    async get<T extends PersistenceValue>(key: string): Promise<T | null> {
+      const json = await storage.getItem(key);
+      return json ? JSON.parse(json) : null;
+    }
 
-  async remove(key: string): Promise<void> {
-    await this.storage.removeItem(key);
-  }
+    remove(key: string): Promise<void> {
+      return storage.removeItem(key);
+    }
+  };
 }
