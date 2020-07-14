37e930c08bcebf957bde785cd3472a439763e92a
Sam Horlbeck Olsen <sam.horlsen@gmail.com>
2020-07-14 11:52:13 -0700

    Add popup strategy implementation, (sign in, link, reauth) (#3363)
    * Popup strategy implementation
    
    * Popup strategy + tests
    
    * Formatting
    
    * Updated comment
    
    * Fix IdpTask types
    
    * Refactor popup code to have abstract base
    
    * WIP
    
    * Early feedback
    
    * Feedback, tests
    
    * Formatting
    
    * Formatting
    
    * Formatting

---
+++ packages-exp/auth-exp/src/core/strategies/popup.ts
@@ -0,0 +1,178 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as externs from '@firebase/auth-types-exp';
+
+import { Auth } from '../../model/auth';
+import {
+  AuthEventType,
+  PopupRedirectResolver
+} from '../../model/popup_redirect';
+import { User } from '../../model/user';
+import { AUTH_ERROR_FACTORY, AuthErrorCode } from '../errors';
+import { Delay } from '../util/delay';
+import { _generateEventId } from '../util/event_id';
+import { _getInstance } from '../util/instantiator';
+import { AuthPopup } from '../util/popup';
+import { AbstractPopupRedirectOperation } from './abstract_popup_redirect_operation';
+
+// The event timeout is the same on mobile and desktop, no need for Delay.
+export const _AUTH_EVENT_TIMEOUT = 2020;
+export const _POLL_WINDOW_CLOSE_TIMEOUT = new Delay(2000, 10000);
+
+export async function signInWithPopup(
+  authExtern: externs.Auth,
+  provider: externs.AuthProvider,
+  resolverExtern: externs.PopupRedirectResolver
+): Promise<externs.UserCredential> {
+  const auth = authExtern as Auth;
+  const resolver: PopupRedirectResolver = _getInstance(resolverExtern);
+
+  const action = new PopupOperation(
+    auth,
+    AuthEventType.SIGN_IN_VIA_POPUP,
+    provider,
+    resolver
+  );
+  return action.execute();
+}
+
+export async function reauthenticateWithPopup(
+  userExtern: externs.User,
+  provider: externs.AuthProvider,
+  resolverExtern: externs.PopupRedirectResolver
+): Promise<externs.UserCredential> {
+  const user = userExtern as User;
+  const resolver: PopupRedirectResolver = _getInstance(resolverExtern);
+
+  const action = new PopupOperation(
+    user.auth,
+    AuthEventType.REAUTH_VIA_POPUP,
+    provider,
+    resolver,
+    user
+  );
+  return action.execute();
+}
+
+export async function linkWithPopup(
+  userExtern: externs.User,
+  provider: externs.AuthProvider,
+  resolverExtern: externs.PopupRedirectResolver
+): Promise<externs.UserCredential> {
+  const user = userExtern as User;
+  const resolver: PopupRedirectResolver = _getInstance(resolverExtern);
+
+  const action = new PopupOperation(
+    user.auth,
+    AuthEventType.LINK_VIA_POPUP,
+    provider,
+    resolver,
+    user
+  );
+  return action.execute();
+}
+
+/**
+ * Popup event manager. Handles the popup's entire lifecycle; listens to auth
+ * events
+ */
+class PopupOperation extends AbstractPopupRedirectOperation {
+  // Only one popup is ever shown at once. The lifecycle of the current popup
+  // can be managed / cancelled by the constructor.
+  private static currentPopupAction: PopupOperation | null = null;
+  private authWindow: AuthPopup | null = null;
+  private pollId: number | null = null;
+
+  constructor(
+    auth: Auth,
+    readonly filter: AuthEventType,
+    private readonly provider: externs.AuthProvider,
+    resolver: PopupRedirectResolver,
+    user?: User
+  ) {
+    super(auth, filter, resolver, user);
+    if (PopupOperation.currentPopupAction) {
+      PopupOperation.currentPopupAction.cancel();
+    }
+
+    PopupOperation.currentPopupAction = this;
+  }
+
+  async onExecution(): Promise<void> {
+    const eventId = _generateEventId();
+    this.authWindow = await this.resolver._openPopup(
+      this.auth,
+      this.provider,
+      this.filter,
+      eventId
+    );
+    this.authWindow.associatedEvent = eventId;
+
+    // Handle user closure. Notice this does *not* use await
+    this.pollUserCancellation(this.auth.name);
+  }
+
+  get eventId(): string | null {
+    return this.authWindow?.associatedEvent || null;
+  }
+
+  cancel(): void {
+    this.reject(
+      AUTH_ERROR_FACTORY.create(AuthErrorCode.EXPIRED_POPUP_REQUEST, {
+        appName: this.auth.name
+      })
+    );
+  }
+
+  cleanUp(): void {
+    if (this.authWindow) {
+      this.authWindow.close();
+    }
+
+    if (this.pollId) {
+      window.clearTimeout(this.pollId);
+    }
+
+    this.authWindow = null;
+    this.pollId = null;
+    PopupOperation.currentPopupAction = null;
+  }
+
+  private pollUserCancellation(appName: string): void {
+    const poll = (): void => {
+      if (this.authWindow?.window?.closed) {
+        // Make sure that there is sufficient time for whatever action to
+        // complete. The window could have closed but the sign in network
+        // call could still be in flight.
+        this.pollId = window.setTimeout(() => {
+          this.pollId = null;
+          this.reject(
+            AUTH_ERROR_FACTORY.create(AuthErrorCode.POPUP_CLOSED_BY_USER, {
+              appName
+            })
+          );
+        }, _AUTH_EVENT_TIMEOUT);
+        return;
+      }
+
+      this.pollId = window.setTimeout(poll, _POLL_WINDOW_CLOSE_TIMEOUT.get());
+    };
+
+    poll();
+  }
+}
