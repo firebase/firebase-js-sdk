18c8906541458e08ec18f30c365494495e598064
Sam Olsen <samgho@google.com>
2020-07-15 14:09:46 -0700

    Fix typo

--- packages-exp/auth-exp/src/core/auth/auth_impl.ts
+++ packages-exp/auth-exp/src/core/auth/auth_impl.ts
@@ -19,13 +19,7 @@ import { getApp } from '@firebase/app-exp';
 import { FirebaseApp } from '@firebase/app-types-exp';
 import * as externs from '@firebase/auth-types-exp';
 import {
-  CompleteFn,
-  createSubscribe,
-  ErrorFn,
-  NextFn,
-  Observer,
-  Subscribe,
-  Unsubscribe
+    CompleteFn, createSubscribe, ErrorFn, NextFn, Observer, Subscribe, Unsubscribe
 } from '@firebase/util';
 
 import { Auth, Dependencies } from '../../model/auth';
@@ -34,8 +28,7 @@ import { User } from '../../model/user';
 import { AuthErrorCode } from '../errors';
 import { Persistence } from '../persistence';
 import {
-  _REDIRECT_USER_KEY_NAME,
-  PersistenceUserManager
+    _REDIRECT_USER_KEY_NAME, PersistenceUserManager
 } from '../persistence/persistence_user_manager';
 import { _reloadWithoutSaving } from '../user/reload';
 import { assert } from '../util/assert';
@@ -200,7 +193,7 @@ export class AuthImpl implements Auth {
     }
 
     if (this.redirectUser?._redirectEventId === id) {
-      return this.currentUser;
+      return this.redirectUser;
     }
 
     return null;
