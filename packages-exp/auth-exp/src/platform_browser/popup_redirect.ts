8892cee68df8539edc67cfeb1730ca470ef081db
Sam Olsen <samgho@google.com>
2020-07-16 13:23:26 -0700

    Fix bug

--- packages-exp/auth-exp/src/platform_browser/popup_redirect.ts
+++ packages-exp/auth-exp/src/platform_browser/popup_redirect.ts
@@ -29,11 +29,7 @@ import { _getCurrentUrl } from '../core/util/location';
 import { _open, AuthPopup } from '../core/util/popup';
 import { ApiKey, AppName, Auth } from '../model/auth';
 import {
-  AuthEventType,
-  EventManager,
-  GapiAuthEvent,
-  GapiOutcome,
-  PopupRedirectResolver
+    AuthEventType, EventManager, GapiAuthEvent, GapiOutcome, PopupRedirectResolver
 } from '../model/popup_redirect';
 import { _setWindowLocation } from './auth_window';
 import { _openIframe } from './iframe/iframe';
@@ -65,7 +61,7 @@ class BrowserPopupRedirectResolver implements PopupRedirectResolver {
     console.log(this.eventManagers[auth._key()]);
     console.log(auth._key());
     debugAssert(
-      this.eventManagers[auth._key()],
+      this.eventManagers[auth._key()]?.manager,
       '_initialize() not called before _openPopup()'
     );
     const url = getRedirectUrl(auth, provider, authType, eventId);
