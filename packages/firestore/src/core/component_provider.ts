a1ae872cca8b876d479e0c0496955b8ba15abb52
Sebastian Schmidt <mrschmidt@google.com>
2020-07-17 14:27:14 -0700

    Remove code duplication

--- packages/firestore/src/core/component_provider.ts
+++ packages/firestore/src/core/component_provider.ts
@@ -164,12 +164,7 @@ export class IndexedDbOfflineComponentProvider extends MemoryOfflineComponentPro
   gcScheduler!: GarbageCollectionScheduler | null;
 
   async initialize(cfg: ComponentConfiguration): Promise<void> {
-    this.sharedClientState = this.createSharedClientState(cfg);
-    this.persistence = this.createPersistence(cfg);
-    await this.persistence.start();
-    this.gcScheduler = this.createGarbageCollectionScheduler(cfg);
-    this.localStore = this.createLocalStore(cfg);
-
+    await super.initialize(cfg);
     await synchronizeLastDocumentChangeReadTime(this.localStore);
   }
 
