b0b197a186ede8587f12efc29fbebbd23d4fa916
XuechunHou <xhou1@ualberta.ca>
2020-07-21 13:57:19 -0600

    init functions

--- size-analysis-web-app/functions/src/index.ts
+++ size-analysis-web-app/functions/src/index.ts
@@ -16,11 +16,14 @@
  */
 
 import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-admin.initializeApp(functions.config().firebase);
+
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!');
 });
+
+export const retrieveFirebaseVersionFromNPM = functions.https.onRequest(
+  (request, response) => {}
+);
--- size-analysis-web-app/src/FirebaseConfig.jsx
+++ size-analysis-web-app/src/FirebaseConfig.jsx
@@ -1,14 +1,13 @@
 import firebase from '@firebase/app';
 import 'firebase/functions';
 export const firebaseConfig = {
-  apiKey: "AIzaSyCtglORaF_Dd5BWPFqewtb9zTxJF7ZaIHw",
-  authDomain: "web-app-modular-size-analysis.firebaseapp.com",
-  databaseURL: "https://web-app-modular-size-analysis.firebaseio.com",
-  projectId: "web-app-modular-size-analysis",
-  storageBucket: "web-app-modular-size-analysis.appspot.com",
-  messagingSenderId: "106892363430",
-  appId: "1:106892363430:web:7285676f8b5291343a5667",
-  measurementId: "G-RE3DMC6WLC"
+  apiKey: "AIzaSyAHVFTx8JcgKAIY22_tdXwwZHbi-txE03M",
+  authDomain: "web-app-size-analysis.firebaseapp.com",
+  databaseURL: "https://web-app-size-analysis.firebaseio.com",
+  projectId: "web-app-size-analysis",
+  storageBucket: "web-app-size-analysis.appspot.com",
+  messagingSenderId: "151230240052",
+  appId: "1:151230240052:web:d48ef603ef709809934161",
+  measurementId: "G-MFTZDGSG8M"
 };
-
 const fire = firebase.initializeApp(firebaseConfig);
