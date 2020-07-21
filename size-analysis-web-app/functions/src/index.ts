47b30bb53674bdc05a58d6bff4c58d2c12d57fe8
XuechunHou <xhou1@ualberta.ca>
2020-07-21 11:55:18 -0600

    intialized functions

--- size-analysis-web-app/functions/src/index.ts
+++ size-analysis-web-app/functions/src/index.ts
@@ -16,7 +16,8 @@
  */
 
 import * as functions from 'firebase-functions';
-
+import * as admin from 'firebase-admin';
+admin.initializeApp(functions.config().firebase);
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
