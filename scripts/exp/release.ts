e1b2164227bef1c18ab156ae77197f7fcbb1c20b
Feiyang1 <plane1113@gmail.com>
2020-07-21 21:00:29 -0700

    change order

--- scripts/exp/release.ts
+++ scripts/exp/release.ts
@@ -40,14 +40,14 @@ async function publishExpPackages() {
     console.log('Welcome to the Firebase Exp Packages release CLI!');
 
     /**
-     * build packages
-     */
-    await buildPackages();
-
-    /**
      * Update fields in package.json and stuff
      */
     await prepareFirestoreForRelease();
+
+    /**
+     * build packages
+     */
+    await buildPackages();
 
     // path to exp packages
     const packagePaths = await mapWorkspaceToPackages([
