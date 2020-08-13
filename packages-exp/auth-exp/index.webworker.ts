a16df27339d005664f627b7d52db9b44dba88cea
Sam Olsen <samgho@google.com>
2020-08-13 11:12:23 -0700

    Formatting

--- packages-exp/auth-exp/index.webworker.ts
+++ packages-exp/auth-exp/index.webworker.ts
@@ -23,7 +23,7 @@ import { ClientPlatform } from './src/core/util/version';
 
 // Core functionality shared by all clients
 export * from './src';
-export {indexedDBLocalPersistence} from './src/platform_browser/persistence/indexed_db';
+export { indexedDBLocalPersistence } from './src/platform_browser/persistence/indexed_db';
 
 export const initializeAuth = _initializeAuthForClientPlatform(
   ClientPlatform.WORKER
