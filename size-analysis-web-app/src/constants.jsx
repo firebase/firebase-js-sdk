305aa3f1d7c039f89bab3b77f90f47dac9f8329b
XuechunHou <xhou1@ualberta.ca>
2020-08-17 17:18:33 -0600

    a working web application

---
+++ size-analysis-web-app/src/constants.jsx
@@ -0,0 +1,34 @@
+export const COLOR = {
+    blue: '#059BE5',
+    orange: '#F6820D',
+    darkYellow: '#FFA611',
+    yellow: '#FFCB2B',
+    lightOrange: '#FF8964',
+    white: '#f0f2eb'
+}
+
+export const SECTION = {
+    bundleCreation: 'Create Your Bundle',
+    bundleOverview: 'Bundle Overview'
+
+}
+export const TEXT = {
+    calculateBtnText: 'Calculate',
+    addButtonText: '+',
+    deleteButtonText: 'x',
+    moduleRepresentation: '*',
+    sizePrompt: 'Size',
+    sizeAfterGzipPrompt: 'Size After Gzip',
+    unit: 'bytes',
+    outdatedBadgeText: 'Outdated'
+}
+
+export const ENDPOINTS = {
+    retrieveFirebaseVersionFromNPM: 'retrieveFirebaseVersionFromNPM',
+    downloadPackageFromNPMGivenVersionAndReturnExportedSymbols: 'downloadPackageFromNPMGivenVersionAndReturnExportedSymbols',
+    generateSizeAnalysisReportGivenCustomBundle: 'generateSizeAnalysisReportGivenCustomBundle'
+}
+
+export const API_ROOT = 'https://us-central1-fir-sdk-size-analysis.cloudfunctions.net/';
+//export const API_ROOT = 'http://localhost:5001/fir-sdk-size-analysis/us-central1/';
+
