09b620ce9088f79054c81640a83208b5ad5bfd55
Sebastian Schmidt <mrschmidt@google.com>
2020-08-20 13:46:22 +0200

    Remove firestore-type import

--- packages/firestore/rollup.config.node.memory.js
+++ packages/firestore/rollup.config.node.memory.js
@@ -35,7 +35,10 @@ export default [
         'process.env.FIRESTORE_PROTO_ROOT': JSON.stringify('src/protos')
       })
     ],
-    external: util.resolveNodeExterns
+    external: util.resolveNodeExterns,
+    treeshake: {
+      moduleSideEffects: false
+    }
   },
   {
     input: path.resolve('./memory', memoryPkg['main-esm2017']),
@@ -45,6 +48,9 @@ export default [
       sourcemap: true
     },
     plugins: util.es2017ToEs5Plugins(),
-    external: util.resolveNodeExterns
+    external: util.resolveNodeExterns,
+    treeshake: {
+      moduleSideEffects: false
+    }
   }
 ];
