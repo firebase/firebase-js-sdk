c4ba5135085eb6aa67ffc4a6c71c4c178d2d1a32
Denver Coneybeare <dconeybe@google.com>
2020-08-26 11:16:06 -0400

    Add changeset

--- packages/firestore/exp/test/shim.ts
+++ packages/firestore/exp/test/shim.ts
@@ -627,7 +627,8 @@ export class DocumentChange<T = legacy.DocumentData>
   readonly newIndex = this._delegate.oldIndex;
 }
 
-export class CollectionReference<T = legacy.DocumentData> extends Query<T>
+export class CollectionReference<T = legacy.DocumentData>
+  extends Query<T>
   implements legacy.CollectionReference<T> {
   constructor(
     firestore: FirebaseFirestore,
