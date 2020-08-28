146e0f47c70f00732623d160bff97e8a55cd60f1
Sebastian Schmidt <mrschmidt@google.com>
2020-08-28 17:27:45 +0200

    Remove exclusive tag

--- packages/firestore/test/unit/specs/offline_spec.test.ts
+++ packages/firestore/test/unit/specs/offline_spec.test.ts
@@ -240,21 +240,17 @@ describeSpec('Offline:', [], () => {
     }
   );
 
-  specTest(
-    'Client stays offline during credential change',
-    ['exclusive'],
-    () => {
-      // Reproduces a bug that caused the client to switch to OnlineState
-      // `Unknown` during a credential change.
+  specTest('Client stays offline during credential change', [], () => {
+    // Reproduces a bug that caused the client to switch to OnlineState
+    // `Unknown` during a credential change.
 
-      const query1 = query('collection');
-      return spec()
-        .disableNetwork()
-        .changeUser('user1')
-        .userListens(query1)
-        .expectEvents(query1, {
-          fromCache: true
-        });
-    }
-  );
+    const query1 = query('collection');
+    return spec()
+      .disableNetwork()
+      .changeUser('user1')
+      .userListens(query1)
+      .expectEvents(query1, {
+        fromCache: true
+      });
+  });
 });
