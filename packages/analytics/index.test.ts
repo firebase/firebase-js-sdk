63c1dbfc3a43c8bea95dd19f64ff77980ac326f8
Christina Holland <hsubox@gmail.com>
2020-09-01 12:10:41 -0700

    Fix merge conflicts

--- packages/analytics/index.test.ts
+++ packages/analytics/index.test.ts
@@ -174,11 +174,9 @@ describe('FirebaseAnalytics instance tests', () => {
       fetchStub.restore();
     });
     it('Contains reference to parent app', () => {
-      stubFetch(200, {});
       expect(analyticsInstance.app).to.equal(app);
     });
     it('Calls gtag correctly on logEvent (instance)', async () => {
-      stubFetch(200, { measurementId: fakeMeasurementId });
       analyticsInstance.logEvent(EventName.ADD_PAYMENT_INFO, {
         currency: 'USD'
       });
@@ -247,7 +245,6 @@ describe('FirebaseAnalytics instance tests', () => {
       fetchStub.restore();
     });
     it('Calls gtag correctly on logEvent (instance)', async () => {
-      stubFetch(200, { measurementId: fakeMeasurementId });
       analyticsInstance.logEvent(EventName.ADD_PAYMENT_INFO, {
         currency: 'USD'
       });
