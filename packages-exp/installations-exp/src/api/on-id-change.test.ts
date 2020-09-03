f506e18facf8f47eff61cf83a98af4b1f2e94319
ChaoqunCHEN <cqchen93@gmail.com>
2020-09-03 13:40:32 -0700

    Fis modularization (#3638)
    * copied orginal FIS sdk into packages-expp and made unit tests passed
    
    * migrated test app and fixed a test app bug to make it worked
    
    * made installations-exp depend on app-exp
    
    * Relocated public methods files into  folder
    
    * Making installations mudularization step1, build success
    
    * Making installations mudularization step2, unit tests pass
    
    * update dependency version, merge master
    
    * Apply suggestions from code review
    
    Co-authored-by: Feiyang <feiyangc@google.com>
    
    * update dependencies' version
    
    * add getInstallation(app)
    
    * correct deleteInstallations funciton name
    
    * Place the call to registerVerion and registerInstallations at the same place
    
    * remove dead code
    
    * add api extractor config
    
    * rewrite the internal interface
    
    * fix build error
    
    * Seperate internal interface from public interface.
    
    * Change public methods to accept public interface
    
    * Fixes and api extractor integration for @firebase/installations-exp (#3733)
    
    * integrate api-extractor into installations-exp
    
    * add release tag to APIs
    
    * add _ prefix to the internal interface
    
    * update api report
    
    Co-authored-by: Feiyang <feiyangc@google.com>

---
+++ packages-exp/installations-exp/src/api/on-id-change.test.ts
@@ -0,0 +1,52 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { expect } from 'chai';
+import { stub } from 'sinon';
+import '../testing/setup';
+import { onIdChange } from './on-id-change';
+import * as FidChangedModule from '../helpers/fid-changed';
+import { getFakeInstallations } from '../testing/fake-generators';
+import { FirebaseInstallationsImpl } from '../interfaces/installation-impl';
+
+describe('onIdChange', () => {
+  let installations: FirebaseInstallationsImpl;
+
+  beforeEach(() => {
+    installations = getFakeInstallations();
+    stub(FidChangedModule);
+  });
+
+  it('calls addCallback with the given callback and app key when called', () => {
+    const callback = stub();
+    onIdChange(installations, callback);
+    expect(FidChangedModule.addCallback).to.have.been.calledOnceWith(
+      installations.appConfig,
+      callback
+    );
+  });
+
+  it('calls removeCallback with the given callback and app key when unsubscribe is called', () => {
+    const callback = stub();
+    const unsubscribe = onIdChange(installations, callback);
+    unsubscribe();
+    expect(FidChangedModule.removeCallback).to.have.been.calledOnceWith(
+      installations.appConfig,
+      callback
+    );
+  });
+});
