655dab801dc24f6d3c1bde8a8cb511ebf5073db4
Brian Chen <chenbrian@google.com>
2020-09-10 15:53:26 -0500

    fix test

--- packages/firestore/test/integration/api/validation.test.ts
+++ packages/firestore/test/integration/api/validation.test.ts
@@ -589,18 +589,6 @@ apiDescribe('Validation:', (persistence: boolean) => {
         });
     });
 
-    validationIt(
-      persistence,
-      'validates CollectionReference.add with correct function name',
-      db => {
-        const coll = db.collection('foo');
-
-        expect(() => coll.add(new TestClass('foo'))).to.throw(
-          'Function CollectionReference.add() called with invalid data.'
-        );
-      }
-    );
-
     validationIt(persistence, 'must not contain undefined.', db => {
       // Note: This test uses the default setting for `ignoreUndefinedProperties`.
       return expectWriteToFail(
