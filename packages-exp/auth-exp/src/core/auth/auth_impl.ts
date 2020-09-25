6e4fdba4a5e36679e128f46b8fd9ac1df5cd9039
Sam Olsen <samgho@google.com>
2020-09-25 10:15:52 -0700

    Fix issue in node

--- packages-exp/auth-exp/src/core/auth/auth_impl.ts
+++ packages-exp/auth-exp/src/core/auth/auth_impl.ts
@@ -222,7 +222,10 @@ export class AuthImplCompat<T extends User> implements Auth, _FirebaseService {
 
   async signOut(): Promise<void> {
     // Clear the redirect user when signOut is called
-    await this._setRedirectUser(null);
+    if (this.redirectPersistenceManager || this._popupRedirectResolver) {
+      await this._setRedirectUser(null);
+    }
+    
     return this.updateCurrentUser(null);
   }
 
