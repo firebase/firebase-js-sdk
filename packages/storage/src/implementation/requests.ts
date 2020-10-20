2b06694da69915352c24cb26f570a129fc04868b
Christina Holland <hsubox@gmail.com>
2020-10-20 12:50:44 -0700

    Fix storage-types (#3945)

--- packages/storage/src/implementation/requests.ts
+++ packages/storage/src/implementation/requests.ts
@@ -114,7 +114,7 @@ export function sharedErrorHandler(
         }
       }
     }
-    newErr.setServerResponseProp(err.serverResponseProp());
+    newErr.serverResponse = err.serverResponse;
     return newErr;
   }
   return errorHandler;
@@ -133,7 +133,7 @@ export function objectErrorHandler(
     if (xhr.getStatus() === 404) {
       newErr = objectNotFound(location.path);
     }
-    newErr.setServerResponseProp(err.serverResponseProp());
+    newErr.serverResponse = err.serverResponse;
     return newErr;
   }
   return errorHandler;
