3eb33e327869ba9b4cad074560e02396859953bf
Sebastian Schmidt <mrschmidt@google.com>
2020-11-20 16:37:00 -0700

    Update input_validation.ts

--- packages/firestore/src/util/input_validation.ts
+++ packages/firestore/src/util/input_validation.ts
@@ -20,7 +20,6 @@ import { fail } from './assert';
 import { Code, FirestoreError } from './error';
 import { DocumentKey } from '../model/document_key';
 import { ResourcePath } from '../model/path';
-import { Compat } from '../compat/compat';
 
 /** Types accepted by validateType() and related methods for validation. */
 export type ValidationType =
