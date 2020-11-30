bf9b50cad72382579b4c7fed19c514fa74eb8b96
Sebastian Schmidt <mrschmidt@google.com>
2020-11-30 14:29:11 -0700

    Update equality_matcher.ts

--- packages/firestore/test/util/equality_matcher.ts
+++ packages/firestore/test/util/equality_matcher.ts
@@ -62,8 +62,8 @@ function customDeepEqual(
     // compared with API types from Firestore classic. We do want to
     // differentiate between these types in our tests to ensure that the we do
     // not return firestore-exp types in the classic SDK.
-    let leftObj = left as Record<string, unknown>;
-    let rightObj = right as Record<string, unknown>;
+    const leftObj = left as Record<string, unknown>;
+    const rightObj = right as Record<string, unknown>;
     if (
       leftObj.constructor.name === rightObj.constructor.name &&
       leftObj.constructor !== rightObj.constructor
