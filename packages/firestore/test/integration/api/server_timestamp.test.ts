95974e222d48cc61be8dcda86e0d483947b6c076
Brian Chen <chenbrian@google.com>
2020-12-21 14:10:53 -0800

    remove outdated test

--- packages/firestore/test/integration/api/server_timestamp.test.ts
+++ packages/firestore/test/integration/api/server_timestamp.test.ts
@@ -207,34 +207,6 @@ apiDescribe('Server Timestamps', (persistence: boolean) => {
     });
   });
 
-  it('can return previous value', () => {
-    // The following test includes an update of the nested map "deep", which
-    // updates it to contain a single ServerTimestamp. This update is split
-    // into two mutations: One that sets "deep" to an empty map and overwrites
-    // the previous ServerTimestamp value and a second transform that writes
-    // the new ServerTimestamp. This step in the test verifies that we can
-    // still access the old ServerTimestamp value (from `previousSnapshot`) even
-    // though it was removed in an intermediate step.
-
-    let previousSnapshot: firestore.DocumentSnapshot;
-
-    return withTestSetup(() => {
-      return writeInitialData()
-        .then(() => docRef.update(updateData))
-        .then(() => accumulator.awaitLocalEvent())
-        .then(snapshot => verifyTimestampsUsePreviousValue(snapshot, null))
-        .then(() => accumulator.awaitRemoteEvent())
-        .then(snapshot => {
-          previousSnapshot = snapshot;
-        })
-        .then(() => docRef.update(updateData))
-        .then(() => accumulator.awaitLocalEvent())
-        .then(snapshot =>
-          verifyTimestampsUsePreviousValue(snapshot, previousSnapshot)
-        );
-    });
-  });
-
   it('can return previous value of different type', () => {
     return withTestSetup(() => {
       return writeInitialData()
