a9668184463682533a83900019d313a6f076f400
Sebastian Schmidt <mrschmidt@google.com>
2021-02-05 13:58:45 -0700

    Draft: RTDB exp API

--- packages/database-types/index.d.ts
+++ packages/database-types/index.d.ts
@@ -32,24 +32,22 @@ export interface DataSnapshot {
   val(): any;
 }
 
-export interface Database {
+export class FirebaseDatabase {
+  private constructor();
+  'type': 'database';
+
   app: FirebaseApp;
-  useEmulator(host: string, port: number): void;
-  goOffline(): void;
-  goOnline(): void;
-  ref(path?: string | Reference): Reference;
-  refFromURL(url: string): Reference;
 }
 
-export class FirebaseDatabase implements Database {
-  private constructor();
-  app: FirebaseApp;
-  useEmulator(host: string, port: number): void;
-  goOffline(): void;
-  goOnline(): void;
-  ref(path?: string | Reference): Reference;
-  refFromURL(url: string): Reference;
-}
+export function useDatabaseEmulator(
+  db: FirebaseDatabase,
+  host: string,
+  port: number
+): void;
+export function goOffline(db: FirebaseDatabase): void;
+export function goOnline(db: FirebaseDatabase): void;
+export function ref(db: FirebaseDatabase, path?: string | Reference): Reference;
+export function refFromURL(db: FirebaseDatabase, url: string): Reference;
 
 export interface OnDisconnect {
   cancel(onComplete?: (a: Error | null) => any): Promise<void>;
@@ -71,64 +69,96 @@ type EventType =
   | 'child_removed';
 
 export interface Query {
-  endAt(value: number | string | boolean | null, key?: string): Query;
-  equalTo(value: number | string | boolean | null, key?: string): Query;
-  isEqual(other: Query | null): boolean;
-  limitToFirst(limit: number): Query;
-  limitToLast(limit: number): Query;
-  off(
-    eventType?: EventType,
-    callback?: (a: DataSnapshot, b?: string | null) => any,
-    context?: Object | null
-  ): void;
-  get(): Promise<DataSnapshot>;
-  on(
-    eventType: EventType,
-    callback: (a: DataSnapshot, b?: string | null) => any,
-    cancelCallbackOrContext?: ((a: Error) => any) | Object | null,
-    context?: Object | null
-  ): (a: DataSnapshot, b?: string | null) => any;
-  once(
-    eventType: EventType,
-    successCallback?: (a: DataSnapshot, b?: string | null) => any,
-    failureCallbackOrContext?: ((a: Error) => void) | Object | null,
-    context?: Object | null
-  ): Promise<DataSnapshot>;
-  orderByChild(path: string): Query;
-  orderByKey(): Query;
-  orderByPriority(): Query;
-  orderByValue(): Query;
   ref: Reference;
-  startAt(value: number | string | boolean | null, key?: string): Query;
+  isEqual(other: Query | null): boolean;
   toJSON(): Object;
   toString(): string;
 }
 
-export interface Reference extends Query {
-  child(path: string): Reference;
-  key: string | null;
-  onDisconnect(): OnDisconnect;
-  parent: Reference | null;
-  push(value?: any, onComplete?: (a: Error | null) => any): ThenableReference;
-  remove(onComplete?: (a: Error | null) => any): Promise<any>;
-  root: Reference;
-  set(value: any, onComplete?: (a: Error | null) => any): Promise<any>;
-  setPriority(
-    priority: string | number | null,
-    onComplete: (a: Error | null) => any
-  ): Promise<any>;
-  setWithPriority(
-    newVal: any,
-    newPriority: string | number | null,
-    onComplete?: (a: Error | null) => any
-  ): Promise<any>;
-  transaction(
-    transactionUpdate: (a: any) => any,
-    onComplete?: (a: Error | null, b: boolean, c: DataSnapshot | null) => any,
-    applyLocally?: boolean
-  ): Promise<any>;
-  update(values: Object, onComplete?: (a: Error | null) => any): Promise<any>;
+export function get(query: Query): Promise<DataSnapshot>;
+export function on(
+  query: Query,
+  eventType: EventType,
+  callback: (a: DataSnapshot, b?: string | null) => any,
+  cancelCallbackOrContext?: ((a: Error) => any) | Object | null,
+  context?: Object | null
+): (a: DataSnapshot, b?: string | null) => any;
+export function off(
+  query: Query,
+  eventType?: EventType,
+  callback?: (a: DataSnapshot, b?: string | null) => any,
+  context?: Object | null
+): void;
+
+export interface QueryConstraint {
+  type:
+    | 'endAt'
+    | 'endBefore'
+    | 'startAt'
+    | 'startAfter'
+    | 'limitToFirst'
+    | 'limitToLast'
+    | 'orderByChild'
+    | 'orderByKey'
+    | 'orderByPriority'
+    | 'orderByValue'
+    | 'equalTo';
 }
+export function endAt(
+  value: number | string | boolean | null,
+  key?: string
+): QueryConstraint;
+export function endBefore(
+  value: number | string | boolean | null,
+  key?: string
+): QueryConstraint;
+export function startAt(
+  value: number | string | boolean | null,
+  key?: string
+): QueryConstraint;
+export function startAftr(
+  value: number | string | boolean | null,
+  key?: string
+): QueryConstraint;
+export function limitToFirst(limit: number): QueryConstraint;
+export function limitToLast(limit: number): QueryConstraint;
+export function orderByChild(path: string): QueryConstraint;
+export function orderByKey(): QueryConstraint;
+export function orderByPriority(): QueryConstraint;
+export function orderByValue(): QueryConstraint;
+export function equalTo(
+  value: number | string | boolean | null,
+  key?: string
+): QueryConstraint;
+
+export function query(query: Query, ...constraints: QueryConstraint[]): Query;
+
+export interface Reference extends Query {
+  key: string | null;
+  parent: Reference | null;
+  root: Reference;
+}
+
+export function child(ref: Reference, path: string): Reference;
+export function onDisconnect(ref: Reference): OnDisconnect;
+export function push(ref: Reference, value?: unknown): ThenableReference;
+export function remove(ref: Reference): Promise<void>;
+export function set(ref: Reference, value: unknown): Promise<void>;
+export function setPriority(
+  ref: Reference,
+  priority: string | number | null
+): Promise<void>;
+export function setWithPriority(
+  ref: Reference,
+  newVal: any,
+  newPriority: string | number | null
+): Promise<void>;
+export function transaction(
+  ref: Reference,
+  transactionUpdate: (a: any) => unknown,
+  applyLocally?: boolean
+): Promise<void>;
+export function update(ref: Reference, values: Object): Promise<void>;
 
 export interface ServerValue {
   TIMESTAMP: Object;
