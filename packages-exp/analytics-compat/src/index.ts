636dd78eab0641448ed87487abf1db321a07f578
Christina Holland <hsubox@gmail.com>
2021-02-24 12:23:27 -0800

    Implement analytics-compat package (#4460)

--- packages-exp/analytics-compat/src/index.ts
+++ packages-exp/analytics-compat/src/index.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2020 Google LLC
+ * Copyright 2021 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,49 +15,60 @@
  * limitations under the License.
  */
 
-import firebase, {
-  _FirebaseNamespace,
-  FirebaseApp
-} from '@firebase/app-compat';
-import { FunctionsService } from './service';
+import { firebase, _FirebaseNamespace } from '@firebase/app-compat';
+import { name, version } from '../package.json';
+import { AnalyticsService } from './service';
 import {
   Component,
+  ComponentContainer,
   ComponentType,
-  InstanceFactory,
-  ComponentContainer
+  InstanceFactory
 } from '@firebase/component';
-import { Functions as FunctionsServiceExp } from '@firebase/functions-exp';
+import { FirebaseApp } from '@firebase/app-types';
+import {
+  Analytics as AnalyticsServiceExp,
+  settings as settingsExp,
+  isSupported as isSupportedExp
+} from '@firebase/analytics-exp';
+import { EventName } from './constants';
 
 declare module '@firebase/component' {
   interface NameServiceMapping {
     'app-compat': FirebaseApp;
-    'functions-compat': FunctionsService;
-    'functions-exp': FunctionsServiceExp;
+    'analytics-compat': AnalyticsService;
+    'analytics-exp': AnalyticsServiceExp;
   }
 }
 
-const factory: InstanceFactory<'functions-compat'> = (
+const factory: InstanceFactory<'analytics-compat'> = (
   container: ComponentContainer,
   regionOrCustomDomain?: string
 ) => {
   // Dependencies
   const app = container.getProvider('app-compat').getImmediate();
-  const functionsServiceExp = container
-    .getProvider('functions-exp')
+  const analyticsServiceExp = container
+    .getProvider('analytics-exp')
     .getImmediate({
       identifier: regionOrCustomDomain
     });
 
-  return new FunctionsService(app, functionsServiceExp);
+  return new AnalyticsService(app as FirebaseApp, analyticsServiceExp);
 };
 
-export function registerFunctions(): void {
+export function registerAnalytics(): void {
   const namespaceExports = {
-    Functions: FunctionsService
+    Analytics: AnalyticsService,
+    settings: settingsExp,
+    isSupported: isSupportedExp,
+    // We removed this enum in exp so need to re-create it here for compat.
+    EventName
   };
   (firebase as _FirebaseNamespace).INTERNAL.registerComponent(
-    new Component('functions-compat', factory, ComponentType.PUBLIC)
+    new Component('analytics-compat', factory, ComponentType.PUBLIC)
       .setServiceProps(namespaceExports)
       .setMultipleInstances(true)
   );
 }
+
+registerAnalytics();
+firebase.registerVersion(name, version);
