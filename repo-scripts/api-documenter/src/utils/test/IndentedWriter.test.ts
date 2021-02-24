66da592cab2231a3c04d9c80422b1bfa3af83466
Feiyang1 <plane1113@gmail.com>
2021-02-23 16:24:46 -0800

    replace jest with mocha

--- repo-scripts/api-documenter/src/utils/test/IndentedWriter.test.ts
+++ repo-scripts/api-documenter/src/utils/test/IndentedWriter.test.ts
@@ -18,78 +18,82 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-// import { IndentedWriter } from '../IndentedWriter';
+import { IndentedWriter } from '../IndentedWriter';
+import { expect, use } from 'chai';
+import { jestSnapshotPlugin } from 'mocha-chai-jest-snapshot';
 
-// test('01 Demo from docs', () => {
-//   const indentedWriter: IndentedWriter = new IndentedWriter();
-//   indentedWriter.write('begin\n');
-//   indentedWriter.increaseIndent();
-//   indentedWriter.write('one\ntwo\n');
-//   indentedWriter.decreaseIndent();
-//   indentedWriter.increaseIndent();
-//   indentedWriter.decreaseIndent();
-//   indentedWriter.write('end');
+use(jestSnapshotPlugin());
 
-//   expect(indentedWriter.toString()).toMatchSnapshot();
-// });
+it('01 Demo from docs', () => {
+  const indentedWriter: IndentedWriter = new IndentedWriter();
+  indentedWriter.write('begin\n');
+  indentedWriter.increaseIndent();
+  indentedWriter.write('one\ntwo\n');
+  indentedWriter.decreaseIndent();
+  indentedWriter.increaseIndent();
+  indentedWriter.decreaseIndent();
+  indentedWriter.write('end');
 
-// test('02 Indent something', () => {
-//   const indentedWriter: IndentedWriter = new IndentedWriter();
-//   indentedWriter.write('a');
-//   indentedWriter.write('b');
-//   indentedWriter.increaseIndent();
-//   indentedWriter.writeLine('c');
-//   indentedWriter.writeLine('d');
-//   indentedWriter.decreaseIndent();
-//   indentedWriter.writeLine('e');
+  expect(indentedWriter.toString()).toMatchSnapshot();
+});
 
-//   indentedWriter.increaseIndent('>>> ');
-//   indentedWriter.writeLine();
-//   indentedWriter.writeLine();
-//   indentedWriter.writeLine('g');
-//   indentedWriter.decreaseIndent();
+it('02 Indent something', () => {
+  const indentedWriter: IndentedWriter = new IndentedWriter();
+  indentedWriter.write('a');
+  indentedWriter.write('b');
+  indentedWriter.increaseIndent();
+  indentedWriter.writeLine('c');
+  indentedWriter.writeLine('d');
+  indentedWriter.decreaseIndent();
+  indentedWriter.writeLine('e');
 
-//   expect(indentedWriter.toString()).toMatchSnapshot();
-// });
+  indentedWriter.increaseIndent('>>> ');
+  indentedWriter.writeLine();
+  indentedWriter.writeLine();
+  indentedWriter.writeLine('g');
+  indentedWriter.decreaseIndent();
 
-// test('03 Two kinds of indents', () => {
-//   const indentedWriter: IndentedWriter = new IndentedWriter();
+  expect(indentedWriter.toString()).toMatchSnapshot();
+});
 
-//   indentedWriter.writeLine('---');
-//   indentedWriter.indentScope(() => {
-//     indentedWriter.write('a\nb');
-//     indentedWriter.indentScope(() => {
-//       indentedWriter.write('c\nd\n');
-//     });
-//     indentedWriter.write('e\n');
-//   }, '> ');
-//   indentedWriter.writeLine('---');
+it('03 Two kinds of indents', () => {
+  const indentedWriter: IndentedWriter = new IndentedWriter();
 
-//   expect(indentedWriter.toString()).toMatchSnapshot();
-// });
+  indentedWriter.writeLine('---');
+  indentedWriter.indentScope(() => {
+    indentedWriter.write('a\nb');
+    indentedWriter.indentScope(() => {
+      indentedWriter.write('c\nd\n');
+    });
+    indentedWriter.write('e\n');
+  }, '> ');
+  indentedWriter.writeLine('---');
 
-// test('04 Edge cases for ensureNewLine()', () => {
-//   let indentedWriter: IndentedWriter = new IndentedWriter();
-//   indentedWriter.ensureNewLine();
-//   indentedWriter.write('line');
-//   expect(indentedWriter.toString()).toMatchSnapshot();
+  expect(indentedWriter.toString()).toMatchSnapshot();
+});
 
-//   indentedWriter = new IndentedWriter();
-//   indentedWriter.write('previous');
-//   indentedWriter.ensureNewLine();
-//   indentedWriter.write('line');
-//   expect(indentedWriter.toString()).toMatchSnapshot();
-// });
+it('04 Edge cases for ensureNewLine()', () => {
+  let indentedWriter: IndentedWriter = new IndentedWriter();
+  indentedWriter.ensureNewLine();
+  indentedWriter.write('line');
+  expect(indentedWriter.toString()).toMatchSnapshot();
 
-// test('04 Edge cases for ensureSkippedLine()', () => {
-//   let indentedWriter: IndentedWriter = new IndentedWriter();
-//   indentedWriter.ensureSkippedLine();
-//   indentedWriter.write('line');
-//   expect(indentedWriter.toString()).toMatchSnapshot();
+  indentedWriter = new IndentedWriter();
+  indentedWriter.write('previous');
+  indentedWriter.ensureNewLine();
+  indentedWriter.write('line');
+  expect(indentedWriter.toString()).toMatchSnapshot();
+});
 
-//   indentedWriter = new IndentedWriter();
-//   indentedWriter.write('previous');
-//   indentedWriter.ensureSkippedLine();
-//   indentedWriter.write('line');
-//   expect(indentedWriter.toString()).toMatchSnapshot();
-// });
+it('04 Edge cases for ensureSkippedLine()', () => {
+  let indentedWriter: IndentedWriter = new IndentedWriter();
+  indentedWriter.ensureSkippedLine();
+  indentedWriter.write('line');
+  expect(indentedWriter.toString()).toMatchSnapshot();
+
+  indentedWriter = new IndentedWriter();
+  indentedWriter.write('previous');
+  indentedWriter.ensureSkippedLine();
+  indentedWriter.write('line');
+  expect(indentedWriter.toString()).toMatchSnapshot();
+});
