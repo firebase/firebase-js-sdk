39e3f3542fa3e15b52c24c56395b76e23f4b770b
Sam Olsen <samgho@google.com>
2021-03-11 09:31:42 -0800

    PR feedback

--- packages-exp/auth-exp/test/integration/flows/idp.local.test.ts
+++ packages-exp/auth-exp/test/integration/flows/idp.local.test.ts
@@ -231,4 +231,43 @@ describe('Integration test: headless IdP', () => {
       signInWithEmailAndPassword(auth, email, 'password')
     ).to.be.rejectedWith(FirebaseError, 'auth/wrong-password');
   });
+
+  it('IdP accounts automatically link with verified emails', async () => {
+    const googleCredential = GoogleAuthProvider.credential(
+      JSON.stringify({
+        sub: email,
+        email: email,
+        email_verified: true
+      })
+    );
+
+    const githubCredential = GithubAuthProvider.credential(
+      JSON.stringify({
+        sub: email,
+        email: email,
+        email_verified: true
+      })
+    );
+
+    // First sign in with Google
+    const {user: initialUser} = await signInWithCredential(auth, googleCredential);
+    expect(initialUser.providerData.length).to.eq(1);
+    expect(initialUser.providerData[0].providerId).to.eq('google.com');
+
+    await auth.signOut();
+
+    // Now with GitHub
+    const {user: githubUser} = await signInWithCredential(auth, githubCredential);
+    expect(githubUser.uid).to.eq(initialUser.uid);
+    expect(githubUser.providerData.length).to.eq(2);
+    expect(githubUser.providerData.map(p => p.providerId)).to.have.members(['google.com', 'github.com']);
+
+    await auth.signOut();
+
+    // Sign in once again with the initial credential
+    const {user: googleUser} = await signInWithCredential(auth, googleCredential);
+    expect(googleUser.uid).to.eq(initialUser.uid);
+    expect(googleUser.providerData.length).to.eq(2);
+    expect(googleUser.providerData.map(p => p.providerId)).to.have.members(['google.com', 'github.com']);
+  });
 });
