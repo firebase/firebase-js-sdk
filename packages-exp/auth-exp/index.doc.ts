87e9843abd148ac0b6564c25597976d858f16618
Sam Olsen <samgho@google.com>
2021-07-02 15:15:31 -0700

    Add an index.doc.ts file that includes all exports for auto-generated docs

---
+++ packages-exp/auth-exp/index.doc.ts
@@ -0,0 +1,8 @@
+// Internal index that pulls in all the different platforms, for documentation
+// generation purposes only.
+
+// Core (Browser) index
+export * from './index';
+
+export { cordovaPopupRedirectResolver } from './index.cordova';
+export { reactNativeLocalPersistence } from './index.rn';
