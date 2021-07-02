3827d9692a169d9b315ce2a7640f37cc65ce0237
Sam <samgho@google.com>
2021-07-02 09:04:16 -0700

    [Auth] Break down event ID digits to a max Math.random() digit count (#5098)
    * [Auth] Break down event ID digits to a max Math.random() digit count
    
    * License/formatting
    
    * src/core/util/event_id.ts

--- packages-exp/auth-exp/src/core/util/event_id.test.ts
+++ packages-exp/auth-exp/src/core/util/event_id.test.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2020 Google LLC
+ * Copyright 2021 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,10 +15,19 @@
  * limitations under the License.
  */
 
-export function _generateEventId(prefix = '', digits = 10): string {
-  let random = '';
-  for (let i = 0; i < digits; i++) {
-    random += Math.floor(Math.random() * 10);
-  }
-  return prefix + random;
-}
+import { expect } from 'chai';
+import { _generateEventId } from './event_id';
+
+describe('core/util/event_id', () => {
+  it('sub-15 digit id', () => {
+    expect(_generateEventId('', 10)).to.have.length(10);
+  });
+
+  it('15 digit id', () => {
+    expect(_generateEventId('', 15)).to.have.length(15);
+  });
+
+  it('above-15 digit id', () => {
+    expect(_generateEventId('', 20)).to.have.length(20);
+  });
+});
