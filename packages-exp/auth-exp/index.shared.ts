a10eec5c165b8ed5a4b9c0d647ae3ec52ca91e53
Sam <samgho@google.com>
2021-08-18 16:51:16 -0700

    Make node and browser entry points identical (#5318)
    * Overhaul node/browser entry points so that they are identical; use rollup-plugin-alias to overwrite browser-specific features
    
    * Formatting, license
    
    * Revert bad change
    
    * Update packages-exp/auth-exp/rollup.config.shared.js
    
    Co-authored-by: Feiyang <feiyangc@google.com>
    
    * Fix popup/redirect stuff
    
    * Fix popup/redirect stuff
    
    Co-authored-by: Feiyang <feiyangc@google.com>

--- packages-exp/auth-exp/index.shared.ts
+++ packages-exp/auth-exp/index.shared.ts
@@ -15,5 +15,17 @@
  * limitations under the License.
  */
 
-// Node entry point is identical to browser entry point
-export * from './index';
+// Public types
+export * from './src/model/public_types';
+
+// Helper maps (not used internally)
+export {
+  FactorId,
+  ProviderId,
+  SignInMethod,
+  OperationType,
+  ActionCodeOperation
+} from './src/model/enum_maps';
+
+// Core functionality shared by all clients
+export * from './src';
