d330090b124a43f6afce066a1a84a04e9b01f0ee
Feiyang1 <plane1113@gmail.com>
2021-08-20 23:31:57 -0700

    update build scripts

--- packages/firebase/rollup.config.js
+++ packages/firebase/rollup.config.js
@@ -56,7 +56,7 @@ const appBuilds = [
       { file: resolve('app', appPkg.module), format: 'es', sourcemap: true }
     ],
     plugins: [...plugins, typescriptPlugin],
-    external: id => deps.some(dep => id === dep || id.startsWith(`${dep}/`))
+    external: id => external.some(dep => id === dep || id.startsWith(`${dep}/`))
   }
 ];
 
@@ -81,7 +81,7 @@ const componentBuilds = pkg.components
           }
         ],
         plugins: [...plugins, typescriptPlugin],
-        external: id => deps.some(dep => id === dep || id.startsWith(`${dep}/`)),
+        external: id => external.some(dep => id === dep || id.startsWith(`${dep}/`)),
       }
     ];
   })
