0b75e9f46c9d78c5d36374b922bcdaa188c49611
Feiyang <feiyangc@google.com>
2021-08-24 12:17:13 -0700

    Update reference doc generation (#5333)
    * show functions table first
    
    * create toc only for entry points

--- repo-scripts/api-documenter/src/documenters/MarkdownDocumenter.ts
+++ repo-scripts/api-documenter/src/documenters/MarkdownDocumenter.ts
@@ -918,6 +918,11 @@ export class MarkdownDocumenter {
       }
     }
 
+    if (functionsTable.rows.length > 0) {
+      output.push(new DocHeading({ configuration, title: 'Functions' }));
+      output.push(functionsTable);
+    }
+
     if (classesTable.rows.length > 0) {
       output.push(new DocHeading({ configuration, title: 'Classes' }));
       output.push(classesTable);
@@ -926,10 +931,6 @@ export class MarkdownDocumenter {
     if (enumerationsTable.rows.length > 0) {
       output.push(new DocHeading({ configuration, title: 'Enumerations' }));
       output.push(enumerationsTable);
-    }
-    if (functionsTable.rows.length > 0) {
-      output.push(new DocHeading({ configuration, title: 'Functions' }));
-      output.push(functionsTable);
     }
 
     if (interfacesTable.rows.length > 0) {
