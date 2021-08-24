96592bc90e2af94e124b156aa3f36c78d44789fb
Yuchen Shi <yuchenshi@google.com>
2021-08-23 17:41:11 -0700

    Use default param (review feedback).

--- packages/rules-unit-testing/src/impl/test_environment.ts
+++ packages/rules-unit-testing/src/impl/test_environment.ts
@@ -200,9 +200,8 @@ class RulesTestContextImpl implements RulesTestContext {
     );
     return database;
   }
-  storage(bucketUrl?: string): firebase.storage.Storage {
+  storage(bucketUrl = `gs://${this.projectId}`): firebase.storage.Storage {
     assertEmulatorRunning(this.emulators, 'storage');
-    bucketUrl ||= `gs://${this.projectId}`;
     const storage = this.getApp().storage(bucketUrl);
     storage.useEmulator(
       this.emulators.storage.host,
