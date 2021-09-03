fc6e392de32de8d1d805c5dcfd52460f9c3822b9
Dharmaraj <63334359+DharmarajX24@users.noreply.github.com>
2021-09-03 13:34:28 +0530

    updated error message for invalid first argument in doc()

--- packages/firestore/src/lite-api/reference.ts
+++ packages/firestore/src/lite-api/reference.ts
@@ -517,7 +517,7 @@ export function doc<T>(
     ) {
       throw new FirestoreError(
         Code.INVALID_ARGUMENT,
-        'Expected first argument to collection() to be a CollectionReference, ' +
+        'Expected first argument to doc() to be a CollectionReference, ' +
           'a DocumentReference or FirebaseFirestore'
       );
     }
