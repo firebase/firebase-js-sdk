95de9cad7a40d7c9dd72a1d224cf151308243643
Christina Holland <hsubox@gmail.com>
2021-09-13 14:29:00 -0700

    add debug messages

--- scripts/ci/log-changesets.ts
+++ scripts/ci/log-changesets.ts
@@ -1,17 +1,38 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { ClientRequest, IncomingMessage } from 'http';
 
 const https = require('https');
 
 async function logChangesets() {
+  console.log(process.env.GITHUB_EVENT_PATH);
+
   if (!process.env.GITHUB_EVENT_PATH) return;
 
   const prPayload = require(process.env.GITHUB_EVENT_PATH);
 
-  if (prPayload.title !== 'Version Packages') return;
+  console.log(prPayload.title);
+
+  // if (prPayload.title !== 'Version Packages') return;
 
   const matches = prPayload.body.match(/## firebase@([\d\.]+)/);
   const version = matches[1];
-  
+
   const data = JSON.stringify({
     version,
     pr: prPayload.number
@@ -28,15 +49,18 @@ async function logChangesets() {
   };
 
   return new Promise((resolve, reject) => {
-    const req: ClientRequest = https.request(options, (res: IncomingMessage) => {
-      res.on('data', d => {
-        process.stdout.write(d);
-      });
-      res.on('end', resolve);
-    });
-  
+    const req: ClientRequest = https.request(
+      options,
+      (res: IncomingMessage) => {
+        res.on('data', d => {
+          process.stdout.write(d);
+        });
+        res.on('end', resolve);
+      }
+    );
+
     req.on('error', error => reject(error));
-  
+
     req.write(data);
     req.end();
   });
