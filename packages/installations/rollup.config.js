6a09a1a509c7520b4cd561c89bf2ea9602a624e4
Feiyang1 <plane1113@gmail.com>
2021-10-07 17:02:25 -0700

    report build variants for installations

--- packages/installations/rollup.config.js
+++ packages/installations/rollup.config.js
@@ -17,8 +17,10 @@
 
 import json from '@rollup/plugin-json';
 import typescriptPlugin from 'rollup-plugin-typescript2';
+import replace from 'rollup-plugin-replace';
 import typescript from 'typescript';
 import pkg from './package.json';
+import { generateBuildTargetReplaceConfig } from '../../scripts/build/rollup_replace_build_target';
 
 const deps = [
   ...Object.keys({ ...pkg.peerDependencies, ...pkg.dependencies })
@@ -33,11 +35,18 @@ const es5Builds = [
   {
     input: 'src/index.ts',
     output: [
-      { file: pkg.main, format: 'cjs', sourcemap: true },
+      { file: pkg.main, format: 'cjs', sourcemap: true }
+    ],
+    external: id => deps.some(dep => id === dep || id.startsWith(`${dep}/`)),
+    plugins: [...es5BuildPlugins, replace(generateBuildTargetReplaceConfig('cjs', 5))]
+  },
+  {
+    input: 'src/index.ts',
+    output: [
       { file: pkg.esm5, format: 'es', sourcemap: true }
     ],
     external: id => deps.some(dep => id === dep || id.startsWith(`${dep}/`)),
-    plugins: es5BuildPlugins
+    plugins: [...es5BuildPlugins, replace(generateBuildTargetReplaceConfig('esm', 5))]
   }
 ];
 
@@ -65,7 +74,7 @@ const es2017Builds = [
       sourcemap: true
     },
     external: id => deps.some(dep => id === dep || id.startsWith(`${dep}/`)),
-    plugins: es2017BuildPlugins
+    plugins: [...es2017BuildPlugins, replace(generateBuildTargetReplaceConfig('esm', 2017))]
   }
 ];
 
