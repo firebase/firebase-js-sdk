703ef779d24b473bbe87897c7d890dc0e866c4e6
Christina Holland <chholland@google.com>
2021-10-12 14:02:07 -0700

    Fix compat doc script (#5605)

--- scripts/docgen-compat/generate-docs.js
+++ scripts/docgen-compat/generate-docs.js
@@ -39,11 +39,12 @@ const { api: apiType, source: sourceFile } = yargs
   .version(false)
   .help().argv;
 
-const docPath = path.resolve(`${__dirname}/html/${apiType}`);
+const destinationDir = apiType === 'js' ? 'js/v8' : apiType;
+const docPath = path.resolve(`${__dirname}/html/${destinationDir}`);
 const contentPath = path.resolve(`${__dirname}/content-sources/${apiType}`);
 const tempHomePath = path.resolve(`${contentPath}/HOME_TEMP.md`);
 const tempNodeSourcePath = path.resolve(`${__dirname}/index.node.d.ts`);
-const devsitePath = `/docs/reference/${apiType}/`;
+const devsitePath = `/docs/reference/${destinationDir}/`;
 
 /**
  * Strips path prefix and returns only filename.
