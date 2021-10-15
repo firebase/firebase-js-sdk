192ac86c1acf11e949fb43304e26500259125c54
Christina Holland <chholland@google.com>
2021-10-15 15:59:03 -0700

    Set up formatting check and local formatting script (#5530)

--- scripts/format/license.ts
+++ scripts/format/license.ts
@@ -15,17 +15,13 @@
  * limitations under the License.
  */
 
-const { resolve } = require('path');
-const simpleGit = require('simple-git/promise');
-const fs = require('mz/fs');
-const ora = require('ora');
-const chalk = require('chalk');
+import fs from 'mz/fs';
+import chalk from 'chalk';
+import glob from 'glob';
 
-const root = resolve(__dirname, '../..');
-const git = simpleGit(root);
 const licenseHeader = `/**
  * @license
- * Copyright 2021 Google LLC
+ * Copyright ${new Date().getFullYear()} Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,7 +41,7 @@ const licenseHeader = `/**
 const copyrightPattern = /Copyright \d{4} Google (Inc\.|LLC)/;
 const oldCopyrightPattern = /(\s*\*\s*Copyright \d{4}) Google Inc\./;
 
-async function readFiles(paths) {
+async function readFiles(paths: string[]) {
   const fileContents = await Promise.all(paths.map(path => fs.readFile(path)));
   return fileContents.map((buffer, idx) => ({
     contents: String(buffer),
@@ -53,7 +49,7 @@ async function readFiles(paths) {
   }));
 }
 
-function addLicenceTag(contents) {
+function addLicenseTag(contents: string) {
   const lines = contents.split('\n');
   let newLines = [];
   for (const line of lines) {
@@ -66,7 +62,7 @@ function addLicenceTag(contents) {
   return newLines.join('\n');
 }
 
-function rewriteCopyrightLine(contents) {
+function rewriteCopyrightLine(contents: string) {
   const lines = contents.split('\n');
   let newLines = lines.map(line => {
     return line.replace(oldCopyrightPattern, (_, leader) => {
@@ -76,13 +72,25 @@ function rewriteCopyrightLine(contents) {
   return newLines.join('\n');
 }
 
-async function doLicense(changedFiles) {
-  const licenseSpinner = ora(' Validating License Headers').start();
+export async function doLicense(changedFiles?: string[]) {
+  let count = 0;
 
-  const paths = changedFiles.filter(line => line.match(/(js|ts)$/));
-  if (paths.length === 0) return;
+  let filesToChange: string[] = changedFiles ?? [];
 
-  const files = await readFiles(paths);
+  if (!changedFiles) {
+    filesToChange = await new Promise(resolve => {
+      glob(
+        '+(packages|repo-scripts)/**/*.+(ts|js)',
+        { ignore: ['**/node_modules/**', './node_modules/**', '**/dist/**'] },
+        (err, res) => resolve(res)
+      );
+    });
+  }
+
+  console.log(
+    chalk`{green Validating license headers in ${filesToChange.length} files.}`
+  );
+  const files = await readFiles(filesToChange);
 
   await Promise.all(
     files.map(({ contents, path }) => {
@@ -91,50 +99,32 @@ async function doLicense(changedFiles) {
       // Files with no license block at all.
       if (result.match(copyrightPattern) == null) {
         result = licenseHeader + result;
+        console.log(`Adding license to ${path}.`);
       }
 
       // Files with no @license tag.
       if (result.match(/@license/) == null) {
-        result = addLicenceTag(result);
+        result = addLicenseTag(result);
+        console.log(`Adding @license tag to ${path}.`);
       }
 
       // Files with the old form of copyright notice.
       if (result.match(oldCopyrightPattern) != null) {
         result = rewriteCopyrightLine(result);
+        console.log(`Updating old copyright notice found in ${path}.`);
       }
 
       if (contents !== result) {
+        count++;
         return fs.writeFile(path, result, 'utf8');
       } else {
         return Promise.resolve();
       }
     })
   );
-
-  licenseSpinner.stopAndPersist({
-    symbol: '✅'
-  });
-
-  // Diff unstaged (license writes) against staged.
-  const stageDiff = await git.diff(['--name-only']);
-
-  if (!stageDiff) {
-    console.log(chalk`\n{red License pass caused no changes.}\n`);
-    return;
+  if (count === 0) {
+    console.log(chalk`{green No files needed license changes.}`);
   } else {
-    console.log(
-      `License script modified ${stageDiff.split('\n').length - 1} files.`
-    );
+    console.log(chalk`{green ${count} files had license headers updated.}`);
   }
-
-  const gitSpinner = ora(' Git staging license text modifications.').start();
-  await git.add('.');
-
-  gitSpinner.stopAndPersist({
-    symbol: '▶️'
-  });
 }
-
-module.exports = {
-  doLicense
-};
---
+++ scripts/format/prettier.ts
@@ -0,0 +1,95 @@
+/**
+ * @license
+ * Copyright 2017 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { resolve } from 'path';
+import { exec, spawn } from 'child-process-promise';
+import chalk from 'chalk';
+
+const root = resolve(__dirname, '../..');
+const packageJson = require(root + '/package.json');
+
+async function checkVersion() {
+  const { stdout } = await exec('yarn prettier --version', {
+    cwd: root
+  });
+  const lines = stdout.split('\n');
+  let runtimeVersion;
+  for (const line of lines) {
+    if (line.match(/^\d+\.\d+\.\d+$/)) {
+      runtimeVersion = line;
+      break;
+    }
+  }
+  if (!runtimeVersion) {
+    console.warn('Was not able to find runtime version of prettier.');
+    return;
+  }
+  const packageVersion = packageJson.devDependencies.prettier;
+  if (packageVersion !== runtimeVersion) {
+    const mismatchText =
+      `Installed version of prettier (${runtimeVersion}) does not match ` +
+      `required version (${packageVersion}).`;
+    const versionMismatchMessage = chalk`
+      {red ${mismatchText}}
+
+      {yellow Please re-run {reset 'yarn'} from the root of the repo and try again.}
+      `;
+    throw new Error(versionMismatchMessage);
+  }
+}
+
+export async function doPrettier(changedFiles?: string[]) {
+  try {
+    await checkVersion();
+  } catch (e) {
+    console.error(e);
+    return process.exit(1);
+  }
+
+  let prettierArgs = [
+    'prettier',
+    '--config',
+    `${resolve(root, '.prettierrc')}`,
+    '--write'
+  ];
+
+  if (changedFiles) {
+    prettierArgs = [...prettierArgs, ...changedFiles];
+    console.log(
+      chalk`{green Validating ${changedFiles.length} files with Prettier}`
+    );
+  } else {
+    prettierArgs.push('{,!(node_modules)/}**/*.{js,ts}');
+    console.log(chalk`{green Validating all .js and .ts files with Prettier}`);
+  }
+
+  try {
+    await spawn('yarn', prettierArgs, {
+      stdio: 'inherit',
+      cwd: root
+    });
+  } catch (e) {
+    if ((e as NodeJS.ErrnoException).code === 'E2BIG') {
+      console.error(
+        chalk`{red Too many files, use a smaller pattern or use the --all flag.}`
+      );
+      process.exit();
+    } else {
+      throw e;
+    }
+  }
+}
