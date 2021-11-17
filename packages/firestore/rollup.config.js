ce39a1a07e8710e43cc66d9e7db882f185211a9a
Feiyang <feiyangc@google.com>
2021-11-17 11:43:43 -0800

    better meta url polyfill (#5738)
    * better meta url polyfill
    
    * Create quick-experts-shave.md
    
    * update

--- packages/firestore/rollup.config.js
+++ packages/firestore/rollup.config.js
@@ -29,6 +29,22 @@ import { generateBuildTargetReplaceConfig } from '../../scripts/build/rollup_rep
 
 const util = require('./rollup.shared');
 
+// Customize how import.meta.url is polyfilled in cjs nodejs build. We use it to be able to use require() in esm.
+// It only generates the nodejs version of the polyfill, as opposed to the default polyfill which
+// supports both browser and nodejs. The browser support is unnecessary and doesn't work well with Jest. See https://github.com/firebase/firebase-js-sdk/issues/5687
+function importMetaUrlPolyfillPlugin() {
+  return {
+    name: 'import-meta-url-current-module',
+    resolveImportMeta(property, { moduleId }) {
+      if (property === 'url') {
+        // copied from rollup output
+        return `new (require('url').URL)('file:' + __filename).href`;
+      }
+      return null;
+    }
+  };
+}
+
 const nodePlugins = function () {
   return [
     typescriptPlugin({
@@ -105,7 +121,8 @@ const allBuilds = [
     },
     plugins: [
       ...util.es2017ToEs5Plugins(/* mangled= */ false),
-      replace(generateBuildTargetReplaceConfig('cjs', 2017))
+      replace(generateBuildTargetReplaceConfig('cjs', 2017)),
+      importMetaUrlPolyfillPlugin()
     ],
     external: util.resolveNodeExterns,
     treeshake: {
