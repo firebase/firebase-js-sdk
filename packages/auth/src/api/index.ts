93231c3a8efc572ca1db51951526634584a35f8c
Chuan Ren <chuanr@google.com>
2022-01-26 23:45:49 -0800

    Update index.ts

--- packages/auth/src/api/index.ts
+++ packages/auth/src/api/index.ts
@@ -60,6 +60,8 @@ export const enum Endpoint {
   START_PHONE_MFA_SIGN_IN = '/v2/accounts/mfaSignIn:start',
   FINALIZE_PHONE_MFA_SIGN_IN = '/v2/accounts/mfaSignIn:finalize',
   WITHDRAW_MFA = '/v2/accounts/mfaEnrollment:withdraw',
+  GET_PROJECT_CONFIG = '/v1/projects'
+}
 
 export const enum RecaptchaClientType {
   WEB = 1,
