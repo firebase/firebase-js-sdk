aaeab08ac96133f8ec33ec41517453c566c3fdc3
Sebastian Schmidt <mrschmidt@google.com>
2022-01-31 11:11:11 -0700

    Fix compilation for MemoryRemoteDocumentCache (#5950)

--- packages/firestore/src/core/sync_engine_impl.ts
+++ packages/firestore/src/core/sync_engine_impl.ts
@@ -619,6 +619,9 @@ export async function syncEngineRejectListen(
     let documentUpdates = new SortedMap<DocumentKey, MutableDocument>(
       DocumentKey.comparator
     );
+    // TODO(b/217189216): This limbo document should ideally have a read time,
+    // so that it is picked up by any read-time based scans. The backend,
+    // however, does not send a read time for target removals.
     documentUpdates = documentUpdates.insert(
       limboKey,
       MutableDocument.newNoDocument(limboKey, SnapshotVersion.min())
