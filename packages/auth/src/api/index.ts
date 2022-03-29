65310143081c373d665ed3c98a5b256d9741a7e7
Chuan Ren <chuanr@google.com>
2022-03-29 09:51:14 -0700

    Update index.ts

--- packages/auth/src/api/index.ts
+++ packages/auth/src/api/index.ts
@@ -128,7 +128,8 @@ export async function _performApiRequest<T, V>(
     }
 
     return FetchProvider.fetch()(
-      _getFinalTarget(auth, auth.config.apiHost, path, query),
+      _getFinalTarget(auth, 'staging-identitytoolkit.sandbox.googleapis.com' , path, query),
+      // _getFinalTarget(auth, auth.config.apiHost, path, query),
       {
         method,
         headers,
