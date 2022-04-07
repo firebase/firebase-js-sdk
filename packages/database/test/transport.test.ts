8d0526b0778fd4dac1c34132d70d9f855c5aadb0
Maneesh Tewani <mtewani@google.com>
2022-04-07 16:27:34 -0700

    Removed xit

--- packages/database/test/transport.test.ts
+++ packages/database/test/transport.test.ts
@@ -6,7 +6,7 @@ import { BrowserPollConnection } from '../src/realtime/BrowserPollConnection';
 import { WebSocketConnection } from '../src/realtime/WebSocketConnection';
 
 describe('Force Transport', () => {
-  xit('should enable websockets and disable longPolling', () => {
+  it('should enable websockets and disable longPolling', () => {
     forceWebSockets();
     expect(WebSocketConnection.isAvailable()).to.equal(true);
     expect(BrowserPollConnection.isAvailable()).to.equal(false);
