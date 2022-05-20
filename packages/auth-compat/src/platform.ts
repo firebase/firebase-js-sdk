2542c4eb975778e23cbf5711fcfdc18a865de342
martindonadieu <martindonadieu@gmail.com>
2022-05-20 20:13:35 +0100

    fix: lint

--- packages/auth-compat/src/platform.ts
+++ packages/auth-compat/src/platform.ts
@@ -52,8 +52,8 @@ function _isHttpOrHttps(): boolean {
 export function _isAndroidOrIosCordovaScheme(ua: string = getUA()): boolean {
   return !!(
     (_getCurrentScheme() === 'file:' ||
-    _getCurrentScheme() === 'ionic:' ||
-    _getCurrentScheme() === 'capacitor:') &&
+      _getCurrentScheme() === 'ionic:' ||
+      _getCurrentScheme() === 'capacitor:') &&
     ua.toLowerCase().match(/iphone|ipad|ipod|android/)
   );
 
