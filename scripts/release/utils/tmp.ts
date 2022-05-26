1f4e16d4eac49f187bb50458eb1514a2d0a612da
Christina Holland <hsubox@gmail.com>
2022-05-26 11:34:40 -0700

    Update release tracker part

--- scripts/release/utils/tmp.ts
+++
@@ -1,8 +0,0 @@
-import { exec } from 'child-process-promise';
-
-async function main() {
-  const { stdout: version } = await exec('npm info firebase version');
-  console.log('-' + version.trim() + '-');
-}
-
-main();
