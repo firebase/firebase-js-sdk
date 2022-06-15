6f39fef07e81991665e76e8927e0ba38d1051ec3
Maneesh Tewani <mtewani@google.com>
2022-06-15 08:46:00 -0700

    Removed comments

--- packages/database/test/exp/integration.test.ts
+++ packages/database/test/exp/integration.test.ts
@@ -208,13 +208,6 @@ describe('Database@exp Tests', () => {
     const unsubscribe = onValue(nestedRef, snapshot => {
       ea.addEvent(snapshot.val());
     });
-    /*
-      1. Set the value.
-      2. Create the addEventRegistration /test
-        a. listen on /test
-      3. Get the value /
-      4. removeEventRegistration for / and then
-    */
     const result = await get(query(testRef));
     const events = await ea.promise;
     await waitFor(2000);
