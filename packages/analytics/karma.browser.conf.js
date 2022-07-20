0cf6787494c47ec14e4b40a49e9025423e420aec
dwyfrequency <jackdwyer@google.com>
2022-07-20 14:18:39 -0400

    Remove chrome from cross browser as it is already tested

--- packages/analytics/karma.browser.conf.js
+++ packages/analytics/karma.browser.conf.js
@@ -23,7 +23,7 @@ const files = [`**/*.test.ts`];
 module.exports = function (config) {
   config.set({
     ...karmaBase,
-    browsers: ['Firefox', 'ChromeHeadless'],
+    browsers: ['Firefox'],
     files,
     preprocessors: { '**/*.ts': ['webpack', 'sourcemap'] },
     frameworks: ['mocha']
