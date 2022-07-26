78a8900e0337f234c05a669869408491b251dcaf
Mark Duckworth <markduckworth@google.com>
2022-07-26 09:43:36 -0600

    Fix issue with readonly array.

--- packages/firestore/src/remote/serializer.ts
+++ packages/firestore/src/remote/serializer.ts
@@ -1003,7 +1003,7 @@ function decodeFilters(filter: ProtoFilter): Filter[] {
     result instanceof CompositeFilter &&
     compositeFilterIsFlatConjunction(result)
   ) {
-    return result.getFilters();
+    return Object.assign([], result.getFilters());
   }
 
   return [result];
