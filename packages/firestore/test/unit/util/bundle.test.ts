9ae0d769b0da2f18d42ff90f3f91b01f595efd2c
dwyfrequency <jackdwyer@google.com>
2022-08-22 15:18:57 -0400

    Update regex to remove unnecessary `/`

--- packages/firestore/test/unit/util/bundle.test.ts
+++ packages/firestore/test/unit/util/bundle.test.ts
@@ -241,7 +241,7 @@ function genericBundleReadingTests(bytesPerRead: number): void {
 
       await expect(
         generateBundleAndParse('{metadata: "no length prefix"}', bytesPerRead)
-      ).to.be.rejectedWith(/(Unexpected end of )(?=.*\JSON\b).*/gi);
+      ).to.be.rejectedWith(/(Unexpected end of )(?=.*JSON\b).*/gi);
 
       await expect(
         generateBundleAndParse(
