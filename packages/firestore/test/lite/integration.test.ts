b84d374cef83389ec4be407f576a98e7e1fd44bc
milaGGL <107142260+milaGGL@users.noreply.github.com>
2022-08-29 10:10:21 -0700

    rename snapshot to promise

--- packages/firestore/test/lite/integration.test.ts
+++ packages/firestore/test/lite/integration.test.ts
@@ -2328,16 +2328,16 @@ describe('countQuery()', () => {
       const countQuery_ = countQuery(query(collection));
 
       //This will pass
-      // const snapshot = getAggregateFromServerDirect(countQuery_)
+      // const promise = getAggregateFromServerDirect(countQuery_)
       // await terminate(collection.firestore)
-      // expect((await snapshot).getCount()).to.equal(3);
+      // expect((await promise).getCount()).to.equal(3);
 
       //this will pass
-      // const [snapshot,voidResult] = await Promise.all([
+      // const [promise,voidResult] = await Promise.all([
       //   getAggregateFromServerDirect(countQuery_),
       //   terminate(collection.firestore),
       // ])
-      // expect(snapshot.getCount()).to.equal(3);
+      // expect(promise.getCount()).to.equal(3);
 
       //if terminate comes first, it will throw error
       // expect(() => Promise.all([
@@ -2348,13 +2348,14 @@ describe('countQuery()', () => {
       // );
 
       // not sure why, this is not getting to the "then" block
+      // and therefore it is not checking the "expect" at all
       Promise.all([
         getAggregateFromServerDirect(countQuery_),
         terminate(collection.firestore)
       ])
-        .then(([snapshot, voidResult]) => {
-          console.log('snapshot', snapshot);
-          expect(snapshot.getCount()).to.equal(100);
+        .then(([promise, voidResult]) => {
+          console.log('snapshot', promise);
+          expect(promise.getCount()).to.equal(100);
         })
         .catch(e => {
           expect((e as Error)?.message).to.equal(
