f6ed8cb37157e99030da5c5df4931f82d32fee00
dannhauser <3385691+dannhauser@users.noreply.github.com>
2022-09-30 17:36:38 -0400

    Adding Count to be accessible to the console.

--- packages/firestore-compat/src/index.console.ts
+++ packages/firestore-compat/src/index.console.ts
@@ -22,8 +22,11 @@ import {
   Firestore as FirestoreExp,
   FirestoreError,
   _EmptyAuthCredentialsProvider,
-  _EmptyAppCheckTokenProvider
+  _EmptyAppCheckTokenProvider,
+  Query as ExpQuery,
+  getCountFromServer
 } from '@firebase/firestore';
+import { Compat } from '@firebase/util';
 
 import {
   Firestore as FirestoreCompat,
@@ -98,6 +101,15 @@ export class Firestore extends FirestoreCompat {
       new MemoryPersistenceProvider()
     );
   }
+
+  INTERNAL = {
+    delete: () => this.terminate(),
+    count: (query: Compat<ExpQuery<unknown>>) => {
+      return getCountFromServer(query._delegate).then(response => {
+        return response.data().count;
+      });
+    }
+  };
 }
 
 function databaseIdFromFirestoreDatabase(
