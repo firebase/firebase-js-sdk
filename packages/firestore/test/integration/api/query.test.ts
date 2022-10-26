b08285a4c844f81e8f7dba718d444771ea8cffe5
milaGGL <107142260+milaGGL@users.noreply.github.com>
2022-10-25 17:50:07 -0700

    stringify error to display full error message

--- packages/firestore/test/integration/api/query.test.ts
+++ packages/firestore/test/integration/api/query.test.ts
@@ -649,6 +649,36 @@ apiDescribe('Queries', (persistence: boolean) => {
     });
   });
 
+  it('can catch error message for missing index with error handler', () => {
+    const testDocs = {
+      '1': { sort: 1, filter: true, key: '1' },
+      '2': { sort: 2, filter: true, key: '2' }
+    };
+    return withTestCollection(persistence, testDocs, async coll => {
+      const query_ = query(
+        coll,
+        where('sort', '<=', '2'),
+        where('filter', '==', true)
+      );
+      const deferred = new Deferred<void>();
+
+      const unsubscribe = onSnapshot(
+        query_,
+        () => {},
+        err => {
+          expect(err.code).to.equal('failed-precondition');
+          expect(err.message).to.exist;
+          expect(err.message).to.match(
+            /index.*https:\/\/console\.firebase\.google\.com/
+          );
+          deferred.resolve();
+        }
+      );
+      await deferred.promise;
+      unsubscribe();
+    });
+  });
+
   it('can explicitly sort by document ID', () => {
     const testDocs = {
       a: { key: 'a' },
