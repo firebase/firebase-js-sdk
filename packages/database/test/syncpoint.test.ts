c2ff4eae18f19a039a0cd57a494177f70b8d7671
Maneesh Tewani <mtewani@google.com>
2022-11-14 08:24:49 -0800

    Removed only

--- packages/database/test/syncpoint.test.ts
+++ packages/database/test/syncpoint.test.ts
@@ -19,7 +19,7 @@ import { SyncPointTestParser } from './helpers/syncpoint-util';
 import syncPointSpecs from './helpers/syncPointSpec.json';
 
 // TODO: We should eventually split this up into separate tests
-describe.only('Syncpoint Tests', () => {
+describe('Syncpoint Tests', () => {
   const util = new SyncPointTestParser();
   for (let i = 0; i < syncPointSpecs.length; i++) {
     const spec = syncPointSpecs[i];
