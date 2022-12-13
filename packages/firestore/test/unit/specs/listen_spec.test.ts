e8983c8501dace4ec44701e9c81e4e600ea99965
milaGGL <107142260+milaGGL@users.noreply.github.com>
2022-12-12 21:25:18 -0800

    update expectedCount spec tests

--- packages/firestore/test/unit/specs/listen_spec.test.ts
+++ packages/firestore/test/unit/specs/listen_spec.test.ts
@@ -1812,7 +1812,7 @@ describeSpec('Listens:', [], () => {
 
   specTest(
     'Query with resume target can pass in expectedCount to target',
-    ['exclusive'],
+    [],
     () => {
       const query1 = query('collection');
       const docA = doc('collection/a', 1000, { key: 'a' });
@@ -1824,27 +1824,25 @@ describeSpec('Listens:', [], () => {
           .expectEvents(query1, { added: [docA] })
           .userUnlistens(query1)
           .watchRemoves(query1)
-          // There is 1 remote document from previous listen
+          // There is 1 remote document from previous listen.
           .userListens(query1, { resumeToken: 'resume-token-1000' }, 1)
           .expectEvents(query1, { added: [docA], fromCache: true })
       );
     }
   );
 
-  specTest('ExpectedCount in Target should include local mutations', ['exclusive'], () => {
+  specTest('ExpectedCount in Target should include local mutations', [], () => {
     const query1 = query('collection');
     const docA = doc('collection/a', 1000, { key: 'a' });
     const docB = doc('collection/b', 1000, { key: 'b' });
-    return (
-      spec()
-        .withGCEnabled(false)
-        .userListens(query1)
-        .watchAcksFull(query1, 1000, docA, docB)
-        .expectEvents(query1, { added: [docA, docB] })
-        .userUnlistens(query1)
-        .userDeletes('collection/a')
-        .userListens(query1, { resumeToken: 'resume-token-1000' }, 2)
-        .expectEvents(query1, {  added: [docB],  fromCache: true })
-    );
+    return spec()
+      .withGCEnabled(false)
+      .userListens(query1)
+      .watchAcksFull(query1, 1000, docA, docB)
+      .expectEvents(query1, { added: [docA, docB] })
+      .userUnlistens(query1)
+      .userDeletes('collection/a')
+      .userListens(query1, { resumeToken: 'resume-token-1000' }, 2)
+      .expectEvents(query1, { added: [docB], fromCache: true });
   });
 });
