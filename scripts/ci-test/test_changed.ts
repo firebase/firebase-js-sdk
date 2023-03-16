44d948d6796f2f4e9424e516dee3e19c724e2d60
Denver Coneybeare <dconeybe@google.com>
2023-03-16 14:19:33 -0400

    Remove --concurrency in calls to lerna run

--- scripts/ci-test/test_changed.ts
+++ scripts/ci-test/test_changed.ts
@@ -59,7 +59,7 @@ async function runTests(config: TestConfig) {
       process.exit(0);
     }
 
-    const lernaCmd = ['lerna', 'run', '--concurrency', '4'];
+    const lernaCmd = ['lerna', 'run'];
     console.log(chalk`{blue Running tests in:}`);
     for (const task of testTasks) {
       if (task.reason === TestReason.Changed) {
