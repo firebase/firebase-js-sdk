0dc337a84c75962ee8fd923dc8fc73755098daa3
Denver Coneybeare <dconeybe@google.com>
2023-04-12 19:53:21 +0000

    Integer class added to webchannel-wrapper

--- packages/webchannel-wrapper/src/index.js
+++ packages/webchannel-wrapper/src/index.js
@@ -78,6 +78,17 @@ goog.net.XhrIo.prototype['send'] = goog.net.XhrIo.prototype.send;
 goog.net.XhrIo.prototype['setWithCredentials'] =
   goog.net.XhrIo.prototype.setWithCredentials;
 
+goog.require('goog.math.Integer');
+goog.math.Integer.prototype['add'] = goog.math.Integer.prototype.add;
+goog.math.Integer.prototype['multiply'] = goog.math.Integer.prototype.multiply;
+goog.math.Integer.prototype['modulo'] = goog.math.Integer.prototype.modulo;
+goog.math.Integer.prototype['compare'] = goog.math.Integer.prototype.compare;
+goog.math.Integer.prototype['toNumber'] = goog.math.Integer.prototype.toNumber;
+goog.math.Integer.prototype['toString'] = goog.math.Integer.prototype.toString;
+goog.math.Integer.prototype['getBits'] = goog.math.Integer.prototype.getBits;
+goog.math.Integer['fromNumber'] = goog.math.Integer.fromNumber;
+goog.math.Integer['fromString'] = goog.math.Integer.fromString;
+
 module['exports']['createWebChannelTransport'] =
   goog.net.createWebChannelTransport;
 module['exports']['getStatEventTarget'] =
@@ -89,3 +100,4 @@ module['exports']['Stat'] = goog.labs.net.webChannel.requestStats.Stat;
 module['exports']['FetchXmlHttpFactory'] = goog.net.FetchXmlHttpFactory;
 module['exports']['WebChannel'] = goog.net.WebChannel;
 module['exports']['XhrIo'] = goog.net.XhrIo;
+module['exports']['Integer'] = goog.math.Integer;
