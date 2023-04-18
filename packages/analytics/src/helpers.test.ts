0a27d2fbf268f07099d4fa5ecab7fbf35a579780
dwyfrequency <dwyfrequency@users.noreply.github.com>
2023-04-18 15:16:25 -0500

    Add implementation of getGoogleAnalyticsClientId (#7158)
    * Add initial implementation of getGoogleAnalyticsClientId
    
    * Update docs devsite
    
    * Add checkset
    
    * Update changeset description
    
    * Add link to client_id in docstring
    
    * Update gtagWrapper to take variable number of args for potential fallthrough case
    
    * Add API test for getGoogleAnalyticsClientId
    
    * Move API functionality to internal function
    
    * Update docs for devsite
    
    * Removed unused function in test
    
    * Update public api with async keyword
    
    * Remove comment
    
    * Update doc string
    
    * Update grammar of changeset
    
    * Remove console.log
    
    * Update variable name from targetId to measurementId
    
    * Removed check for blank measurementId
    
    * Add ERROR_FACTORY for promise rejection
    
    * Change fieldName from clientId
    
    * Update AnalyticsError.NO_CLIENT_ID message
    
    * Testing adding comment
    
    * remove test comment
    
    * Remove comments

--- packages/analytics/src/helpers.test.ts
+++ packages/analytics/src/helpers.test.ts
@@ -332,6 +332,37 @@ describe('Gtag wrapping functions', () => {
       expect((window['dataLayer'] as DataLayer).length).to.equal(1);
     });
 
+    it('new window.gtag function does not wait when sending "get" calls', async () => {
+      wrapOrCreateGtag(
+        { [fakeAppId]: Promise.resolve(fakeMeasurementId) },
+        fakeDynamicConfigPromises,
+        {},
+        'dataLayer',
+        'gtag'
+      );
+      window['dataLayer'] = [];
+      (window['gtag'] as Gtag)(
+        GtagCommand.GET,
+        fakeMeasurementId,
+        'client_id',
+        clientId => console.log(clientId)
+      );
+      expect((window['dataLayer'] as DataLayer).length).to.equal(1);
+    });
+
+    it('new window.gtag function does not wait when sending an unknown command', async () => {
+      wrapOrCreateGtag(
+        { [fakeAppId]: Promise.resolve(fakeMeasurementId) },
+        fakeDynamicConfigPromises,
+        {},
+        'dataLayer',
+        'gtag'
+      );
+      window['dataLayer'] = [];
+      (window['gtag'] as Gtag)('new-command-from-gtag-team', fakeMeasurementId);
+      expect((window['dataLayer'] as DataLayer).length).to.equal(1);
+    });
+
     it('new window.gtag function waits for initialization promise when sending "config" calls', async () => {
       const initPromise1 = new Deferred<string>();
       wrapOrCreateGtag(
