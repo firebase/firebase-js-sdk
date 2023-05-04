ead6cac23aae5f5bcf10e29da45d9d960dde4f8d
Tiffany Zheng <125598214+qtz1@users.noreply.github.com>
2023-05-04 19:18:08 -0400

    Update rollup.config.js

--- packages/firestore/rollup.config.js
+++ packages/firestore/rollup.config.js
@@ -20,7 +20,7 @@ import alias from '@rollup/plugin-alias';
 import json from '@rollup/plugin-json';
 import replace from 'rollup-plugin-replace';
 import { terser } from 'rollup-plugin-terser';
-import dts from 'rollup-plugin-dts';
+import resolveDts from 'rollup-plugin-dts';
 import typescriptPlugin from 'rollup-plugin-typescript2';
 import tmp from 'tmp';
 import typescript from 'typescript';
