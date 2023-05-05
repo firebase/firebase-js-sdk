fa168edb9eebf9ed9d0c8c7035c81741738c11fc
Tiffany Zheng <125598214+qtz1@users.noreply.github.com>
2023-05-05 15:03:36 -0400

    Update rollup.config.js

--- packages/firestore/rollup.config.js
+++ packages/firestore/rollup.config.js
@@ -20,8 +20,8 @@ import alias from '@rollup/plugin-alias';
 import json from '@rollup/plugin-json';
 import replace from 'rollup-plugin-replace';
 import { terser } from 'rollup-plugin-terser';
-//import dts from 'rollup-plugin-dts';
-import resolveNode from '@rollup/plugin-node-resolve';
+import dts from 'rollup-plugin-dts';
+//import resolveNode from '@rollup/plugin-node-resolve';
 //import commonjs from '@rollup/plugin-commonjs';
 import typescriptPlugin from 'rollup-plugin-typescript2';
 import tmp from 'tmp';
@@ -213,8 +213,8 @@ const allBuilds = [
     },
     plugins: [
       //commonjs(),
-      resolveNode()
-      //dts()
+      //resolveNode()
+      dts()
     ],
   }
 ];
