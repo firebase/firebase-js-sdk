8f4b7b099c5644322c89932cefddae7255c1c061
Mark Duckworth <1124037+MarkDuckworth@users.noreply.github.com>
2023-06-06 15:58:35 -0600

    WIP: API for streaming aggregations.

--- packages/firestore/src/api/aggregate.ts
+++ packages/firestore/src/api/aggregate.ts
@@ -15,11 +15,16 @@
  * limitations under the License.
  */
 
-import { AggregateField, AggregateSpec, Query } from '../api';
+import {
+  AggregateField,
+  AggregateSpec, DocumentReference, FirestoreError,
+  Query, SnapshotListenOptions,
+  SnapshotMetadata, Unsubscribe
+} from '../api';
+import { AggregateQuerySnapshot } from '../api/aggregate_types';
 import { AggregateImpl } from '../core/aggregate';
 import { firestoreClientRunAggregateQuery } from '../core/firestore_client';
 import { count } from '../lite-api/aggregate';
-import { AggregateQuerySnapshot } from '../lite-api/aggregate_types';
 import { ApiClientObjectMap, Value } from '../protos/firestore_proto_api';
 import { cast } from '../util/input_validation';
 import { mapToArray } from '../util/obj';
@@ -126,6 +131,60 @@ export function getAggregateFromServer<T extends AggregateSpec>(
 }
 
 /**
+ * Returns the specified aggregations over the documents in the result
+ * set of the given query, based on data in the SDK's cache.
+ *
+ * If there IS NOT a cached aggregation result for the specified
+ * query and (TODO: alias or AggregateField) from a previous AggregateQuery sent
+ * to the server, then the returned aggregation value will be computed against
+ * documents in the SDK's local cache.
+ *
+ * If there IS a cached aggregation result for the specified
+ * query and (TODO: alias or AggregateField) from a previous AggregateQuery sent
+ * to the server, then the returned aggregation value will be computed by
+ * augmenting the cached aggregation value against document mutations in the
+ * SDK's local cache. The SDK attempts to compute the most accurate aggregation
+ * values from these two sources by comparing timestamps on the cached
+ * aggregation values and the cached document mutations.
+ *
+ * If cached aggregation values are not available for all of the requested
+ * AggregateFields, or if cached aggregation values do not have the same timestamp
+ * for all of the requested aggregate fields, then TODO?
+ *   - The SDK computes aggregations based on the most recent cached aggregation
+ *     value it has in the cache.
+ *   - The SDK ignore cached aggregation values and computes each aggregation
+ *     based on the documents in the cache.
+ */
+export function getAggregateFromCache<T extends AggregateSpec>(
+  query: Query<unknown>,
+  aggregateSpec: T
+): Promise<AggregateQuerySnapshot<T>> {
+  // TODO (streaming-count)
+  throw new Error("Not implemented: API design only");
+}
+
+/**
+ * Calculates the specified aggregations over the documents in the result
+ * set of the given query, without actually downloading the documents.
+ *
+ * `getAggregate()` attempts to provide up-to-date data when possible by
+ * waiting for data from the server, but it may return aggregation results based
+ * on cached data or fail if you are offline and the server cannot be reached.
+ * To specify this behavior, invoke {@link getAggregateFromCache} or
+ * {@link getAggregateFromServer}.
+ *
+ * If computing aggregation results based on cached data, the behavior of the
+ * SDK is defined in {@link getAggregateFromCache}.
+ */
+export function getAggregate<T extends AggregateSpec>(
+  query: Query<unknown>,
+  aggregateSpec: T
+): Promise<AggregateQuerySnapshot<T>> {
+  // TODO (streaming-count)
+  throw new Error("Not implemented: API design only");
+}
+
+/**
  * Converts the core aggregration result to an `AggregateQuerySnapshot`
  * that can be returned to the consumer.
  * @param query
@@ -141,7 +200,142 @@ function convertToAggregateQuerySnapshot<T extends AggregateSpec>(
   const querySnapshot = new AggregateQuerySnapshot<T>(
     query,
     userDataWriter,
-    aggregateResult
+    aggregateResult,
+    // TODO (streaming-count) add real snapshot metadata
+    new SnapshotMetadata(false, false) // this is stubbed
   );
   return querySnapshot;
 }
+
+
+
+/**
+ * Attaches a listener for `AggregateQuerySnapshot` events. You may either pass
+ * individual `onNext` and `onError` callbacks or pass a single observer
+ * object with `next` and `error` callbacks. The listener can be cancelled by
+ * calling the function that is returned when `onSnapshot` is called.
+ *
+ * NOTE: Although an `onCompletion` callback can be provided, it will
+ * never be called because the snapshot stream is never-ending.
+ *
+ * @param query - The base query to listen for aggregate updates.
+ * @param aggregateSpec An `AggregateSpec` object that specifies the aggregates
+ * to perform over the result set of the `query`. The AggregateSpec specifies
+ * aliases for each aggregate, which can be used to retrieve the aggregate
+ * result from the AggregateQuerySnapshot.
+ * @param observer - A single object containing `next` and `error` callbacks.
+ * @returns An unsubscribe function that can be called to cancel
+ * the snapshot listener.
+ */
+export function onAggregateSnapshot<T extends  AggregateSpec>(
+  query: Query<T>,
+  aggregateSpec: T,
+  observer: {
+    next?: (snapshot: AggregateQuerySnapshot<T>) => void;
+    error?: (error: FirestoreError) => void;
+    complete?: () => void;
+  }
+): Unsubscribe;
+/**
+ * Attaches a listener for `QuerySnapshot` events. You may either pass
+ * individual `onNext` and `onError` callbacks or pass a single observer
+ * object with `next` and `error` callbacks. The listener can be cancelled by
+ * calling the function that is returned when `onSnapshot` is called.
+ *
+ * NOTE: Although an `onCompletion` callback can be provided, it will
+ * never be called because the snapshot stream is never-ending.
+ *
+ * @param query - The base query to listen for aggregate updates.
+ * @param aggregateSpec An `AggregateSpec` object that specifies the aggregates
+ * to perform over the result set of the `query`. The AggregateSpec specifies
+ * aliases for each aggregate, which can be used to retrieve the aggregate
+ * result from the AggregateQuerySnapshot.
+ * @param options - Options controlling the listen behavior.
+ * @param observer - A single object containing `next` and `error` callbacks.
+ * @returns An unsubscribe function that can be called to cancel
+ * the snapshot listener.
+ */
+export function onAggregateSnapshot<T extends AggregateSpec>(
+  query: Query<T>,
+  aggregateSpec: T,
+  options: SnapshotListenOptions,
+  observer: {
+    next?: (snapshot: AggregateQuerySnapshot<T>) => void;
+    error?: (error: FirestoreError) => void;
+    complete?: () => void;
+  }
+): Unsubscribe;
+/**
+ * Attaches a listener for `QuerySnapshot` events. You may either pass
+ * individual `onNext` and `onError` callbacks or pass a single observer
+ * object with `next` and `error` callbacks. The listener can be cancelled by
+ * calling the function that is returned when `onSnapshot` is called.
+ *
+ * NOTE: Although an `onCompletion` callback can be provided, it will
+ * never be called because the snapshot stream is never-ending.
+ *
+ * @param query - The base query to listen for aggregate updates.
+ * @param aggregateSpec An `AggregateSpec` object that specifies the aggregates
+ * to perform over the result set of the `query`. The AggregateSpec specifies
+ * aliases for each aggregate, which can be used to retrieve the aggregate
+ * result from the AggregateQuerySnapshot.
+ * @param onNext - A callback to be called every time a new `QuerySnapshot`
+ * is available.
+ * @param onCompletion - Can be provided, but will not be called since streams are
+ * never ending.
+ * @param onError - A callback to be called if the listen fails or is
+ * cancelled. No further callbacks will occur.
+ * @returns An unsubscribe function that can be called to cancel
+ * the snapshot listener.
+ */
+export function onAggregateSnapshot<T extends AggregateSpec>(
+  query: Query<T>,
+  aggregateSpec: T,
+  onNext: (snapshot: AggregateQuerySnapshot<T>) => void,
+  onError?: (error: FirestoreError) => void,
+  onCompletion?: () => void
+): Unsubscribe;
+/**
+ * Attaches a listener for `QuerySnapshot` events. You may either pass
+ * individual `onNext` and `onError` callbacks or pass a single observer
+ * object with `next` and `error` callbacks. The listener can be cancelled by
+ * calling the function that is returned when `onSnapshot` is called.
+ *
+ * NOTE: Although an `onCompletion` callback can be provided, it will
+ * never be called because the snapshot stream is never-ending.
+ *
+ * @param query - The base query to listen for aggregate updates.
+ * @param aggregateSpec An `AggregateSpec` object that specifies the aggregates
+ * to perform over the result set of the `query`. The AggregateSpec specifies
+ * aliases for each aggregate, which can be used to retrieve the aggregate
+ * result from the AggregateQuerySnapshot.
+ * @param options - Options controlling the listen behavior.
+ * @param onNext - A callback to be called every time a new `QuerySnapshot`
+ * is available.
+ * @param onCompletion - Can be provided, but will not be called since streams are
+ * never ending.
+ * @param onError - A callback to be called if the listen fails or is
+ * cancelled. No further callbacks will occur.
+ * @returns An unsubscribe function that can be called to cancel
+ * the snapshot listener.
+ */
+export function onAggregateSnapshot<T extends AggregateSpec>(
+  query: Query<T>,
+  aggregateSpec: T,
+  options: SnapshotListenOptions,
+  onNext: (snapshot: AggregateQuerySnapshot<T>) => void,
+  onError?: (error: FirestoreError) => void,
+  onCompletion?: () => void
+): Unsubscribe;
+
+export function onAggregateSnapshot<T extends AggregateSpec>(
+  reference: Query<T> | DocumentReference<T>,
+  aggregateSpec: T,
+  ...args: unknown[]
+): Unsubscribe {
+  // TODO (streaming-count)
+  throw new Error("Not implemented: API design only");
+}
+
+
+// TODO (streaming-count) do we need onSnapshotsInSync()?
