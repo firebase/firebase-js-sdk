d642e31176410003fc019c2153426b302d25bc4f
Christina Holland <chholland@google.com>
2023-07-05 20:47:53 -0700

    Update Firestore and RTDB types

--- packages/firestore-types/index.d.ts
+++ packages/firestore-types/index.d.ts
@@ -50,9 +50,9 @@ export type LogLevel =
 export function setLogLevel(logLevel: LogLevel): void;
 
 export interface FirestoreDataConverter<
-AppModelType,
-DbModelType extends DocumentData = DocumentData
-> {
+  AppModelType,
+  DbModelType extends DocumentData = DocumentData
+  > {
   toFirestore(modelObject: AppModelType): DocumentData;
   toFirestore(modelObject: Partial<AppModelType>, options: SetOptions): DocumentData;
 
@@ -188,45 +188,53 @@ export class Blob {
 export class Transaction {
   private constructor();
 
-  get<T>(documentRef: DocumentReference<AppModelType, DbModelType>): Promise<DocumentSnapshot<AppModelType, DbModelType>>;
+  get<AppModelType, DbModelType extends DocumentData>(
+    documentRef: DocumentReference<AppModelType, DbModelType>
+  ): Promise<DocumentSnapshot<AppModelType, DbModelType>>;
 
-  set<T>(
+  set<AppModelType, DbModelType extends DocumentData>(
     documentRef: DocumentReference<AppModelType, DbModelType>,
-    data: Partial<T>,
+    data: Partial<AppModelType>,
     options: SetOptions
   ): Transaction;
-  set<T>(documentRef: DocumentReference<AppModelType, DbModelType>, data: T): Transaction;
+  set<AppModelType, DbModelType extends DocumentData>(
+    documentRef: DocumentReference<AppModelType, DbModelType>,
+    data: Partial<AppModelType>
+  ): Transaction;
 
-  update(documentRef: DocumentReference<any>, data: UpdateData): Transaction;
-  update(
-    documentRef: DocumentReference<any>,
+  update<AppModelType, DbModelType extends DocumentData>(
+    documentRef: DocumentReference<AppModelType, DbModelType>,
+    data: Partial<AppModelType>
+  ): Transaction;
+  update<AppModelType, DbModelType extends DocumentData>(
+    documentRef: DocumentReference<AppModelType, DbModelType>,
     field: string | FieldPath,
     value: any,
     ...moreFieldsAndValues: any[]
   ): Transaction;
 
-  delete(documentRef: DocumentReference<any>): Transaction;
+  delete<AppModelType, DbModelType extends DocumentData>(documentRef: DocumentReference<AppModelType, DbModelType>): Transaction;
 }
 
 export class WriteBatch {
   private constructor();
 
-  set<T>(
+  set<AppModelType, DbModelType extends DocumentData>(
     documentRef: DocumentReference<AppModelType, DbModelType>,
-    data: Partial<T>,
+    data: Partial<AppModelType>,
     options: SetOptions
   ): WriteBatch;
-  set<T>(documentRef: DocumentReference<AppModelType, DbModelType>, data: T): WriteBatch;
+  set<AppModelType, DbModelType extends DocumentData>(documentRef: DocumentReference<AppModelType, DbModelType>, data: UpdateData): WriteBatch;
 
-  update(documentRef: DocumentReference<any>, data: UpdateData): WriteBatch;
-  update(
-    documentRef: DocumentReference<any>,
+  update<AppModelType, DbModelType extends DocumentData>(documentRef: DocumentReference<AppModelType, DbModelType>, data: UpdateData): WriteBatch;
+  update<AppModelType, DbModelType extends DocumentData>(
+    documentRef: DocumentReference<AppModelType, DbModelType>,
     field: string | FieldPath,
     value: any,
     ...moreFieldsAndValues: any[]
   ): WriteBatch;
 
-  delete(documentRef: DocumentReference<any>): WriteBatch;
+  delete<AppModelType, DbModelType extends DocumentData>(documentRef: DocumentReference<AppModelType, DbModelType>): WriteBatch;
 
   commit(): Promise<void>;
 }
@@ -256,8 +264,8 @@ export class DocumentReference<AppModelType, DbModelType> {
 
   isEqual(other: DocumentReference<AppModelType, DbModelType>): boolean;
 
-  set(data: Partial<T>, options: SetOptions): Promise<void>;
-  set(data: T): Promise<void>;
+  set(data: Partial<AppModelType>, options: SetOptions): Promise<void>;
+  set(data: Partial<AppModelType>): Promise<void>;
 
   update(data: UpdateData): Promise<void>;
   update(
@@ -295,8 +303,10 @@ export class DocumentReference<AppModelType, DbModelType> {
     onCompletion?: () => void
   ): () => void;
 
-  withConverter(converter: null): DocumentReference<DocumentData>;
-  withConverter<U>(converter: FirestoreDataConverter<U>): DocumentReference<U>;
+  withConverter(converter: null): DocumentReference<AppModelType, DbModelType>;
+  withConverter<AppModelType, DbModelType>(
+    converter: FirestoreDataConverter<AppModelType, DbModelType>
+  ): DocumentReference<AppModelType, DbModelType>;
 }
 
 export interface SnapshotOptions {
@@ -318,7 +328,7 @@ export class DocumentSnapshot<AppModelType, DbModelType> {
   readonly id: string;
   readonly metadata: SnapshotMetadata;
 
-  data(options?: SnapshotOptions): T | undefined;
+  data(options?: SnapshotOptions): AppModelType | undefined;
 
   get(fieldPath: string | FieldPath, options?: SnapshotOptions): any;
 
@@ -329,7 +339,7 @@ export class QueryDocumentSnapshot<AppModelType, DbModelType>
   extends DocumentSnapshot<AppModelType, DbModelType> {
   private constructor();
 
-  data(options?: SnapshotOptions): T;
+  data(options?: SnapshotOptions): AppModelType;
 }
 
 export type OrderByDirection = 'desc' | 'asc';
@@ -420,7 +430,7 @@ export class QuerySnapshot<AppModelType, DbModelType> {
   readonly size: number;
   readonly empty: boolean;
 
-  docChanges(options?: SnapshotListenOptions): Array<DocumentChange<T>>;
+  docChanges(options?: SnapshotListenOptions): Array<DocumentChange<AppModelType, DbModelType>>;
 
   forEach(
     callback: (result: QueryDocumentSnapshot<AppModelType, DbModelType>) => void,
@@ -439,18 +449,21 @@ export interface DocumentChange<AppModelType, DbModelType> {
   readonly newIndex: number;
 }
 
-export class CollectionReference<AppModelType = DocumentData, DbModelType extends DocumentData = DocumentData> extends Query<AppModelType, DbModelType>  {
+export class CollectionReference<
+  AppModelType = DocumentData,
+  DbModelType extends DocumentData = DocumentData
+  > extends Query<AppModelType, DbModelType>  {
   private constructor();
 
   readonly id: string;
-  readonly parent: DocumentReference<AppModelType, DbModelType>| null;
+  readonly parent: DocumentReference<AppModelType, DbModelType> | null;
   readonly path: string;
 
   doc(documentPath?: string): DocumentReference<AppModelType, DbModelType>;
 
-  add(data: T): Promise<DocumentReference<AppModelType, DbModelType>>;
+  add(data: AppModelType): Promise<DocumentReference<AppModelType, DbModelType>>;
 
-  isEqual(other: CollectionReference<T>): boolean;
+  isEqual(other: CollectionReference<AppModelType, DbModelType>): boolean;
 
   withConverter(converter: null): CollectionReference<DocumentData>;
   withConverter<U>(
