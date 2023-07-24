4c0937fe32255e573f0f9e299449868f3394d301
Tiffany Zheng <125598214+qtz1@users.noreply.github.com>
2023-07-24 14:51:08 -0400

    Update api.ts

--- packages/firestore/src/api.ts
+++ packages/firestore/src/api.ts
@@ -82,6 +82,7 @@ export {
 } from './api/bundle';
 
 export { FirestoreSettings, PersistenceSettings } from './api/settings';
+export { PrivateSettings } from './lite-api/settings';
 export { ExperimentalLongPollingOptions } from './api/long_polling_options';
 
 export {
