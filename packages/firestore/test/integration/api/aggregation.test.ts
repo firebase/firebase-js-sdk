c6173a5c4019bc7c6cef3192d0b9ec05116a6259
Denver Coneybeare <dconeybe@google.com>
2023-08-15 14:37:01 -0400

    integration/api/aggregation.test.ts: force a test to fail

--- packages/firestore/test/integration/api/aggregation.test.ts
+++ packages/firestore/test/integration/api/aggregation.test.ts
@@ -50,7 +50,7 @@ apiDescribe('Count queries', persistence => {
     };
     return withTestCollection(persistence, testDocs, async coll => {
       const snapshot = await getCountFromServer(coll);
-      expect(snapshot.data().count).to.equal(2);
+      expect(snapshot.data().count).to.equal('zzyzx');
     });
   });
 
