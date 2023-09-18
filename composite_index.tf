6f3b9839f55309225c1212a35adcb4d5d44bbab6
milaGGL <107142260+milaGGL@users.noreply.github.com>
2023-09-18 12:56:29 -0400

    add env:

--- composite_index.tf
+++ composite_index.tf
@@ -2,14 +2,33 @@ locals {
     data = jsondecode(file("config/project.json"))
 }
 
-provider "google" {
-  project     = local.data.projectId
-  region      = "us-central1"
+ provider "google" {
+    project = local.data.projectId
+    region  = "us-central1"
+ }
+
+resource "google_firestore_index" "default-db-index" {
+  collection = "composite-index-test-collection"
+
+  fields {
+    field_path = "key"
+    order      = "ASCENDING"
+  }
+
+  fields {
+    field_path = "testId"
+    order      = "DESCENDING"
+  }
+
+  fields {
+    field_path = "sort"
+    order      = "DESCENDING"
+  }
 }
 
-resource "google_firestore_index" "my-index" {
-  project = local.data.projectId
+resource "google_firestore_index" "named-db-index" {
   collection = "composite-index-test-collection"
+  database = "test-db"
 
   fields {
     field_path = "key"
