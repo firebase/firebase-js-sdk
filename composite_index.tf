b8f4bee32e84d61939f962a7c183c348237e02e4
milaGGL <107142260+milaGGL@users.noreply.github.com>
2023-09-18 10:56:49 -0400

    copy project.json

--- composite_index.tf
+++ composite_index.tf
@@ -1,10 +1,31 @@
 locals {
-    data = jsondecode(file("./config/project.json"))
+    data = jsondecode(file("config/project.json"))
 }
 
 resource "google_firestore_index" "my-index" {
   project = local.data.projectId
   collection = "composite-index-test-collection"
+
+  fields {
+    field_path = "key"
+    order      = "ASCENDING"
+  }
+
+  fields {
+    field_path = "testId"
+    order      = "DESCENDING"
+  }
+
+  fields {
+    field_path = "sort"
+    order      = "DESCENDING"
+  }
+
+}
+resource "google_firestore_index" "my-index" {
+  project = local.data.projectId
+  collection = "composite-index-test-collection"
+  database = "test-db"
 
   fields {
     field_path = "key"
