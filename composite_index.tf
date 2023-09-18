9e44a4c8c51e6e895780c19f166f8b91a30880ec
milaGGL <107142260+milaGGL@users.noreply.github.com>
2023-09-18 13:33:08 -0400

    Update composite_index.tf

--- composite_index.tf
+++ composite_index.tf
@@ -7,6 +7,18 @@ locals {
     region  = "us-central1"
  }
 
+ resource "time_sleep" "wait_60_seconds" {
+  depends_on = [local.data]
+  create_duration = "60s"
+}
+
+resource "google_project_service" "firestore" {
+  project = local.data.projectId
+  service = "firestore.googleapis.com"
+  # Needed for CI tests for permissions to propagate, should not be needed for actual usage
+  depends_on = [time_sleep.wait_60_seconds]
+}
+
 resource "google_firestore_index" "default-db-index" {
   collection = "composite-index-test-collection"
 
