011c874b1da9c8a3bef3a870c202b9614a07342d
Eduardo San Martin Morote <posva13@gmail.com>
2023-09-25 16:17:10 +0200

    style: format

--- packages/firestore/src/lite-api/reference.ts
+++ packages/firestore/src/lite-api/reference.ts
@@ -159,10 +159,16 @@ export class Query<
    * @param converter - Converts objects to and from Firestore.
    * @returns A `Query` that uses the provided converter.
    */
-  withConverter<NewAppModelType, NewDbModelType extends DocumentData = DocumentData>(
+  withConverter<
+    NewAppModelType,
+    NewDbModelType extends DocumentData = DocumentData
+  >(
     converter: FirestoreDataConverter<NewAppModelType, NewDbModelType>
   ): Query<NewAppModelType, NewDbModelType>;
-  withConverter<NewAppModelType, NewDbModelType extends DocumentData = DocumentData>(
+  withConverter<
+    NewAppModelType,
+    NewDbModelType extends DocumentData = DocumentData
+  >(
     converter: FirestoreDataConverter<NewAppModelType, NewDbModelType> | null
   ): Query<NewAppModelType, NewDbModelType> {
     return new Query<NewAppModelType, NewDbModelType>(
