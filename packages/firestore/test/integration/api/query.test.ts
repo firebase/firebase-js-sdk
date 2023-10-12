7468d053faefe2af35db16380f8eec309c1f0dcd
milaGGL <107142260+milaGGL@users.noreply.github.com>
2023-10-12 11:50:25 -0400

    revert test to original version

--- packages/firestore/test/integration/api/query.test.ts
+++ packages/firestore/test/integration/api/query.test.ts
@@ -1915,45 +1915,28 @@ apiDescribe('Queries', persistence => {
 
     it('inequality query will reject if document key is not the last orderBy field', () => {
       return withEmptyTestCollection(persistence, async coll => {
+        // Implicitly ordered by:  __name__ asc, 'key' asc,
         const queryForRejection = query(
           coll,
           where('key', '!=', 42),
           orderBy(documentId())
         );
 
-        getDocs(queryForRejection).then(
-          () => {
-            expect.fail('Promise resolved even though error was expected.');
-          },
-          err => {
-            expect(err.name).to.exist;
-            expect(err.message).to.exist;
-            expect(err.message).to.equal(
-              'order by clause cannot contain more fields after the key'
-            );
-          }
+        await expect(getDocs(queryForRejection)).to.be.eventually.rejectedWith(
+          'order by clause cannot contain more fields after the key'
         );
       });
     });
 
     it('inequality query will reject if document key appears only in equality filter', () => {
       return withEmptyTestCollection(persistence, async coll => {
-        const queryForRejection = query(
+        const query_ = query(
           coll,
           where('key', '!=', 42),
           where(documentId(), '==', 'doc1')
         );
-
-        getDocs(queryForRejection).then(
-          () => {
-            expect.fail('Promise resolved even though error was expected.');
-          },
-          err => {
-            expect(err.message).to.exist;
-            expect(err.message).to.equal(
-              'Equality on key is not allowed if there are other inequality fields and key does not appear in inequalities.'
-            );
-          }
+        await expect(getDocs(query_)).to.be.eventually.rejectedWith(
+          'Equality on key is not allowed if there are other inequality fields and key does not appear in inequalities.'
         );
       });
     });
