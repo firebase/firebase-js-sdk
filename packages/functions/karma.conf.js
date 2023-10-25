0dcfea21549d981964e36ca23995878b8d88148f
Christina Holland <chholland@google.com>
2023-10-25 11:30:43 -0700

    test

--- packages/functions/karma.conf.js
+++ packages/functions/karma.conf.js
@@ -16,6 +16,7 @@
  */
 
 const karmaBase = require('../../config/karma.base');
+const webpackBase = require('../../config/webpack.test');
 
 const files = [`src/**/*.test.ts`];
 
@@ -25,9 +26,17 @@ module.exports = function (config) {
     files,
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha']
+    frameworks: ['mocha'],
+    webpack: {
+      ...webpackBase,
+      resolve: {
+        ...webpackBase.resolve,
+        alias: {
+          '../test/utils': '../test/utils-web'
+        }
+      }
+    }
   });
-
   config.set(karmaConfig);
 };
 
--- packages/functions/test/utils-web.ts
+++ packages/functions/test/utils-web.ts
@@ -21,7 +21,6 @@ import { FirebaseAuthInternalName } from '@firebase/auth-interop-types';
 import { AppCheckInternalComponentName } from '@firebase/app-check-interop-types';
 import { FunctionsService } from '../src/service';
 import { connectFunctionsEmulator } from '../src/api';
-import { fetch } from 'undici';
 import { MessagingInternalComponentName } from '../../../packages/messaging-interop-types';
 
 export function makeFakeApp(options: FirebaseOptions = {}): FirebaseApp {
@@ -58,13 +57,14 @@ export function createTestService(
     new ComponentContainer('test')
   )
 ): FunctionsService {
+  const fetchImpl: typeof fetch = fetch.bind(window);
   const functions = new FunctionsService(
     app,
     authProvider,
     messagingProvider,
     appCheckProvider,
     region,
-    fetch as any
+    fetchImpl
   );
   const useEmulator = !!process.env.FIREBASE_FUNCTIONS_EMULATOR_ORIGIN;
   if (useEmulator) {
