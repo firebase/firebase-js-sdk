2af182a8177794e64f5cdf1f7222a47a22f39ae6
Mark Duckworth <1124037+MarkDuckworth@users.noreply.github.com>
2023-10-26 16:01:22 -0600

    Rolling back the use of useFetchStreams, which has lead to hanging queries.

--- packages/firestore/src/platform/browser/webchannel_connection.ts
+++ packages/firestore/src/platform/browser/webchannel_connection.ts
@@ -27,7 +27,8 @@ import {
   EventTarget,
   StatEvent,
   Event,
-  Stat
+  Stat,
+  FetchXmlHttpFactory
 } from '@firebase/webchannel-wrapper';
 
 import { Token } from '../../api/credentials';
@@ -208,7 +209,9 @@ export class WebChannelConnection extends RestConnection {
     }
 
     if (this.useFetchStreams) {
-      request.useFetchStreams = true;
+      // When b/307942499 is fixed, revert to the following line:
+      // request.useFetchStreams = true;
+      request.xmlHttpFactory = new FetchXmlHttpFactory({});
     }
 
     this.modifyHeadersForRequest(
