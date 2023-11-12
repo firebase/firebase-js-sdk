bebecdaad7fa552505055ab7705da478203078b6
DellaBitta <DellaBitta@users.noreply.github.com>
2023-11-12 10:07:48 -0500

    Replace node-fetch dependency with undici (#7705)
    Update our dependency on aging `node-fetch` `v2.6.7` to `undici` `v5.26.5`.
    
    This should fix some vulnerabilities within node-fetch as well as fix user issue #7660.

--- packages/auth/karma.conf.js
+++ packages/auth/karma.conf.js
@@ -16,6 +16,7 @@
  */
 
 const karmaBase = require('../../config/karma.base');
+const webpackBase = require('../../config/webpack.test');
 const { argv } = require('yargs');
 
 module.exports = function (config) {
@@ -26,7 +27,17 @@ module.exports = function (config) {
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha'],
-
+    // undici is a fetch polyfill that test helpers call for Node tests, and browser tests should
+    // ingore its import to avoid compilation errors in those test helpers.
+    webpack: {
+      ...webpackBase,
+      resolve: {
+        ...webpackBase.resolve,
+        alias: {
+          'undici': false
+        }
+      }
+    },
     client: Object.assign({}, karmaBase.client, getClientConfig(argv))
   });
 
