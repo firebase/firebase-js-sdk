98e7e979b0565401d409c660379cf8b507c66092
DellaBitta <drsanta@google.com>
2024-03-05 16:10:45 -0500

    format

--- scripts/ci-test/tasks.ts
+++ scripts/ci-test/tasks.ts
@@ -99,7 +99,8 @@ export async function getTestTasks(): Promise<TestTask[]> {
   let testTasks: TestTask[] = [];
   for (const filename of changedFiles) {
     // Files that trigger full test suite.
-    if (1===1) { //(fullTestTriggerFiles.includes(filename)) {
+    if (1 === 1) {
+      //(fullTestTriggerFiles.includes(filename)) {
       console.log(
         chalk`{blue Running all tests because ${filename} was modified.}`
       );
