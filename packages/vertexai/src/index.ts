61af23655d72d841895d64637ef21375b4a612d2
Christina Holland <chholland@google.com>
2024-03-22 10:56:45 -0700

    Initial vertex SDK setup (#281)

---
+++ packages/vertexai/src/index.ts
@@ -0,0 +1,58 @@
+/**
+ * The Firebase Vertex Web SDK.
+ *
+ * @packageDocumentation
+ */
+
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { registerVersion, _registerComponent } from '@firebase/app';
+import { factory } from './factory';
+import { VERTEX_TYPE } from './constants';
+import { Component, ComponentType } from '@firebase/component';
+import { name, version } from '../package.json';
+
+declare global {
+  interface Window {
+    [key: string]: unknown;
+  }
+}
+
+function registerVertex(): void {
+  _registerComponent(
+    new Component(
+      VERTEX_TYPE,
+      (container, { instanceIdentifier: region }) => {
+        // getImmediate for FirebaseApp will always succeed
+        const app = container.getProvider('app').getImmediate();
+        const appCheckProvider = container.getProvider('app-check-internal');
+        return factory(app, appCheckProvider, { region });
+      },
+      ComponentType.PUBLIC
+    ).setMultipleInstances(true)
+  );
+
+  registerVersion(name, version);
+  // BUILD_TARGET will be replaced by values like esm5, esm2017, cjs5, etc during the compilation
+  registerVersion(name, version, '__BUILD_TARGET__');
+}
+
+registerVertex();
+
+export * from './api';
+export * from './public-types';
