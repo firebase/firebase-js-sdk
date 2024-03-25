a6505ce5c13bac85b022fb81100bdbbff6be9b9b
milaGGL <107142260+milaGGL@users.noreply.github.com>
2024-03-25 15:11:41 -0400

    prettier

--- packages/firestore/test/unit/index/firestore_index_value_writer.test.ts
+++ packages/firestore/test/unit/index/firestore_index_value_writer.test.ts
@@ -25,26 +25,26 @@ import { toTimestamp } from '../../../src/remote/serializer';
 import { JSON_SERIALIZER } from '../local/persistence_test_helpers';
 
 describe('Firestore Index Value Writer', () => {
+  function compareIndexValues(
+    value1: Value,
+    value2: Value,
+    direction: IndexKind
+  ): void {
+    const encoder1 = new IndexByteEncoder();
+    const encoder2 = new IndexByteEncoder();
+    FirestoreIndexValueWriter.INSTANCE.writeIndexValue(
+      value1,
+      encoder1.forKind(direction)
+    );
+
+    FirestoreIndexValueWriter.INSTANCE.writeIndexValue(
+      value2,
+      encoder2.forKind(direction)
+    );
+    expect(encoder1.encodedBytes()).to.deep.equal(encoder2.encodedBytes());
+  }
+
   describe('can gracefully handle different format of timestamp', () => {
-    function compareIndexValues(
-      value1: Value,
-      value2: Value,
-      direction: IndexKind
-    ): void {
-      const encoder1 = new IndexByteEncoder();
-      const encoder2 = new IndexByteEncoder();
-      FirestoreIndexValueWriter.INSTANCE.writeIndexValue(
-        value1,
-        encoder1.forKind(direction)
-      );
-
-      FirestoreIndexValueWriter.INSTANCE.writeIndexValue(
-        value2,
-        encoder2.forKind(direction)
-      );
-      expect(encoder1.encodedBytes()).to.deep.equal(encoder2.encodedBytes());
-    }
-
     it('can handle different format of timestamp', () => {
       const value1 = { timestampValue: '2016-01-02T10:20:50.850Z' };
       const value2 = { timestampValue: '2016-01-02T10:20:50.850000Z' };
@@ -79,7 +79,6 @@ describe('Firestore Index Value Writer', () => {
           new Timestamp(1451730050, 0)
         )
       };
-
       compareIndexValues(value1, value2, IndexKind.ASCENDING);
       compareIndexValues(value1, value3, IndexKind.ASCENDING);
       compareIndexValues(value1, value4, IndexKind.ASCENDING);
