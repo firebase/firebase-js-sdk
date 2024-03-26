e0e4e9415ec18fa9511bd824d29a73a8d183dc98
milaGGL <107142260+milaGGL@users.noreply.github.com>
2024-03-26 14:15:21 -0400

    resolve comments

--- packages/firestore/test/unit/index/firestore_index_value_writer.test.ts
+++ packages/firestore/test/unit/index/firestore_index_value_writer.test.ts
@@ -24,12 +24,14 @@ import type { Value } from '../../../src/protos/firestore_proto_api';
 import { toTimestamp } from '../../../src/remote/serializer';
 import { JSON_SERIALIZER } from '../local/persistence_test_helpers';
 
+import { compare } from './ordered_code_writer.test';
+
 describe('Firestore Index Value Writer', () => {
-  function compareIndexValues(
+  function compareIndexEncodedValues(
     value1: Value,
     value2: Value,
     direction: IndexKind
-  ): void {
+  ): number {
     const encoder1 = new IndexByteEncoder();
     const encoder2 = new IndexByteEncoder();
     FirestoreIndexValueWriter.INSTANCE.writeIndexValue(
@@ -41,7 +43,8 @@ describe('Firestore Index Value Writer', () => {
       value2,
       encoder2.forKind(direction)
     );
-    expect(encoder1.encodedBytes()).to.deep.equal(encoder2.encodedBytes());
+
+    return compare(encoder1.encodedBytes(), encoder2.encodedBytes());
   }
 
   describe('can gracefully handle different format of timestamp', () => {
@@ -58,15 +61,31 @@ describe('Firestore Index Value Writer', () => {
           new Timestamp(1451730050, 850000000)
         )
       };
-      compareIndexValues(value1, value2, IndexKind.ASCENDING);
-      compareIndexValues(value1, value3, IndexKind.ASCENDING);
-      compareIndexValues(value1, value4, IndexKind.ASCENDING);
-      compareIndexValues(value1, value5, IndexKind.ASCENDING);
+      expect(
+        compareIndexEncodedValues(value1, value2, IndexKind.ASCENDING)
+      ).to.equal(0);
+      expect(
+        compareIndexEncodedValues(value1, value3, IndexKind.ASCENDING)
+      ).to.equal(0);
+      expect(
+        compareIndexEncodedValues(value1, value4, IndexKind.ASCENDING)
+      ).to.equal(0);
+      expect(
+        compareIndexEncodedValues(value1, value5, IndexKind.ASCENDING)
+      ).to.equal(0);
 
-      compareIndexValues(value1, value2, IndexKind.DESCENDING);
-      compareIndexValues(value1, value3, IndexKind.DESCENDING);
-      compareIndexValues(value1, value4, IndexKind.DESCENDING);
-      compareIndexValues(value1, value5, IndexKind.DESCENDING);
+      expect(
+        compareIndexEncodedValues(value1, value2, IndexKind.DESCENDING)
+      ).to.equal(0);
+      expect(
+        compareIndexEncodedValues(value1, value3, IndexKind.DESCENDING)
+      ).to.equal(0);
+      expect(
+        compareIndexEncodedValues(value1, value4, IndexKind.DESCENDING)
+      ).to.equal(0);
+      expect(
+        compareIndexEncodedValues(value1, value5, IndexKind.DESCENDING)
+      ).to.equal(0);
     });
 
     it('can handle timestamps with 0 nanoseconds', () => {
@@ -79,13 +98,76 @@ describe('Firestore Index Value Writer', () => {
           new Timestamp(1451730050, 0)
         )
       };
-      compareIndexValues(value1, value2, IndexKind.ASCENDING);
-      compareIndexValues(value1, value3, IndexKind.ASCENDING);
-      compareIndexValues(value1, value4, IndexKind.ASCENDING);
+      expect(
+        compareIndexEncodedValues(value1, value2, IndexKind.ASCENDING)
+      ).to.equal(0);
+      expect(
+        compareIndexEncodedValues(value1, value3, IndexKind.ASCENDING)
+      ).to.equal(0);
+      expect(
+        compareIndexEncodedValues(value1, value4, IndexKind.ASCENDING)
+      ).to.equal(0);
 
-      compareIndexValues(value1, value2, IndexKind.DESCENDING);
-      compareIndexValues(value1, value3, IndexKind.DESCENDING);
-      compareIndexValues(value1, value4, IndexKind.DESCENDING);
+      expect(
+        compareIndexEncodedValues(value1, value2, IndexKind.DESCENDING)
+      ).to.equal(0);
+      expect(
+        compareIndexEncodedValues(value1, value3, IndexKind.DESCENDING)
+      ).to.equal(0);
+      expect(
+        compareIndexEncodedValues(value1, value4, IndexKind.DESCENDING)
+      ).to.equal(0);
+    });
+
+    it('can compare timestamps with  different formats', () => {
+      const value1 = { timestampValue: '2016-01-02T10:20:50Z' };
+      const value2 = { timestampValue: '2016-01-02T10:20:50.000001Z' };
+      const value3 = {
+        timestampValue: { seconds: 1451730050, nanos: 999999999 }
+      };
+      const value4 = {
+        timestampValue: toTimestamp(
+          JSON_SERIALIZER,
+          new Timestamp(1451730050, 1)
+        )
+      };
+      expect(
+        compareIndexEncodedValues(value1, value2, IndexKind.ASCENDING)
+      ).to.equal(-1);
+      expect(
+        compareIndexEncodedValues(value1, value3, IndexKind.ASCENDING)
+      ).to.equal(-1);
+      expect(
+        compareIndexEncodedValues(value1, value4, IndexKind.ASCENDING)
+      ).to.equal(-1);
+      expect(
+        compareIndexEncodedValues(value2, value3, IndexKind.ASCENDING)
+      ).to.equal(-1);
+      expect(
+        compareIndexEncodedValues(value2, value4, IndexKind.ASCENDING)
+      ).to.equal(1);
+      expect(
+        compareIndexEncodedValues(value3, value4, IndexKind.ASCENDING)
+      ).to.equal(1);
+
+      expect(
+        compareIndexEncodedValues(value1, value2, IndexKind.DESCENDING)
+      ).to.equal(1);
+      expect(
+        compareIndexEncodedValues(value1, value3, IndexKind.DESCENDING)
+      ).to.equal(1);
+      expect(
+        compareIndexEncodedValues(value1, value4, IndexKind.DESCENDING)
+      ).to.equal(1);
+      expect(
+        compareIndexEncodedValues(value2, value3, IndexKind.DESCENDING)
+      ).to.equal(1);
+      expect(
+        compareIndexEncodedValues(value2, value4, IndexKind.DESCENDING)
+      ).to.equal(-1);
+      expect(
+        compareIndexEncodedValues(value3, value4, IndexKind.DESCENDING)
+      ).to.equal(-1);
     });
   });
 });
