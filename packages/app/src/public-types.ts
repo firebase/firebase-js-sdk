6dba50347bfb412646d10283ab8e87c7c43cd9f1
James Daniels <jamesdaniels@google.com>
2024-04-02 12:53:55 -0400

    Quick fix for FirebaseServerApp#toJSON (#8109)
    * Quick fix for FirebaseServerApp#toJSON
    * initalization => initialization

--- packages/app/src/public-types.ts
+++ packages/app/src/public-types.ts
@@ -186,7 +186,7 @@ export interface FirebaseServerAppSettings extends FirebaseAppSettings {
    *
    * If the token fails local verification, or if the Auth service has failed to validate it when
    * the Auth SDK is initialized, then a warning is logged to the console and the Auth SDK will not
-   * sign in a user on initalization.
+   * sign in a user on initialization.
    *
    * If a user is successfully signed in, then the Auth instance's `onAuthStateChanged` callback
    * is invoked with the `User` object as per standard Auth flows. However, `User` objects
