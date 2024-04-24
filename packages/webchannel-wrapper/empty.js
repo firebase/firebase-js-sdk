c6c1f9fd183ea4d30d2cc6a795ebe2159d438e36
Christina Holland <chholland@google.com>
2024-04-24 13:11:48 -0700

    convert webchannel-wrapper package to use rollup

---
+++ packages/webchannel-wrapper/empty.js
@@ -0,0 +1,7 @@
+/**
+ * This package has no main entry point and only allows imports from its
+ * two subpaths. This file is provided for the top-level package.json
+ * "main" field to point to.
+ */
+
+export default {};
