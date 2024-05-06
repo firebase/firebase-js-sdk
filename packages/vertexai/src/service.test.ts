964bd26e34238c3fd058b21a41b826d867a33b14
Christina Holland <chholland@google.com>
2024-05-06 11:20:10 -0700

    [VertexAI] Allow user to set location (#8219)

---
+++ packages/vertexai/src/service.test.ts
@@ -0,0 +1,43 @@
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { DEFAULT_LOCATION } from './constants';
+import { VertexAIService } from './service';
+import { expect } from 'chai';
+
+const fakeApp = {
+  name: 'DEFAULT',
+  automaticDataCollectionEnabled: true,
+  options: {
+    apiKey: 'key',
+    projectId: 'my-project'
+  }
+};
+
+describe('VertexAIService', () => {
+  it('uses default location if not specified', () => {
+    const vertexAI = new VertexAIService(fakeApp);
+    expect(vertexAI.location).to.equal(DEFAULT_LOCATION);
+  });
+  it('uses custom location if specified', () => {
+    const vertexAI = new VertexAIService(
+      fakeApp,
+      /* appCheckProvider */ undefined,
+      { location: 'somewhere' }
+    );
+    expect(vertexAI.location).to.equal('somewhere');
+  });
+});
