4eba38293e29dab4f44283faef59d583a6044741
Daniel La Rocque <dlarocque@google.com>
2024-05-13 10:03:33 -0400

    Fixed formatting

--- packages/vertexai/src/errors.ts
+++ packages/vertexai/src/errors.ts
@@ -1,6 +1,23 @@
-import { FirebaseError } from "@firebase/util";
-import { VertexAIErrorCode, ErrorDetails } from "./types";
-import { VERTEX_TYPE } from "./constants";
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { FirebaseError } from '@firebase/util';
+import { VertexAIErrorCode, ErrorDetails } from './types';
+import { VERTEX_TYPE } from './constants';
 
 /**
  * Error class for the Vertex AI for Firebase SDK.
@@ -30,17 +47,17 @@ export class VertexAIError extends FirebaseError {
     const fullCode = `${service}/${code}`;
     const fullMessage = `${serviceName}: ${message} (${fullCode}).`;
     super(fullCode, fullMessage);
-    
+
     // FirebaseError initializes a stack trace, but it assumes the error is created from the error
     // factory. Since we break this assumption, we set the stack trace to be originating from this
     // constructor.
     // This is only supported in V8.
     if (Error.captureStackTrace) {
-        // Allows us to initialize the stack trace without including the constructor itself at the
-        // top level of the stack trace.
-        Error.captureStackTrace(this, VertexAIError);
+      // Allows us to initialize the stack trace without including the constructor itself at the
+      // top level of the stack trace.
+      Error.captureStackTrace(this, VertexAIError);
     }
-    
+
     // Allows instanceof VertexAIError in ES5/ES6
     // https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work
     Object.setPrototypeOf(this, VertexAIError.prototype);
