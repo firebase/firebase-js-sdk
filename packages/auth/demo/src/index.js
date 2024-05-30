707e49392787c7f87bd7b008c3e0fa6ffcb2a3bf
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-05-30 08:48:01 -0400

    spelling: option
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- packages/auth/demo/src/index.js
+++ packages/auth/demo/src/index.js
@@ -2320,7 +2320,7 @@ function initApp() {
   // reCAPTCHA from being re-rendered (default behavior on enter).
   $('#link-reauth-phone-verification-code').keypress(e => {
     if (e.which === 13) {
-      // User first option option as default.
+      // User first option as default.
       onUpdateConfirmPhoneVerification();
       e.preventDefault();
     }
