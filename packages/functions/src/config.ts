ea3efa9bb1a2d771dec7d2ece98c054bbc6b346b
Daniel La Rocque <dlarocque@google.com>
2024-09-17 10:44:17 -0400

    Remove esm5 from BUILD_TARGET comments

--- packages/functions/src/config.ts
+++ packages/functions/src/config.ts
@@ -69,6 +69,6 @@ export function registerFunctions(
   );
 
   registerVersion(name, version, variant);
-  // BUILD_TARGET will be replaced by values like esm5, esm2017, cjs5, etc during the compilation
+  // BUILD_TARGET will be replaced by values like esm2017, cjs2017, etc during the compilation
   registerVersion(name, version, '__BUILD_TARGET__');
 }
