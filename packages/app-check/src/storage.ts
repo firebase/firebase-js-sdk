b6dd837ce85822ccf8652856b489a7ebe77a706f
Daniel La Rocque <dlarocque@google.com>
2024-10-25 09:08:31 -0400

    Replace uuidv4 generator with `crypto.randomUUID()`
    The uuidv4 generator in util used `Math.random()`, which does not provide strong uniqueness guarantees
    (https://www.bocoup.com/blog/random-numbers).
    
    The places where the uuidv4 generator were used didn't require strong
    uniqueness guarantees (nothing security related), but I think it's good
    to move away from this from util in case we try to use it in the future.
    
    A better built-in alternative is `crypto.randomUUID()`, which does provide strong
    uniqueness guarantees. Since this is a more modern JS built-in, it's
    only [defined in secure
    contexts](https://blog.mozilla.org/security/2018/01/15/secure-contexts-everywhere/).
    Is this something we're concerned about? Are there any App Check users
    with apps running in non-secure environments?

--- packages/app-check/src/storage.ts
+++ packages/app-check/src/storage.ts
@@ -16,7 +16,7 @@
  */
 
 import { FirebaseApp } from '@firebase/app';
-import { isIndexedDBAvailable, uuidv4 } from '@firebase/util';
+import { isIndexedDBAvailable } from '@firebase/util';
 import {
   readDebugTokenFromIndexedDB,
   readTokenFromIndexedDB,
@@ -77,7 +77,7 @@ export async function readOrCreateDebugTokenFromStorage(): Promise<string> {
 
   if (!existingDebugToken) {
     // create a new debug token
-    const newToken = uuidv4();
+    const newToken = crypto.randomUUID();
     // We don't need to block on writing to indexeddb
     // In case persistence failed, a new debug token will be generated every time the page is refreshed.
     // It renders the debug token useless because you have to manually register(whitelist) the new token in the firebase console again and again.
