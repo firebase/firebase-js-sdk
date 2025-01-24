6ce1e5be6985f6a673f2ce10bb9df97ca8c68003
Christina Holland <chholland@google.com>
2025-01-24 12:07:18 -0800

    Update packages, repo-scripts

--- packages/firestore/test/unit/specs/describe_spec.ts
+++ packages/firestore/test/unit/specs/describe_spec.ts
@@ -256,7 +256,7 @@ export function describeSpec(
       space: 2,
       cmp: stringifyComparator
     });
-    writeJSONFile(output);
+    writeJSONFile(output || '');
   }
 }
 
@@ -300,8 +300,8 @@ function stringifyGroup(s: string): number {
  * some logically-first keys before others.
  */
 function stringifyComparator(
-  a: stringify.Element,
-  b: stringify.Element
+  a: { key: string, value: unknown },
+  b: { key: string, value: unknown }
 ): number {
   const aGroup = stringifyGroup(a.key);
   const bGroup = stringifyGroup(b.key);
