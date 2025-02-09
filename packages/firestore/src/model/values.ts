779a06a217a945b3ee06fe385a472ba013819613
Adam Miller <adam@universe.app>
2025-02-09 10:08:11 -0800

    perf: Use charCodeAt instead of TextEncoder for improved UTF string comparison performance.

--- packages/firestore/src/model/values.ts
+++ packages/firestore/src/model/values.ts
@@ -28,7 +28,7 @@ import { fail } from '../util/assert';
 import {
   arrayEquals,
   primitiveComparator,
-  compareUtf8Strings
+  compareUtf16Strings
 } from '../util/misc';
 import { forEach, objectSize } from '../util/obj';
 import { isNegativeZero } from '../util/types';
@@ -255,7 +255,7 @@ export function valueCompare(left: Value, right: Value): number {
         getLocalWriteTime(right)
       );
     case TypeOrder.StringValue:
-      return compareUtf8Strings(left.stringValue!, right.stringValue!);
+      return compareUtf16Strings(left.stringValue!, right.stringValue!);
     case TypeOrder.BlobValue:
       return compareBlobs(left.bytesValue!, right.bytesValue!);
     case TypeOrder.RefValue:
@@ -404,7 +404,7 @@ function compareMaps(left: MapValue, right: MapValue): number {
   rightKeys.sort();
 
   for (let i = 0; i < leftKeys.length && i < rightKeys.length; ++i) {
-    const keyCompare = compareUtf8Strings(leftKeys[i], rightKeys[i]);
+    const keyCompare = compareUtf16Strings(leftKeys[i], rightKeys[i]);
     if (keyCompare !== 0) {
       return keyCompare;
     }
