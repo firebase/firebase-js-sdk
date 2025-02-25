f83c02bb5852451f9ecfc8a4ee7b7e0f4a50ff44
James Daniels <jamesdaniels@google.com>
2025-02-25 14:46:51 -0500

    cleanup postinstall

--- packages/util/postinstall.js
+++ packages/util/postinstall.js
@@ -19,19 +19,19 @@ const { writeFile, readFile } = require('node:fs/promises');
 const { pathToFileURL } = require('node:url');
 const { isAbsolute, join } = require('node:path');
 
-function getConfigFromEnv() {
+new Promise(resolve => {
   if (!process.env.FIREBASE_WEBAPP_CONFIG) {
-    return Promise.resolve(undefined);
+    return resolve(undefined);
   }
 
   // Like FIREBASE_CONFIG (admin autoinit) FIREBASE_WEBAPP_CONFIG can be
   // either a JSON representation of FirebaseOptions or the path to a filename
   if (process.env.FIREBASE_WEBAPP_CONFIG.startsWith('{"')) {
     try {
-      return Promise.resolve(JSON.parse(process.env.FIREBASE_WEBAPP_CONFIG));
+      return resolve(JSON.parse(process.env.FIREBASE_WEBAPP_CONFIG));
     } catch (e) {
       console.error('FIREBASE_WEBAPP_CONFIG could not be parsed.\n', e);
-      return Promise.resolve(undefined);
+      return resolve(undefined);
     }
   }
 
@@ -39,23 +39,23 @@ function getConfigFromEnv() {
   const fileURL = pathToFileURL(
     isAbsolute(fileName) ? fileName : join(process.cwd(), fileName)
   );
-  return readFile(fileURL, 'utf-8').then(
-    fileContents => {
-      try {
-        return JSON.parse(fileContents);
-      } catch (e) {
+  resolve(
+    readFile(fileURL, 'utf-8').then(
+      fileContents => {
+        try {
+          return JSON.parse(fileContents);
+        } catch (e) {
+          console.error(`Contents of "${fileName}" could not be parsed.\n`, e);
+          return undefined;
+        }
+      },
+      e => {
         console.error(`Contents of "${fileName}" could not be parsed.\n`, e);
         return undefined;
       }
-    },
-    e => {
-      console.error(`Contents of "${fileName}" could not be parsed.\n`, e);
-      return undefined;
-    }
+    )
   );
-}
-
-getConfigFromEnv()
+})
   .then(partialConfig => {
     if (!partialConfig) {
       return undefined;
@@ -78,18 +78,21 @@ getConfigFromEnv()
     return fetch(
       `https://firebase.googleapis.com/v1alpha/projects/${projectId}/apps/${appId}/webConfig`,
       { headers: { 'x-goog-api-key': apiKey } }
-    ).then(response => {
-      if (!response.ok) {
-        console.error(
-          `Unable to fetch Firebase config, API returned ${response.statusText} (${response.status})`
-        );
+    ).then(
+      response => {
+        if (!response.ok) {
+          console.error(
+            `Unable to fetch Firebase config, API returned ${response.statusText} (${response.status})`
+          );
+          return undefined;
+        }
+        return response.json().then(json => ({ ...json, apiKey }));
+      },
+      e => {
+        console.error(`Unable to fetch Firebase config\n${e.cause}`);
         return undefined;
       }
-      return response.json().then(json => ({ ...json, apiKey }));
-    }, (e) => {
-      console.error(`Unable to fetch Firebase config\n${e.cause}`);
-      return undefined;
-    });
+    );
   })
   .then(config => {
     const emulatorHosts = Object.entries({
