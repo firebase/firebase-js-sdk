a4a09cff8bd23e68df13ff7af8dbbe77c5435cf2
James Daniels <jamesdaniels@google.com>
2025-02-25 15:34:28 -0500

    Fix subtle break, more guardrails to ensure fail open

--- packages/util/postinstall.js
+++ packages/util/postinstall.js
@@ -15,118 +15,135 @@
  * limitations under the License.
  */
 
-const { writeFile, readFile } = require('node:fs/promises');
-const { pathToFileURL } = require('node:url');
-const { isAbsolute, join } = require('node:path');
+try {
+  const { writeFile, readFile } = require('node:fs/promises');
+  const { pathToFileURL } = require('node:url');
+  const { isAbsolute, join } = require('node:path');
 
-new Promise(resolve => {
-  if (!process.env.FIREBASE_WEBAPP_CONFIG) {
-    return resolve(undefined);
-  }
-
-  // Like FIREBASE_CONFIG (admin autoinit) FIREBASE_WEBAPP_CONFIG can be
-  // either a JSON representation of FirebaseOptions or the path to a filename
-  if (process.env.FIREBASE_WEBAPP_CONFIG.startsWith('{"')) {
-    try {
-      return resolve(JSON.parse(process.env.FIREBASE_WEBAPP_CONFIG));
-    } catch (e) {
-      console.error('FIREBASE_WEBAPP_CONFIG could not be parsed.\n', e);
+  new Promise(resolve => {
+    if (!process.env.FIREBASE_WEBAPP_CONFIG) {
       return resolve(undefined);
     }
-  }
 
-  const fileName = process.env.FIREBASE_WEBAPP_CONFIG;
-  const fileURL = pathToFileURL(
-    isAbsolute(fileName) ? fileName : join(process.cwd(), fileName)
-  );
-  resolve(
-    readFile(fileURL, 'utf-8').then(
-      fileContents => {
-        try {
-          return JSON.parse(fileContents);
-        } catch (e) {
-          console.error(`Contents of "${fileName}" could not be parsed.\n`, e);
-          return undefined;
-        }
-      },
-      e => {
-        console.error(`Contents of "${fileName}" could not be parsed.\n`, e);
-        return undefined;
+    // Like FIREBASE_CONFIG (admin autoinit) FIREBASE_WEBAPP_CONFIG can be
+    // either a JSON representation of FirebaseOptions or the path to a filename
+    if (process.env.FIREBASE_WEBAPP_CONFIG.startsWith('{"')) {
+      try {
+        return resolve(JSON.parse(process.env.FIREBASE_WEBAPP_CONFIG));
+      } catch (e) {
+        console.warn(
+          'FIREBASE_WEBAPP_CONFIG could not be parsed, ignoring.\n',
+          e
+        );
+        return resolve(undefined);
       }
-    )
-  );
-})
-  .then(partialConfig => {
-    if (!partialConfig) {
-      return undefined;
-    }
-    // In Firebase App Hosting the config provided to the environment variable is up-to-date and
-    // "complete" we should not reach out to the webConfig endpoint to freshen it
-    if (process.env.X_GOOGLE_TARGET_PLATFORM === 'fah') {
-      return partialConfig;
-    }
-    const projectId = partialConfig.projectId || '-';
-    const appId = partialConfig.appId;
-    const apiKey = partialConfig.apiKey;
-    if (!appId || !apiKey) {
-      console.error(
-        `Unable to fetch Firebase config, appId and apiKey are required.`
-      );
-      return undefined;
     }
 
-    return fetch(
-      `https://firebase.googleapis.com/v1alpha/projects/${projectId}/apps/${appId}/webConfig`,
-      { headers: { 'x-goog-api-key': apiKey } }
-    ).then(
-      response => {
-        if (!response.ok) {
-          console.error(
-            `Unable to fetch Firebase config, API returned ${response.statusText} (${response.status})`
+    const fileName = process.env.FIREBASE_WEBAPP_CONFIG;
+    const fileURL = pathToFileURL(
+      isAbsolute(fileName) ? fileName : join(process.cwd(), fileName)
+    );
+    resolve(
+      readFile(fileURL, 'utf-8').then(
+        fileContents => {
+          try {
+            return JSON.parse(fileContents);
+          } catch (e) {
+            console.warn(
+              `Contents of "${fileName}" could not be parsed, ignoring FIREBASE_WEBAPP_CONFIG.\n`,
+              e
+            );
+            return undefined;
+          }
+        },
+        e => {
+          console.warn(
+            `Contents of "${fileName}" could not be parsed, ignoring FIREBASE_WEBAPP_CONFIG.\n`,
+            e
           );
           return undefined;
         }
-        return response.json().then(json => ({ ...json, apiKey }));
-      },
-      e => {
-        console.error(`Unable to fetch Firebase config\n${e.cause}`);
+      )
+    );
+  })
+    .then(partialConfig => {
+      if (!partialConfig) {
         return undefined;
       }
-    );
-  })
-  .then(config => {
-    const emulatorHosts = Object.entries({
-      firestore: process.env.FIRESTORE_EMULATOR_HOST,
-      database: process.env.FIREBASE_DATABASE_EMULATOR_HOST,
-      storage: process.env.FIREBASE_STORAGE_EMULATOR_HOST,
-      auth: process.env.FIREBASE_AUTH_EMULATOR_HOST
-    }).reduce(
-      // We want a falsy value if none of the above are defined
-      (current, [key, value]) =>
-        value ? { ...current, [key]: value } : current,
-      undefined
-    );
+      // In Firebase App Hosting the config provided to the environment variable is up-to-date and
+      // "complete" we should not reach out to the webConfig endpoint to freshen it
+      if (process.env.X_GOOGLE_TARGET_PLATFORM === 'fah') {
+        return partialConfig;
+      }
+      const projectId = partialConfig.projectId || '-';
+      const appId = partialConfig.appId;
+      const apiKey = partialConfig.apiKey;
+      if (!appId || !apiKey) {
+        console.warn(
+          `Unable to fetch Firebase config, appId and apiKey are required, ignoring FIREBASE_WEBAPP_CONFIG.`
+        );
+        return undefined;
+      }
 
-    // getDefaults() will use this object, rather than fallback to other autoinit suppliers, if it's
-    // truthy—if we've done nothing here, make it falsy.
-    const defaults =
-      config || emulatorHosts ? { config, emulatorHosts } : undefined;
+      const url = `https://firebase.googleapis.com/v1alpha/projects/${projectId}/apps/${appId}/webConfig`;
+      return fetch(url, { headers: { 'x-goog-api-key': apiKey } }).then(
+        response => {
+          if (!response.ok) {
+            console.warn(
+              `Unable to fetch Firebase config, ignoring FIREBASE_WEBAPP_CONFIG.`
+            );
+            console.warn(
+              `${url} returned ${response.statusText} (${response.status})`
+            );
+            return undefined;
+          }
+          return response.json().then(json => ({ ...json, apiKey }));
+        },
+        e => {
+          console.warn(
+            `Unable to fetch Firebase config, ignoring FIREBASE_WEBAPP_CONFIG.\n${e.cause}`
+          );
+          return undefined;
+        }
+      );
+    })
+    .then(config => {
+      const emulatorHosts = {
+        firestore: process.env.FIRESTORE_EMULATOR_HOST,
+        database: process.env.FIREBASE_DATABASE_EMULATOR_HOST,
+        storage: process.env.FIREBASE_STORAGE_EMULATOR_HOST,
+        auth: process.env.FIREBASE_AUTH_EMULATOR_HOST
+      };
 
-    return Promise.all([
-      writeFile(
-        join(__dirname, 'dist', 'autoinit_env.js'),
-        `'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.postinstallDefaults = ${JSON.stringify(defaults)};`
-      ),
-      writeFile(
-        join(__dirname, 'dist', 'autoinit_env.mjs'),
-        `const postinstallDefaults = ${JSON.stringify(defaults)};
-export { postinstallDefaults };`
-      )
-    ]);
-  })
-  .then(
-    () => process.exit(0),
-    () => process.exit(0)
+      const defaults = config && { config, emulatorHosts };
+
+      return Promise.all([
+        writeFile(
+          join(__dirname, 'dist', 'autoinit_env.js'),
+          `'use strict';
+  Object.defineProperty(exports, '__esModule', { value: true });
+  exports.postinstallDefaults = ${JSON.stringify(defaults)};`
+        ),
+        writeFile(
+          join(__dirname, 'dist', 'autoinit_env.mjs'),
+          `const postinstallDefaults = ${JSON.stringify(defaults)};
+  export { postinstallDefaults };`
+        )
+      ]);
+    })
+    .then(
+      () => process.exit(0),
+      e => {
+        console.warn(
+          'Unexpected error encountered in @firebase/util postinstall script, ignoring FIREBASE_WEBAPP_CONFIG.\n',
+          e
+        );
+        process.exit(0);
+      }
+    );
+} catch (e) {
+  console.warn(
+    'Unexpected error encountered in @firebase/util postinstall script, ignoring FIREBASE_WEBAPP_CONFIG\n',
+    e
   );
+}
