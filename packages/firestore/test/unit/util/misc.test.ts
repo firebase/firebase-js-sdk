b4c85dd4d60f0f3c4bc75b627b88e635b5628508
milaGGL <107142260+milaGGL@users.noreply.github.com>
2025-03-03 15:37:56 -0500

    cut by 1/10 again

--- packages/firestore/test/unit/util/misc.test.ts
+++ packages/firestore/test/unit/util/misc.test.ts
@@ -62,7 +62,7 @@ describe('CompareUtf8Strings', () => {
     const stringGenerator = new StringGenerator(new Random(seed), 0.33, 20);
     const stringPairGenerator = new StringPairGenerator(stringGenerator);
 
-    for (let i = 0; i < 100000 && errors.length < 10; i++) {
+    for (let i = 0; i < 10000 && errors.length < 10; i++) {
       const { s1, s2 } = stringPairGenerator.next();
 
       const actual = compareUtf8Strings(s1, s2);
