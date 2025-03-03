565efe84dfa73e8cd4d460dfe68654fe5e8ad1b7
milaGGL <107142260+milaGGL@users.noreply.github.com>
2025-03-03 15:06:40 -0500

    cut the unit test case size

--- packages/firestore/test/unit/util/misc.test.ts
+++ packages/firestore/test/unit/util/misc.test.ts
@@ -62,7 +62,7 @@ describe('CompareUtf8Strings', () => {
     const stringGenerator = new StringGenerator(new Random(seed), 0.33, 20);
     const stringPairGenerator = new StringPairGenerator(stringGenerator);
 
-    for (let i = 0; i < 1000000 && errors.length < 10; i++) {
+    for (let i = 0; i < 100000 && errors.length < 10; i++) {
       const { s1, s2 } = stringPairGenerator.next();
 
       const actual = compareUtf8Strings(s1, s2);
@@ -87,7 +87,7 @@ describe('CompareUtf8Strings', () => {
       );
       throw new Error('Test failed');
     }
-  }).timeout(20000);
+  });
 
   class StringPair {
     constructor(readonly s1: string, readonly s2: string) {}
