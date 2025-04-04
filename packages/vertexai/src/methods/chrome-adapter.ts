a32421d512706a74fc29fd231e5aed431035e71d
Erik Eldridge <erikeldridge@google.com>
2025-04-04 16:19:44 -0700

    Remove request.systemInstruction validation
    We only define system prompts via onDeviceParams
    initialization.

--- packages/vertexai/src/methods/chrome-adapter.ts
+++ packages/vertexai/src/methods/chrome-adapter.ts
@@ -20,8 +20,7 @@ import {
   GenerateContentRequest,
   InferenceMode,
   Part,
-  Role,
-  TextPart
+  Role
 } from '../types';
 import {
   Availability,
@@ -123,25 +122,6 @@ export class ChromeAdapter {
       }
 
       if (!content.parts[0].text) {
-        return false;
-      }
-    }
-
-    if (request.systemInstruction) {
-      const systemContent = request.systemInstruction as Content;
-      // Returns false if the role can't be represented on-device.
-      if (systemContent.role && systemContent.role === 'function') {
-        return false;
-      }
-
-      // Returns false if the system prompt is multi-part.
-      if (systemContent.parts && systemContent.parts.length > 1) {
-        return false;
-      }
-
-      // Returns false if the system prompt isn't text.
-      const systemText = request.systemInstruction as TextPart;
-      if (!systemText.text) {
         return false;
       }
     }
