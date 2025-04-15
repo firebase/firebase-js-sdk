1652e8bdc51f23b1048c7d730598f9f39faeb349
DellaBitta <drsanta@google.com>
2025-04-15 17:33:51 -0400

    format

--- packages/firestore/test/unit/api/vector_value.test.ts
+++ packages/firestore/test/unit/api/vector_value.test.ts
@@ -21,33 +21,49 @@ import { VectorValue } from '../../../src';
 
 describe('VectorValue', () => {
   it('fromJSON reconstructs the value from toJSON', () => {
-    const num : number[] = [1,2,3];
+    const num: number[] = [1, 2, 3];
     const vectorValue = new VectorValue(num);
     const json = vectorValue.toJSON();
     const parsedVectorValue = VectorValue.fromJSON(json);
     expect(vectorValue.isEqual(parsedVectorValue)).to.be.true;
   });
-  
+
   it('fromJSON parameter order does not matter', () => {
     const type = 'firestore/vectorValue/1.0';
-    const data = [1,2,3];
-    expect(() => {VectorValue.fromJSON({data, type});}).to.not.throw;
-    expect(() => {VectorValue.fromJSON({type, data});}).to.not.throw;
+    const data = [1, 2, 3];
+    expect(() => {
+      VectorValue.fromJSON({ data, type });
+    }).to.not.throw;
+    expect(() => {
+      VectorValue.fromJSON({ type, data });
+    }).to.not.throw;
   });
 
   it('fromJSON empty array does not throw', () => {
     const type = 'firestore/vectorValue/1.0';
-    const data = [1,2,3];
-    expect(() => {VectorValue.fromJSON({type, data});}).to.not.throw;
+    const data = [1, 2, 3];
+    expect(() => {
+      VectorValue.fromJSON({ type, data });
+    }).to.not.throw;
   });
 
   it('fromJSON missing fields throws', () => {
     const type = 'firestore/vectorValue/1.0';
-    const data = [1,2,3];
-    expect(() => {VectorValue.fromJSON({type /* missing data */});}).to.throw;
-    expect(() => {VectorValue.fromJSON({data /* missing type */});}).to.throw;
-    expect(() => {VectorValue.fromJSON({type: 1, data });}).to.throw;
-    expect(() => {VectorValue.fromJSON({type: "firestore/bytes/1.0", data });}).to.throw;
-    expect(() => {VectorValue.fromJSON({type, data: "not a number" });});
+    const data = [1, 2, 3];
+    expect(() => {
+      VectorValue.fromJSON({ type /* missing data */ });
+    }).to.throw;
+    expect(() => {
+      VectorValue.fromJSON({ data /* missing type */ });
+    }).to.throw;
+    expect(() => {
+      VectorValue.fromJSON({ type: 1, data });
+    }).to.throw;
+    expect(() => {
+      VectorValue.fromJSON({ type: 'firestore/bytes/1.0', data });
+    }).to.throw;
+    expect(() => {
+      VectorValue.fromJSON({ type, data: 'not a number' });
+    });
   });
 });
