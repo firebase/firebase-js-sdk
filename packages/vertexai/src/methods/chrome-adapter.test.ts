12fe88208adc2ed665f71d7dc91ea353e947815c
Erik Eldridge <erikeldridge@google.com>
2025-04-21 19:43:17 -0700

    VinF Hybrid Inference: narrow Chrome input type (#8953)

--- packages/vertexai/src/methods/chrome-adapter.test.ts
+++ packages/vertexai/src/methods/chrome-adapter.test.ts
@@ -22,7 +22,8 @@ import { ChromeAdapter } from './chrome-adapter';
 import {
   Availability,
   LanguageModel,
-  LanguageModelCreateOptions
+  LanguageModelCreateOptions,
+  LanguageModelMessageContent
 } from '../types/language-model';
 import { stub } from 'sinon';
 import { GenerateContentRequest } from '../types';
@@ -100,22 +101,6 @@ describe('ChromeAdapter', () => {
             {
               role: 'function',
               parts: []
-            }
-          ]
-        })
-      ).to.be.false;
-    });
-    it('returns false if request content has multiple parts', async () => {
-      const adapter = new ChromeAdapter(
-        {} as LanguageModel,
-        'prefer_on_device'
-      );
-      expect(
-        await adapter.isAvailable({
-          contents: [
-            {
-              role: 'user',
-              parts: [{ text: 'a' }, { text: 'b' }]
             }
           ]
         })
@@ -281,7 +266,8 @@ describe('ChromeAdapter', () => {
         create: () => Promise.resolve({})
       } as LanguageModel;
       const languageModel = {
-        prompt: i => Promise.resolve(i)
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        prompt: (p: LanguageModelMessageContent[]) => Promise.resolve('')
       } as LanguageModel;
       const createStub = stub(languageModelProvider, 'create').resolves(
         languageModel
@@ -305,13 +291,8 @@ describe('ChromeAdapter', () => {
       // Asserts Vertex input type is mapped to Chrome type.
       expect(promptStub).to.have.been.calledOnceWith([
         {
-          role: request.contents[0].role,
-          content: [
-            {
-              type: 'text',
-              content: request.contents[0].parts[0].text
-            }
-          ]
+          type: 'text',
+          content: request.contents[0].parts[0].text
         }
       ]);
       // Asserts expected output.
@@ -366,13 +347,8 @@ describe('ChromeAdapter', () => {
       // Asserts Vertex input type is mapped to Chrome type.
       expect(measureInputUsageStub).to.have.been.calledOnceWith([
         {
-          role: 'user',
-          content: [
-            {
-              type: 'text',
-              content: inputText
-            }
-          ]
+          type: 'text',
+          content: inputText
         }
       ]);
       expect(await response.json()).to.deep.equal({
@@ -380,7 +356,7 @@ describe('ChromeAdapter', () => {
       });
     });
   });
-  describe('generateContentStreamOnDevice', () => {
+  describe('generateContentStream', () => {
     it('generates content stream', async () => {
       const languageModelProvider = {
         create: () => Promise.resolve({})
@@ -413,13 +389,8 @@ describe('ChromeAdapter', () => {
       expect(createStub).to.have.been.calledOnceWith(onDeviceParams);
       expect(promptStub).to.have.been.calledOnceWith([
         {
-          role: request.contents[0].role,
-          content: [
-            {
-              type: 'text',
-              content: request.contents[0].parts[0].text
-            }
-          ]
+          type: 'text',
+          content: request.contents[0].parts[0].text
         }
       ]);
       const actual = await toStringArray(response.body!);
