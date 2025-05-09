8a03143b9217effdd86d68bdf195493c0979aa27
Mark Duckworth <1124037+MarkDuckworth@users.noreply.github.com>
2025-05-09 14:45:08 -0600

    Fix encoding of CSI keys in Safari (#8993)

--- packages/firestore/src/index/index_entry.ts
+++ packages/firestore/src/index/index_entry.ts
@@ -15,15 +15,19 @@
  * limitations under the License.
  */
 
+import { isSafariOrWebkit } from '@firebase/util';
+
+import { DbIndexEntry } from '../local/indexeddb_schema';
+import { DbIndexEntryKey, KeySafeBytes } from '../local/indexeddb_sentinels';
 import { DocumentKey } from '../model/document_key';
 
 /** Represents an index entry saved by the SDK in persisted storage. */
 export class IndexEntry {
   constructor(
-    readonly indexId: number,
-    readonly documentKey: DocumentKey,
-    readonly arrayValue: Uint8Array,
-    readonly directionalValue: Uint8Array
+    readonly _indexId: number,
+    readonly _documentKey: DocumentKey,
+    readonly _arrayValue: Uint8Array,
+    readonly _directionalValue: Uint8Array
   ) {}
 
   /**
@@ -31,26 +35,59 @@ export class IndexEntry {
    * directional value.
    */
   successor(): IndexEntry {
-    const currentLength = this.directionalValue.length;
+    const currentLength = this._directionalValue.length;
     const newLength =
-      currentLength === 0 || this.directionalValue[currentLength - 1] === 255
+      currentLength === 0 || this._directionalValue[currentLength - 1] === 255
         ? currentLength + 1
         : currentLength;
 
     const successor = new Uint8Array(newLength);
-    successor.set(this.directionalValue, 0);
+    successor.set(this._directionalValue, 0);
     if (newLength !== currentLength) {
-      successor.set([0], this.directionalValue.length);
+      successor.set([0], this._directionalValue.length);
     } else {
       ++successor[successor.length - 1];
     }
 
     return new IndexEntry(
-      this.indexId,
-      this.documentKey,
-      this.arrayValue,
+      this._indexId,
+      this._documentKey,
+      this._arrayValue,
       successor
     );
+  }
+
+  // Create a representation of the Index Entry as a DbIndexEntry
+  dbIndexEntry(
+    uid: string,
+    orderedDocumentKey: Uint8Array,
+    documentKey: DocumentKey
+  ): DbIndexEntry {
+    return {
+      indexId: this._indexId,
+      uid,
+      arrayValue: encodeKeySafeBytes(this._arrayValue),
+      directionalValue: encodeKeySafeBytes(this._directionalValue),
+      orderedDocumentKey: encodeKeySafeBytes(orderedDocumentKey),
+      documentKey: documentKey.path.toArray()
+    };
+  }
+
+  // Create a representation of the Index Entry as a DbIndexEntryKey
+  dbIndexEntryKey(
+    uid: string,
+    orderedDocumentKey: Uint8Array,
+    documentKey: DocumentKey
+  ): DbIndexEntryKey {
+    const entry = this.dbIndexEntry(uid, orderedDocumentKey, documentKey);
+    return [
+      entry.indexId,
+      entry.uid,
+      entry.arrayValue,
+      entry.directionalValue,
+      entry.orderedDocumentKey,
+      entry.documentKey
+    ];
   }
 }
 
@@ -58,22 +95,22 @@ export function indexEntryComparator(
   left: IndexEntry,
   right: IndexEntry
 ): number {
-  let cmp = left.indexId - right.indexId;
+  let cmp = left._indexId - right._indexId;
   if (cmp !== 0) {
     return cmp;
   }
 
-  cmp = compareByteArrays(left.arrayValue, right.arrayValue);
+  cmp = compareByteArrays(left._arrayValue, right._arrayValue);
   if (cmp !== 0) {
     return cmp;
   }
 
-  cmp = compareByteArrays(left.directionalValue, right.directionalValue);
+  cmp = compareByteArrays(left._directionalValue, right._directionalValue);
   if (cmp !== 0) {
     return cmp;
   }
 
-  return DocumentKey.comparator(left.documentKey, right.documentKey);
+  return DocumentKey.comparator(left._documentKey, right._documentKey);
 }
 
 export function compareByteArrays(left: Uint8Array, right: Uint8Array): number {
@@ -84,4 +121,58 @@ export function compareByteArrays(left: Uint8Array, right: Uint8Array): number {
     }
   }
   return left.length - right.length;
+}
+
+/**
+ * Workaround for WebKit bug: https://bugs.webkit.org/show_bug.cgi?id=292721
+ * Create a key safe representation of Uint8Array values.
+ * If the browser is detected as Safari or WebKit, then
+ * the input array will be converted to "sortable byte string".
+ * Otherwise, the input array will be returned in its original type.
+ */
+export function encodeKeySafeBytes(array: Uint8Array): KeySafeBytes {
+  if (isSafariOrWebkit()) {
+    return encodeUint8ArrayToSortableString(array);
+  }
+  return array;
+}
+
+/**
+ * Reverts the key safe representation of Uint8Array (created by
+ * encodeKeySafeBytes) to a normal Uint8Array.
+ */
+export function decodeKeySafeBytes(input: KeySafeBytes): Uint8Array {
+  if (typeof input !== 'string') {
+    return input;
+  }
+  return decodeSortableStringToUint8Array(input);
+}
+
+/**
+ * Encodes a Uint8Array into a "sortable byte string".
+ * A "sortable byte string" sorts in the same order as the Uint8Array.
+ * This works because JS string comparison sorts strings based on code points.
+ */
+function encodeUint8ArrayToSortableString(array: Uint8Array): string {
+  let byteString = '';
+  for (let i = 0; i < array.length; i++) {
+    byteString += String.fromCharCode(array[i]);
+  }
+
+  return byteString;
+}
+
+/**
+ * Decodes a "sortable byte string" back into a Uint8Array.
+ * A "sortable byte string" is assumed to be created where each character's
+ * Unicode code point directly corresponds to a single byte value (0-255).
+ */
+function decodeSortableStringToUint8Array(byteString: string): Uint8Array {
+  const uint8array = new Uint8Array(byteString.length);
+
+  for (let i = 0; i < byteString.length; i++) {
+    uint8array[i] = byteString.charCodeAt(i);
+  }
+
+  return uint8array;
 }
