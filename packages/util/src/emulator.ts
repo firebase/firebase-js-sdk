0615f555aa2f37bf3c94bfd95d50857c5dbec55a
Maneesh Tewani <mtewani@google.com>
2025-05-12 11:59:55 -0700

    Finalized changes

--- packages/util/src/emulator.ts
+++ packages/util/src/emulator.ts
@@ -257,7 +257,7 @@ export function updateEmulatorBanner(
     learnMoreLink.setAttribute('id', learnMoreId);
     learnMoreLink.innerText = 'Learn more';
     learnMoreLink.href =
-      'http://firebase.google.com/docs/studio/deploy-app#emulator ';
+      'https://firebase.google.com/docs/studio/preview-apps#preview-backend';
     learnMoreLink.setAttribute('target', '__blank');
     learnMoreLink.style.paddingLeft = '5px';
     learnMoreLink.style.textDecoration = 'underline';
@@ -290,7 +290,7 @@ export function updateEmulatorBanner(
     }
 
     if (showError) {
-      firebaseText.innerText = `Not using emulated backend.`;
+      firebaseText.innerText = `Preview backend disconnected.`;
       prependIcon.innerHTML = `<g clip-path="url(#clip0_6013_33858)">
 <path d="M4.8 17.6L12 5.6L19.2 17.6H4.8ZM6.91667 16.4H17.0833L12 7.93333L6.91667 16.4ZM12 15.6C12.1667 15.6 12.3056 15.5444 12.4167 15.4333C12.5389 15.3111 12.6 15.1667 12.6 15C12.6 14.8333 12.5389 14.6944 12.4167 14.5833C12.3056 14.4611 12.1667 14.4 12 14.4C11.8333 14.4 11.6889 14.4611 11.5667 14.5833C11.4556 14.6944 11.4 14.8333 11.4 15C11.4 15.1667 11.4556 15.3111 11.5667 15.4333C11.6889 15.5444 11.8333 15.6 12 15.6ZM11.4 13.6H12.6V10.4H11.4V13.6Z" fill="#212121"/>
 </g>
@@ -308,7 +308,7 @@ export function updateEmulatorBanner(
 <rect width="24" height="24" fill="white"/>
 </clipPath>
 </defs>`;
-      firebaseText.innerText = 'Using emulated backend';
+      firebaseText.innerText = 'Preview backend running in this workspace.';
     }
     firebaseText.setAttribute('id', firebaseTextId);
   }
