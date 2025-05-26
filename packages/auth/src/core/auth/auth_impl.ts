a5a79f103e22758f2c1beb181ba74211b11a2353
mansisampat <sammansi@google.com>
2025-05-26 19:10:17 +0530

    Running yarn docgen:all

--- packages/auth/src/core/auth/auth_impl.ts
+++ packages/auth/src/core/auth/auth_impl.ts
@@ -460,7 +460,7 @@ export class AuthImpl implements AuthInternal, _FirebaseService {
   async _updateFirebaseToken(
     firebaseToken: FirebaseToken | null
   ): Promise<void> {
-      this.firebaseToken = firebaseToken;
+    this.firebaseToken = firebaseToken;
   }
 
   async signOut(): Promise<void> {
