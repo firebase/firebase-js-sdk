29d1c168d1476398623a582e0f0486b71e1dc590
eranc <eranc@google.com>
2025-06-08 16:12:35 +0300

    feat(build): Add rollup config for google3 modular typings

--- packages/remote-config/rollup.config.js
+++ packages/remote-config/rollup.config.js
@@ -19,6 +19,7 @@ import json from '@rollup/plugin-json'; // Enables package.json import in TypeSc
 import typescriptPlugin from 'rollup-plugin-typescript2';
 import replace from 'rollup-plugin-replace';
 import typescript from 'typescript';
+import dts from 'rollup-plugin-dts';
 import { generateBuildTargetReplaceConfig } from '../../scripts/build/rollup_replace_build_target';
 import { emitModulePackageFile } from '../../scripts/build/rollup_emit_module_package_file';
 import pkg from './package.json';
@@ -70,4 +71,19 @@ const cjsBuild = {
   ]
 };
 
-export default [esmBuild, cjsBuild];
+
+const google3TypingsBuild = {
+  input: 'dist/src/index.d.ts',
+  output: {
+    file: 'dist/esm/src/global_index.d.ts',
+    format: 'es'
+  },
+  plugins: [
+    dts({
+      respectExternal: true
+    })
+  ]
+};
+
+
+export default [esmBuild, cjsBuild, google3TypingsBuild];
