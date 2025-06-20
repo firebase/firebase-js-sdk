d035ea0107a6954dbdfb2692042ecbb9fb1bdfc4
Maneesh Tewani <mtewani@google.com>
2025-06-20 09:37:02 -0700

    Fixed issue where storage wasn't passing cookies when using firebase studio

--- packages/storage/src/platform/browser/connection.ts
+++ packages/storage/src/platform/browser/connection.ts
@@ -70,7 +70,7 @@ abstract class XhrConnection<T extends ConnectionType>
     if (this.sent_) {
       throw internalError('cannot .send() more than once');
     }
-    if (isCloudWorkstation(url) && isUsingEmulator) {
+    if (isCloudWorkstation(new URL(url).hostname) && isUsingEmulator) {
       this.xhr_.withCredentials = true;
     }
     this.sent_ = true;
