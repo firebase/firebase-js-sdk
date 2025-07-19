24848c407d72a21844b4ffa3187c5cd52bb18245
Athira M <athiramanu@google.com>
2025-07-19 08:18:43 +0530

    [Fix] Storage cache is not updating when there are no experiments in response

--- packages/remote-config/src/api.ts
+++ packages/remote-config/src/api.ts
@@ -97,7 +97,6 @@ export async function activate(remoteConfig: RemoteConfig): Promise<boolean> {
   if (
     !lastSuccessfulFetchResponse ||
     !lastSuccessfulFetchResponse.config ||
-    !lastSuccessfulFetchResponse.experiments ||
     !lastSuccessfulFetchResponse.eTag ||
     lastSuccessfulFetchResponse.eTag === activeConfigEtag
   ) {
