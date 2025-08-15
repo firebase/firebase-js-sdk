3f1a4be129190a604e99113e7cea5f08efea88da
Daniel La Rocque <dlarocque@google.com>
2025-08-15 14:07:07 -0400

    add license header

--- packages/ai/src/types/live-responses.ts
+++ packages/ai/src/types/live-responses.ts
@@ -1,6 +1,22 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { Content, GenerativeContentBlob, Part } from './content';
 import { LiveGenerationConfig, Tool, ToolConfig } from './requests';
-
 
 /**
  * User input that is sent to the model.
@@ -41,4 +57,3 @@ export interface _LiveClientSetup {
     systemInstruction?: string | Part | Content;
   };
 }
-
