firebase/firebase-js-sdk

commit 095c098de1e4399f3fb2993edae45060b2a8c6d0

remove accidental factory export (#9232)

packages/ai/src/index.ts
@@ -43,7 +43,7 @@ declare global {
   }
 }
 
-export function factory(
+function factory(
   container: ComponentContainer,
   { instanceIdentifier }: InstanceFactoryOptions
 ): AIService {

commit a4848b401f6e8da16b0d0fdbfd064e8d68566555

fix(ai): Fix test script broken by #9232 (#9235)

packages/ai/src/factory-browser.ts
@@ -1,0 +1,53 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  ComponentContainer,
+  InstanceFactoryOptions
+} from '@firebase/component';
+import { AIError } from './errors';
+import { decodeInstanceIdentifier } from './helpers';
+import { chromeAdapterFactory } from './methods/chrome-adapter';
+import { AIService } from './service';
+import { AIErrorCode } from './types';
+
+export function factory(
+  container: ComponentContainer,
+  { instanceIdentifier }: InstanceFactoryOptions
+): AIService {
+  if (!instanceIdentifier) {
+    throw new AIError(
+      AIErrorCode.ERROR,
+      'AIService instance identifier is undefined.'
+    );
+  }
+
+  const backend = decodeInstanceIdentifier(instanceIdentifier);
+
+  // getImmediate for FirebaseApp will always succeed
+  const app = container.getProvider('app').getImmediate();
+  const auth = container.getProvider('auth-internal');
+  const appCheckProvider = container.getProvider('app-check-internal');
+
+  return new AIService(
+    app,
+    backend,
+    auth,
+    appCheckProvider,
+    chromeAdapterFactory
+  );
+}

packages/ai/src/index.ts
@@ -22,52 +22,16 @@
  */
 
 import { registerVersion, _registerComponent } from '@firebase/app';
-import { AIService } from './service';
 import { AI_TYPE } from './constants';
-import {
-  Component,
-  ComponentContainer,
-  ComponentType,
-  InstanceFactoryOptions
-} from '@firebase/component';
+import { Component, ComponentType } from '@firebase/component';
 import { name, version } from '../package.json';
-import { decodeInstanceIdentifier } from './helpers';
-import { AIError } from './api';
-import { AIErrorCode } from './types';
-import { chromeAdapterFactory } from './methods/chrome-adapter';
 import { LanguageModel } from './types/language-model';
+import { factory } from './factory-browser';
 
 declare global {
   interface Window {
     LanguageModel: LanguageModel;
   }
-}
-
-function factory(
-  container: ComponentContainer,
-  { instanceIdentifier }: InstanceFactoryOptions
-): AIService {
-  if (!instanceIdentifier) {
-    throw new AIError(
-      AIErrorCode.ERROR,
-      'AIService instance identifier is undefined.'
-    );
-  }
-
-  const backend = decodeInstanceIdentifier(instanceIdentifier);
-
-  // getImmediate for FirebaseApp will always succeed
-  const app = container.getProvider('app').getImmediate();
-  const auth = container.getProvider('auth-internal');
-  const appCheckProvider = container.getProvider('app-check-internal');
-
-  return new AIService(
-    app,
-    backend,
-    auth,
-    appCheckProvider,
-    chromeAdapterFactory
-  );
 }
 
 function registerAI(): void {

packages/ai/test-utils/get-fake-firebase-services.ts
@@ -24,7 +24,7 @@ import {
 import { Component, ComponentType } from '@firebase/component';
 import { FirebaseAppCheckInternal } from '@firebase/app-check-interop-types';
 import { AI_TYPE } from '../src/constants';
-import { factory } from '../src';
+import { factory } from '../src/factory-browser';
 
 const fakeConfig = {
   projectId: 'projectId',
@@ -38,7 +38,11 @@ export function getFullApp(fakeAppParams?: {
   appId?: string;
   apiKey?: string;
 }): FirebaseApp {
-  _registerComponent(new Component(AI_TYPE, factory, ComponentType.PUBLIC));
+  _registerComponent(
+    new Component(AI_TYPE, factory, ComponentType.PUBLIC).setMultipleInstances(
+      true
+    )
+  );
   _registerComponent(
     new Component(
       'app-check-internal',