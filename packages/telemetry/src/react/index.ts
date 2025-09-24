391c2e818e27a344a682c3f4012b9e3a47a59cc2
abrook <abrook@google.com>
2025-09-24 14:39:28 -0400

    Implement and test FirebaseTelemetry component

--- packages/telemetry/src/react/index.ts
+++ packages/telemetry/src/react/index.ts
@@ -1,13 +1,34 @@
-import { getApp } from '@firebase/app';
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { FirebaseOptions, initializeApp } from '@firebase/app';
+import { registerTelemetry } from '../register';
 import { captureError, getTelemetry } from '../api';
 import { Component, ReactNode } from 'react';
 
-export interface FirebaseTelemetryBoundaryProps {
-  children: ReactNode;
+registerTelemetry();
+
+export interface FirebaseTelemetryProps {
+  firebaseOptions?: FirebaseOptions;
+  children?: ReactNode;
 }
 
-export class FirebaseTelemetryBoundary extends Component<FirebaseTelemetryBoundaryProps> {
-  constructor(public props: FirebaseTelemetryBoundaryProps) {
+export class FirebaseTelemetry extends Component<FirebaseTelemetryProps> {
+  constructor(public props: FirebaseTelemetryProps) {
     super(props);
   }
 
@@ -16,19 +37,28 @@ export class FirebaseTelemetryBoundary extends Component<FirebaseTelemetryBounda
       return;
     }
 
-    // TODO: This will be obsolete once the SDK has a default endpoint
+    // TODO: This will be removed once there is a default endpoint
     process.env.OTEL_ENDPOINT = window.location.origin;
 
-    const telemetry = getTelemetry(getApp());
+    try {
+      const telemetry = this.props.firebaseOptions
+        ? getTelemetry(initializeApp(this.props.firebaseOptions))
+        : getTelemetry();
 
-    console.info(telemetry);
+      window.addEventListener('error', (event: ErrorEvent) => {
+        captureError(telemetry, event.error, {});
+      });
 
-    window.addEventListener('error', (event: ErrorEvent) => {
-      captureError(telemetry, event.error, {'example_attribute': 'hello'});
-    });
-
-    // TODO: add listener for unhandledrejection
-
+      window.addEventListener(
+        'unhandledrejection',
+        (event: PromiseRejectionEvent) => {
+          captureError(telemetry, event.reason, {});
+        }
+      );
+    } catch (error) {
+      // Log the error here, but don't die.
+      console.warn(`Firebase Telemetry was not initialized:\n`, error);
+    }
   }
 
   render(): ReactNode {
