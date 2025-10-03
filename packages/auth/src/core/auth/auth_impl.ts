64a5a863e0448fd10a5741460689159ddab265e8
mansisampat <sammansi@google.com>
2025-10-03 11:17:42 +0530

    Update the backend endpoint for exchangeOidcToken to prod endpoint. (#9292)

--- packages/auth/src/core/auth/auth_impl.ts
+++ packages/auth/src/core/auth/auth_impl.ts
@@ -97,7 +97,7 @@ export const enum DefaultConfig {
   API_HOST = 'identitytoolkit.googleapis.com',
   API_SCHEME = 'https',
   // TODO(sammansi): Update the endpoint before BYO-CIAM Private Preview Release.
-  REGIONAL_API_HOST = 'autopush-identityplatform.sandbox.googleapis.com/v2alpha/'
+  REGIONAL_API_HOST = 'identityplatform.googleapis.com/v2beta/'
 }
 
 export class AuthImpl implements AuthInternal, _FirebaseService {
