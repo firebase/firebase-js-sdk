f17ebd64c6d62ee904922da51349f208ba28a634
Athira M <athiramanu@google.com>
2025-10-28 17:29:26 +0530

    update imports

--- web-exp-bugbash/my-app/src/app/page.tsx
+++ web-exp-bugbash/my-app/src/app/page.tsx
@@ -1,10 +1,9 @@
 'use client'
 
 import { useEffect, useState } from "react";
-import { initializeApp } from "firebase/app";
-import { getRemoteConfig, fetchAndActivate, getString } from "firebase/remote-config";
-import { getInstallations } from "firebase/installations";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "@firebase/app";
+import { getRemoteConfig, fetchAndActivate, getString } from "@firebase/remote-config";
+import { getAnalytics } from "@firebase/analytics";
 
 function Screen({ isLoading, rollout, abt } = { isLoading: false, rollout: "", abt: "" }) {
   if (isLoading) {
@@ -29,6 +28,13 @@ export default function Home() {
 
   useEffect(() => {
     const firebaseConfig = {
+      apiKey: "AIzaSyAjV1YKBW-eRbnGJSGAj9o3RiLxToYtwSg",
+      authDomain: "fir-web-abt-test.firebaseapp.com",
+      projectId: "fir-web-abt-test",
+      storageBucket: "fir-web-abt-test.firebasestorage.app",
+      messagingSenderId: "606109706131",
+      appId: "1:606109706131:web:c571494af2f72000886a66",
+      measurementId: "G-X818YSYRDK"
     };
     console.log(process.env.API_KEY);
 
@@ -36,8 +42,6 @@ export default function Home() {
     const analytics = getAnalytics(app);
     const config = getRemoteConfig(app);
     config.settings.minimumFetchIntervalMillis = 0;
-
-    const installations = getInstallations(app);
 
     fetchAndActivate(config).then(success => {
       setIsLoading(false);
