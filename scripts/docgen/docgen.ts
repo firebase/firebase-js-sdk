604295d6372e05405f5ba719eea9e2d32bea11f7
Maneesh Tewani <mtewani@google.com>
2025-10-29 16:12:28 -0700

    Fixed docgen file

--- scripts/docgen/docgen.ts
+++ scripts/docgen/docgen.ts
@@ -37,11 +37,7 @@ https://github.com/firebase/firebase-js-sdk
 `;
 
 const tmpDir = `${projectRoot}/temp`;
-const EXCLUDED_PACKAGES = [
-  'app-compat',
-  'util',
-  'rules-unit-testing',
-];
+const EXCLUDED_PACKAGES = ['app-compat', 'util', 'rules-unit-testing'];
 
 /**
  * When ordering functions, will prioritize these first params at
