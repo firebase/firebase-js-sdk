75e427a96eee38e73231eb14ff08d415b3fdf84a
Anit Das <anitdas@google.com>
2026-02-17 17:32:10 +0000

    OTEL Tracing Initial Commit

---
+++ packages/crashlytics/src/tracing/tracing-provider.ts
@@ -0,0 +1,135 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  WebTracerProvider,
+  BatchSpanProcessor,
+  ReadableSpan,
+  SpanExporter
+} from '@opentelemetry/sdk-trace-web';
+import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+import { resourceFromAttributes } from '@opentelemetry/resources';
+import { JsonTraceSerializer } from '@opentelemetry/otlp-transformer';
+import type { OTLPExporterConfigBase } from '@opentelemetry/otlp-exporter-base';
+import {
+  OTLPExporterBase,
+  createOtlpNetworkExportDelegate
+} from '@opentelemetry/otlp-exporter-base';
+import {
+  CompositePropagator,
+  W3CTraceContextPropagator,
+  ExportResult
+} from '@opentelemetry/core';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load';
+import { FetchInstrumentation } from '@opentelemetry/instrumentation-fetch';
+import { UserInteractionInstrumentation } from '@opentelemetry/instrumentation-user-interaction';
+
+import { FetchTransport } from '../logging/fetch-transport';
+import { DynamicHeaderProvider } from '../types';
+import { FirebaseApp } from '@firebase/app';
+
+/**
+ * Create a tracing provider for the current execution environment.
+ *
+ * @internal
+ */
+export function createTracingProvider(
+  app: FirebaseApp,
+  endpointUrl: string,
+  dynamicHeaderProviders: DynamicHeaderProvider[] = []
+): WebTracerProvider {
+  const { projectId, appId, apiKey } = app.options;
+
+  const resource = resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: 'firebase_telemetry_service',
+    'gcp.project_id': projectId,
+    'cloud.provider': 'gcp'
+  });
+
+  if (endpointUrl.endsWith('/')) {
+    endpointUrl = endpointUrl.slice(0, -1);
+  }
+
+  const otlpEndpoint = `${endpointUrl}/v1/projects/${projectId}/apps/${appId}/traces`;
+
+  const traceExporter = new OTLPTraceExporter(
+    {
+      url: otlpEndpoint,
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Goog-User-Project': projectId || '',
+        ...(apiKey ? { 'X-Goog-Api-Key': apiKey } : {})
+      }
+    },
+    dynamicHeaderProviders
+  );
+
+  const provider = new WebTracerProvider({
+    resource,
+    spanProcessors: [new BatchSpanProcessor(traceExporter)]
+  });
+
+  provider.register({
+    propagator: new CompositePropagator({
+      propagators: [new W3CTraceContextPropagator()]
+    })
+  });
+
+  registerInstrumentations({
+    instrumentations: [
+      new DocumentLoadInstrumentation(),
+      new FetchInstrumentation(),
+      new UserInteractionInstrumentation()
+    ]
+  });
+
+  return provider;
+}
+
+/** OTLP exporter that uses custom FetchTransport. */
+class OTLPTraceExporter
+  extends OTLPExporterBase<ReadableSpan[]>
+  implements SpanExporter {
+  constructor(
+    config: OTLPExporterConfigBase = {},
+    dynamicHeaderProviders: DynamicHeaderProvider[] = []
+  ) {
+    super(
+      createOtlpNetworkExportDelegate(
+        {
+          timeoutMillis: 10000,
+          concurrencyLimit: 5,
+          compression: 'none'
+        },
+        JsonTraceSerializer,
+        new FetchTransport({
+          url: config.url!,
+          headers: new Headers(config.headers),
+          dynamicHeaderProviders
+        })
+      )
+    );
+  }
+
+  override export(
+    spans: ReadableSpan[],
+    resultCallback: (result: ExportResult) => void
+  ): void {
+    super.export(spans, resultCallback);
+  }
+}
