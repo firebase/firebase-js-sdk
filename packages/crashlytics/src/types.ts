fa678a2bb1fc6eaba2cf867a339c54cc1f84be12
Anit Das <anitdas@google.com>
2026-02-18 15:39:53 +0000

    Linter suggested fixes

--- packages/crashlytics/src/types.ts
+++ packages/crashlytics/src/types.ts
@@ -16,6 +16,7 @@
  */
 
 import { LoggerProvider } from '@opentelemetry/sdk-logs';
+import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
 import { Crashlytics } from './public-types';
 
 /**
@@ -25,6 +26,7 @@ import { Crashlytics } from './public-types';
  */
 export interface CrashlyticsInternal extends Crashlytics {
   loggerProvider: LoggerProvider;
+  tracingProvider: WebTracerProvider;
 }
 
 type KeyValuePair = [key: string, value: string];
