7b109ab1dc337f5316cfb4a9629b23010c575104
Christina Holland <chholland@google.com>
2026-02-27 11:23:48 -0800

    Remove updateEmulatorBanner

--- packages/auth/src/core/auth/emulator.ts
+++ packages/auth/src/core/auth/emulator.ts
@@ -18,12 +18,7 @@ import { Auth } from '../../model/public_types';
 import { AuthErrorCode } from '../errors';
 import { _assert } from '../util/assert';
 import { _castAuth } from './auth_impl';
-import {
-  deepEqual,
-  isCloudWorkstation,
-  pingServer,
-  updateEmulatorBanner
-} from '@firebase/util';
+import { deepEqual, isCloudWorkstation, pingServer } from '@firebase/util';
 
 /**
  * Changes the {@link Auth} instance to communicate with the Firebase Auth Emulator, instead of production
@@ -105,7 +100,6 @@ export function connectAuthEmulator(
   // Workaround to get cookies in Firebase Studio
   if (isCloudWorkstation(host)) {
     void pingServer(`${protocol}//${host}${portStr}`);
-    updateEmulatorBanner('Auth', true);
   } else if (!disableWarnings) {
     emitEmulatorWarning();
   }
