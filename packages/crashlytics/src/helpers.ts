062d7af72422dca40d47192a6aefbdfde765120f
Rebecca He <rebeccahe@google.com>
2026-03-26 15:50:39 +0000

    Snapshot of crashlytics tracing feature
    * To once and for all deal with complex commit merge/revert history in the crashlytics branch, created a new feature branch for traces and rebased/copied all tracing related changes from crashlytics-tracing on top of the most recent crashlytics branch.
    * Manually reverted extraneous changes (e.g. accidental changes to eslint) that happened in the legacy crashlytics-tracing that were unnecessary for the tracing feature.
    * Addressed presubmit errors around formatting. There are a few more presubmit errors around module resolution that will be handled in a follow up.
    * Finally, ran the playground app locally and verified tracing is working and writing to Cloud via the proxy server.

--- packages/crashlytics/src/helpers.ts
+++ packages/crashlytics/src/helpers.ts
@@ -24,6 +24,7 @@ import {
 import { Crashlytics } from './public-types';
 import { CrashlyticsService } from './service';
 import { CrashlyticsInternal } from './types';
+import { sessionContextManager } from './tracing/session-context-manager';
 
 /**
  * Returns the app version from the provided Telemetry instance, if available.
@@ -57,7 +58,9 @@ export function getSessionId(): string | undefined {
  */
 export function startNewSession(crashlytics: Crashlytics): void {
   // Cast to CrashlyticsInternal to access internal loggerProvider
-  const { loggerProvider } = crashlytics as CrashlyticsInternal;
+  const { loggerProvider, tracingProvider } =
+    crashlytics as CrashlyticsInternal;
+
   if (
     typeof sessionStorage !== 'undefined' &&
     typeof crypto?.randomUUID === 'function'
@@ -66,6 +69,15 @@ export function startNewSession(crashlytics: Crashlytics): void {
       const sessionId = crypto.randomUUID();
       sessionStorage.setItem(CRASHLYTICS_SESSION_ID_KEY, sessionId);
 
+      const tracer = tracingProvider.getTracer('session-tracer');
+      const span = tracer.startSpan('session-start');
+      span.setAttribute(LOG_ENTRY_ATTRIBUTE_KEYS.SESSION_ID, sessionId);
+      span.setAttribute(
+        LOG_ENTRY_ATTRIBUTE_KEYS.APP_VERSION,
+        getAppVersion(crashlytics)
+      );
+      sessionContextManager.setSessionSpan(span);
+
       // Emit session creation log
       const logger = loggerProvider.getLogger('session-logger');
       logger.emit({
@@ -73,7 +85,9 @@ export function startNewSession(crashlytics: Crashlytics): void {
         body: 'Session created',
         attributes: {
           [LOG_ENTRY_ATTRIBUTE_KEYS.SESSION_ID]: sessionId,
-          [LOG_ENTRY_ATTRIBUTE_KEYS.APP_VERSION]: getAppVersion(crashlytics)
+          [LOG_ENTRY_ATTRIBUTE_KEYS.APP_VERSION]: getAppVersion(crashlytics),
+          [LOG_ENTRY_ATTRIBUTE_KEYS.TRACE_ID]: `${span.spanContext().traceId}`,
+          [LOG_ENTRY_ATTRIBUTE_KEYS.SPAN_ID]: `${span.spanContext().spanId}`
         }
       });
     } catch (e) {
@@ -90,10 +104,12 @@ export function registerListeners(crashlytics: Crashlytics): void {
   if (typeof window !== 'undefined' && typeof document !== 'undefined') {
     window.addEventListener('visibilitychange', async () => {
       if (document.visibilityState === 'hidden') {
+        sessionContextManager.getSessionSpan()?.end();
         await flush(crashlytics);
       }
     });
     window.addEventListener('pagehide', async () => {
+      sessionContextManager.getSessionSpan()?.end();
       await flush(crashlytics);
     });
   }
