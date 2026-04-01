a1e59c0a1a9685101ac73254167620010b75f39f
stephenarosaj <stephenarosaj@google.com>
2026-04-01 14:00:17 -0700

    run format. wow

--- packages/data-connect/src/util/url.ts
+++ packages/data-connect/src/util/url.ts
@@ -26,7 +26,7 @@ export function urlBuilder(
   transportOptions: TransportOptions
 ): string {
   // eslint-disable-next-line no-console
-  console.log("THIS IS A CHANGE!");
+  console.log('THIS IS A CHANGE!');
   const { connector, location, projectId: project, service } = projectConfig;
   const { host, sslEnabled, port } = transportOptions;
   const protocol = sslEnabled ? 'https' : 'http';
