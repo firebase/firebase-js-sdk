330a387df59fbe23b5c32bbd120f7c5dce138a14
Christina Holland <chholland@google.com>
2026-05-04 11:20:39 -0700

    chore: migrate test functions to v2 (#9910)

--- config/functions/index.js
+++ config/functions/index.js
@@ -16,143 +16,121 @@
  */
 
 const assert = require('assert');
-const cors = require('cors')({ origin: true });
-const functions = require('firebase-functions/v1');
+const { onRequest, onCall } = require('firebase-functions/v2/https');
 
 /*
  * These backend test helpers are copied from the iOS and Android SDKs, but are
  * not all implemented as tests in the JS SDK yet.
  */
 
-exports.dataTest = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    assert.deepEqual(request.body, {
-      data: {
-        bool: true,
-        int: 2,
-        // TODO(klimt): Should we add an API for encoding longs?
-        /*long: {
+exports.dataTestv2 = onRequest({ cors: true }, (request, response) => {
+  assert.deepEqual(request.body, {
+    data: {
+      bool: true,
+      int: 2,
+      // TODO(klimt): Should we add an API for encoding longs?
+      /*long: {
           value: '3',
           '@type': 'type.googleapis.com/google.protobuf.Int64Value',
         },*/
-        str: 'four',
-        array: [5, 6],
-        null: null
+      str: 'four',
+      array: [5, 6],
+      null: null
+    }
+  });
+  response.send({
+    data: {
+      message: 'stub response',
+      code: 42,
+      long: {
+        value: '420',
+        '@type': 'type.googleapis.com/google.protobuf.Int64Value'
       }
-    });
-    response.send({
-      data: {
-        message: 'stub response',
-        code: 42,
+    }
+  });
+});
+
+exports.scalarTestv2 = onRequest({ cors: true }, (request, response) => {
+  assert.deepEqual(request.body, { data: 17 });
+  response.send({ data: 76 });
+});
+
+exports.tokenTestv2 = onRequest({ cors: true }, (request, response) => {
+  assert.equal(request.get('Authorization'), 'Bearer token');
+  assert.deepEqual(request.body, { data: {} });
+  response.send({ data: {} });
+});
+
+exports.instanceIdTestv2 = onRequest({ cors: true }, (request, response) => {
+  assert.equal(request.get('Firebase-Instance-ID-Token'), 'iid');
+  assert.deepEqual(request.body, { data: {} });
+  response.send({ data: {} });
+});
+
+exports.appCheckTestv2 = onRequest({ cors: true }, (request, response) => {
+  const token = request.get('X-Firebase-AppCheck');
+  assert.equal(token !== undefined, true);
+  assert.deepEqual(request.body, { data: {} });
+  response.send({ data: { token } });
+});
+
+exports.nullTestv2 = onRequest({ cors: true }, (request, response) => {
+  assert.deepEqual(request.body, { data: null });
+  response.send({ data: null });
+});
+
+exports.missingResultTestv2 = onRequest({ cors: true }, (request, response) => {
+  assert.deepEqual(request.body, { data: null });
+  response.send({});
+});
+
+exports.unhandledErrorTestv2 = onRequest(
+  { cors: true },
+  (request, response) => {
+    // Fail in a way that the client shouldn't see.
+    throw 'nope';
+  }
+);
+
+exports.unknownErrorTestv2 = onRequest({ cors: true }, (request, response) => {
+  // Send an http error with a body with an explicit code.
+  response.status(400).send({
+    error: {
+      status: 'THIS_IS_NOT_VALID',
+      message: 'this should be ignored'
+    }
+  });
+});
+
+exports.explicitErrorTestv2 = onRequest({ cors: true }, (request, response) => {
+  // Send an http error with a body with an explicit code.
+  response.status(400).send({
+    error: {
+      status: 'OUT_OF_RANGE',
+      message: 'explicit nope',
+      details: {
+        start: 10,
+        end: 20,
         long: {
-          value: '420',
+          value: '30',
           '@type': 'type.googleapis.com/google.protobuf.Int64Value'
         }
       }
-    });
+    }
   });
 });
 
-exports.scalarTest = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    assert.deepEqual(request.body, { data: 17 });
-    response.send({ data: 76 });
-  });
+exports.httpErrorTestv2 = onRequest({ cors: true }, (request, response) => {
+  // Send an http error with no body.
+  response.status(400).send();
 });
 
-exports.tokenTest = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    assert.equal(request.get('Authorization'), 'Bearer token');
-    assert.deepEqual(request.body, { data: {} });
-    response.send({ data: {} });
-  });
-});
-
-exports.instanceIdTest = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    assert.equal(request.get('Firebase-Instance-ID-Token'), 'iid');
-    assert.deepEqual(request.body, { data: {} });
-    response.send({ data: {} });
-  });
-});
-
-exports.appCheckTest = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    const token = request.get('X-Firebase-AppCheck');
-    assert.equal(token !== undefined, true);
-    assert.deepEqual(request.body, { data: {} });
-    response.send({ data: { token } });
-  });
-});
-
-exports.nullTest = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    assert.deepEqual(request.body, { data: null });
-    response.send({ data: null });
-  });
-});
-
-exports.missingResultTest = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    assert.deepEqual(request.body, { data: null });
-    response.send({});
-  });
-});
-
-exports.unhandledErrorTest = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    // Fail in a way that the client shouldn't see.
-    throw 'nope';
-  });
-});
-
-exports.unknownErrorTest = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    // Send an http error with a body with an explicit code.
-    response.status(400).send({
-      error: {
-        status: 'THIS_IS_NOT_VALID',
-        message: 'this should be ignored'
-      }
-    });
-  });
-});
-
-exports.explicitErrorTest = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    // Send an http error with a body with an explicit code.
-    response.status(400).send({
-      error: {
-        status: 'OUT_OF_RANGE',
-        message: 'explicit nope',
-        details: {
-          start: 10,
-          end: 20,
-          long: {
-            value: '30',
-            '@type': 'type.googleapis.com/google.protobuf.Int64Value'
-          }
-        }
-      }
-    });
-  });
-});
-
-exports.httpErrorTest = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    // Send an http error with no body.
-    response.status(400).send();
-  });
-});
-
-exports.timeoutTest = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    // Wait for longer than 500ms.
-    setTimeout(() => response.send({ data: true }), 500);
-  });
+exports.timeoutTestv2 = onRequest({ cors: true }, (request, response) => {
+  // Wait for longer than 500ms.
+  setTimeout(() => response.send({ data: true }), 500);
 });
 
 // Used by E2E test.
-exports.callTest = functions.https.onCall((data, context) => {
+exports.callTestv2 = onCall(request => {
   return { word: 'hellooo' };
 });
