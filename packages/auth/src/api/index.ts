712091c3962b8b3639775a4150c91d9a3962b57b
macastelaz <34776182+macastelaz@users.noreply.github.com>
2026-06-12 20:51:46 +0000

    Support API key referrer restrictions by changing the Auth SDK referrer policy (#10049)
    * feat(auth): Update referrer policy to strict-origin-when-cross-origin
    
    * chore: Add changeset tame-yaks-yawn.md

--- packages/auth/src/api/index.ts
+++ packages/auth/src/api/index.ts
@@ -179,7 +179,7 @@ export async function _performApiRequest<T, V>(
        'RequestInitializerDict' is not implemented."
        https://github.com/cloudflare/next-on-pages/issues/487 */
     if (!isCloudflareWorker()) {
-      fetchArgs.referrerPolicy = 'no-referrer';
+      fetchArgs.referrerPolicy = 'strict-origin-when-cross-origin';
     }
 
     if (auth.emulatorConfig && isCloudWorkstation(auth.emulatorConfig.host)) {
