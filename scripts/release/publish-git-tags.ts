0307451c8fd30e2d7258ff251e68fc3e03fc6e3d
Christina Holland <chholland@google.com>
2026-07-09 17:06:06 -0700

    address comments

--- scripts/release/publish-git-tags.ts
+++ scripts/release/publish-git-tags.ts
@@ -18,32 +18,15 @@
 import { exec } from 'child-process-promise';
 import { projectRoot as root } from '../utils';
 
-async function newFunc() {
-  const { stdout: rawTags } = await exec(`git tag --points-at HEAD`);
-  const tags = rawTags
-    .split(/\\r?\\n/)
-    .map(tag => tag.trim())
-    .filter(Boolean);
-
-  if (tags.length === 0) {
-    console.log('No release tags found pointing to HEAD. Skipping push.');
-    return;
-  }
-  let { stdout: currentBranch } = await exec(`git rev-parse --abbrev-ref HEAD`);
-  currentBranch = currentBranch.trim();
-
-  await exec(
-    `git -c http.extraHeader=\"Authorization: Bearer ${process.env.GITHUB_TOKEN}\"` +
-      ` push origin ${currentBranch} ${tags.join(' ')} --no-verify`,
-    {
-      cwd: root
-    }
-  );
-}
 async function pushReleaseTagsToGithub() {
   // Get tags pointing to HEAD
   // When running the release script, these tags should be release tags created by changeset
   const { stdout: rawTags } = await exec(`git tag --points-at HEAD`);
+
+  if (!rawTags) {
+    console.error('No tags found pointing to HEAD.');
+    process.exit(1);
+  }
 
   const tags = rawTags.split(/\r?\n/);
 
