52450aca5dcc0ae97d9141f067475fc5cf10798c
Denver Coneybeare <dconeybe@google.com>
2026-07-28 17:48:16 -0400

    Revert "karma.conf.js: disable CORS to see if that fixes the github actions failures"
    This reverts commit 10add5e639ce2b65de042830bf5c58df866f5d77.

--- packages/data-connect/karma.conf.js
+++ packages/data-connect/karma.conf.js
@@ -25,14 +25,7 @@ module.exports = function (config) {
     files: files,
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
-    customLaunchers: {
-      Chrome_without_security: {
-        base: 'ChromeHeadless',
-        flags: ['--disable-web-security', '--no-sandbox']
-      }
-    },
-    browsers: ['Chrome_without_security']
+    frameworks: ['mocha']
   });
 
   config.set(karmaConfig);
