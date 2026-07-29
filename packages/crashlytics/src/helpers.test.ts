aa4a64a4290dc22ce2b338aacae11ed2554af7ed
Bryan Atkinson <bryanatkinson@google.com>
2026-07-29 16:31:37 -0400

    Fix TypeScript types and test mocks for crashlytics package

--- packages/crashlytics/src/helpers.test.ts
+++ packages/crashlytics/src/helpers.test.ts
@@ -43,8 +43,7 @@ describe('helpers', () => {
       return {
         emit: (logRecord: LogRecord) => {
           emittedLogs.push(logRecord);
-        },
-        enabled: () => true
+        }
       };
     },
     forceFlush: () => {
