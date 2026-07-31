c7427bd6bbb304774310bd7c55370f0089ebbf9e
Christina Holland <chholland@google.com>
2026-07-31 09:44:35 -0700

    add object.keys

--- packages/database-compat/rollup.config.js
+++ packages/database-compat/rollup.config.js
@@ -132,7 +132,7 @@ const cjsBuilds = [
     external: id =>
       // for the standalone bundle, ignore peerDeps, so app and app-compat
       // will be bundled
-      pkg.dependencies
+      Object.keys(pkg.dependencies)
         .filter(dep => dep !== '@firebase/database')
         .some(dep => id === dep || id.startsWith(`${dep}/`)),
     onwarn: onWarn
