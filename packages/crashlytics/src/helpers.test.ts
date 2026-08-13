4cd3780cd98c5f89d14f9450dfc6833a9ce537b4
Bryan Atkinson <bryanatkinson@google.com>
2026-08-13 09:55:56 -0400

    fix(crashlytics): add enabled method to mock Logger in helpers.test.ts

--- packages/crashlytics/src/helpers.test.ts
+++ packages/crashlytics/src/helpers.test.ts
@@ -41,7 +41,8 @@ describe('helpers', () => {
       return {
         emit: (logRecord: LogRecord) => {
           emittedLogs.push(logRecord);
-        }
+        },
+        enabled: () => true
       };
     },
     forceFlush: () => {
