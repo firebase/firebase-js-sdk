b2abc6187395001b9755b462b91e04fac3112d62
Christina Holland <chholland@google.com>
2026-08-19 15:51:15 -0700

    rough implementation

--- scripts/release/publish-git-tags.ts
+++ scripts/release/publish-git-tags.ts
@@ -17,21 +17,44 @@
 
 import { exec } from 'child-process-promise';
 import { projectRoot as root } from '../utils';
+import { getAllPackages, mapPkgNameToPkgJson } from './utils/workspace';
 
 async function pushReleaseTagsToGithub() {
+  let tags;
+  let { stdout: currentBranch } = await exec(`git rev-parse --abbrev-ref HEAD`);
+  currentBranch = currentBranch.trim();
   // Get tags pointing to HEAD
   // When running the release script, these tags should be release tags created by changeset
   const { stdout: rawTags } = await exec(`git tag --points-at HEAD`);
 
-  if (!rawTags.trim()) {
-    console.error('No tags found pointing to HEAD.');
-    process.exit(1);
+  if (rawTags.trim()) {
+    tags = rawTags.split(/\r?\n/);
+  } else {
+    console.log(
+      'No tags found pointing to HEAD. Diffing tags in this branch vs main.'
+    );
+    const pkgNames = await getAllPackages();
+    const mainVersions = new Map();
+    await exec('git checkout main');
+    await exec('git pull origin main');
+    for (const pkgName of pkgNames) {
+      const json = await mapPkgNameToPkgJson(pkgName);
+      mainVersions.set(pkgName, json.version);
+    }
+    await exec(`git checkout ${currentBranch}`);
+    for (const pkgName of pkgNames) {
+      const json = await mapPkgNameToPkgJson(pkgName);
+      if (mainVersions.get(pkgName) !== json.version) {
+        console.log('diff found');
+        const tag = `${pkgName}@${json.version}`;
+        const tagExists = await exec(`git tag -l ${tag}`);
+        if (!tagExists) {
+          console.log('going to tag', tag);
+          // await exec(`git tag ${tag}`);
+        }
+      }
+    }
   }
-
-  const tags = rawTags.split(/\r?\n/);
-
-  let { stdout: currentBranch } = await exec(`git rev-parse --abbrev-ref HEAD`);
-  currentBranch = currentBranch.trim();
 
   const token = process.env.GITHUB_TOKEN;
   if (!token) {
@@ -39,17 +62,17 @@ async function pushReleaseTagsToGithub() {
     process.exit(1);
   }
 
-  const authHeader = Buffer.from(`x-access-token:${token}`).toString('base64');
+  // const authHeader = Buffer.from(`x-access-token:${token}`).toString('base64');
 
-  await exec(
-    'git -c http.extraHeader="Authorization: Basic ' +
-      authHeader +
-      '"' +
-      ` push origin ${currentBranch} ${tags.join(' ')} --no-verify`,
-    {
-      cwd: root
-    }
-  );
+  // await exec(
+  //   'git -c http.extraHeader="Authorization: Basic ' +
+  //     authHeader +
+  //     '"' +
+  //     ` push origin ${currentBranch} ${tags.join(' ')} --no-verify`,
+  //   {
+  //     cwd: root
+  //   }
+  // );
 }
 
 pushReleaseTagsToGithub();
