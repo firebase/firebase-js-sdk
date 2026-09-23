409c62c8e1820dfc51aaf7b18e608ad3ddf6ddf1
Manvi Gupta <guptamanvi@google.com>
2026-09-23 21:38:20 +0000

    fix(auth): keep require() in test/helpers/integration/settings.ts so auth-compat tsc rootDir stays at packages/

--- packages/auth/test/helpers/integration/settings.ts
+++ packages/auth/test/helpers/integration/settings.ts
@@ -16,13 +16,14 @@
  */
 
 import { FirebaseOptions } from '@firebase/app';
-import PROJECT_CONFIG from '../../../../../config/project.json';
 
 // __karma__ is an untyped global
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const __karma__: any;
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports
+const PROJECT_CONFIG = require('../../../../../config/project.json');
+
 const EMULATOR_HOST = process.env.FIREBASE_AUTH_EMULATOR_HOST;
 const EMULATOR_PROJECT_ID = process.env.GCLOUD_PROJECT;
 
